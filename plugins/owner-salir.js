fa27eff12f33400e04b76e12217f44f0482515ac
Niño Piña <ninopina10@gmail.com>
2024-12-08 12:31:35 -0600

    Actualizar plugins

---
+++ plugins/owner-salir.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn, text, command }) => {
+let id = text ? text : m.chat  
+let img = 'https://i.ibb.co/s9N9QhG/file.jpg'
+await conn.sendMessage(m.chat, { video: { url: pp }, gifPlayback: true, caption: '*ADIOS, BOTBARBOZA SE DESPIDE!🌠👋*', mentions: [m.sender] }, { quoted: estilo })
+await conn.groupLeave(id)}
+handler.help = ['salir']
+handler.tags = ['owner']
+handler.command = /^(salir|out|leavegc|leave|salirdelgrupo)$/i
+handler.group = true
+handler.rowner = true
+
+export default handler
