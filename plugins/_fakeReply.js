5ba194600d714a4dce9581a3261a99ee037ea196
Niño Piña <ninopina10@gmail.com>
2024-12-07 22:41:09 -0600

    Actualizar plugins

---
+++ plugins/_fakeReply.js
@@ -0,0 +1,49 @@
+import fetch from 'node-fetch'
+
+export async function before(m, { conn }) {
+let name = '⛄𝑩𝒐𝒕𝑩𝒂𝒓𝒃𝒐𝒛𝒂 𝑴𝑫 - 𝑪𝒉𝒂𝒏𝒏𝒆𝒍🌲'
+let imagenes = ["https://i.ibb.co/JmcS3kv/Sylph.jpg",
+"https://i.ibb.co/Cs6Tt9V/Sylph.jpg",
+"https://i.ibb.co/JmcS3kv/Sylph.jpg",
+"https://i.ibb.co/Cs6Tt9V/Sylph.jpg",
+"https://i.ibb.co/JmcS3kv/Sylph.jpg",
+"https://i.ibb.co/Cs6Tt9V/Sylph.jpg",
+"https://i.ibb.co/JmcS3kv/Sylph.jpg"]
+
+let icono = imagenes[Math.floor(Math.random() * imagenes.length)]
+
+
+global.rcanal = {
+ contextInfo: {
+             isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: "120363317263885467@newsletter",
+      serverMessageId: 100,
+      newsletterName: name,
+   }, 
+   externalAdReply: {
+    showAdAttribution: true, 
+    title: botname, 
+    body: textbot, 
+    mediaUrl: null, 
+    description: null, 
+    previewType: "PHOTO", 
+    thumbnailUrl: icono, 
+    sourceUrl: canal, 
+    mediaType: 1, 
+    renderLargerThumbnail: false }, 
+    }, 
+    }
+
+
+ global.fake = {
+    contextInfo: {
+            isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: "120363317263885467@newsletter",
+      serverMessageId: 100,
+      newsletterName: name,
+    },
+    },
+  }
+}
