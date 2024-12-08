5b0855fb4b0ae22430f6ac3a3a9d05ab61701fb7
Niño Piña <ninopina10@gmail.com>
2024-12-08 12:25:55 -0600

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -42,23 +42,23 @@ const defaultMenu = {
 > 「 ${textbot} あ⁩ 」\n
 
 ›    ╔──────¤◎¤──────╗
-› *╭━❍☆BARBOZA-BOT-AI☆❍━╮*
+› *╭━❍🎄BARBOZA-BOT-AI⛄❍━╮*
 ›    ╚──────¤◎¤──────╝
 ╭━─━─━─≪𖣘≫─━─━─━╮
-┃✰ ➬ *Cliente:* %name
-┃✰ ➬ *Estrellas:* %limit
-┃✰ ➬ *Nivel:* %level [ %xp4levelup Xp Para Subir De Nivel]
-┃✰ ➬ *Xp:* %exp / %maxexp
-┃✰ ➬ *TotalXp:* %totalexp
+┃❄️ ➬ *Cliente:* %name
+┃❄️ ➬ *Estrellas:* %limit
+┃❄️ ➬ *Nivel:* %level [ %xp4levelup Xp Para Subir De Nivel]
+┃❄️ ➬ *Xp:* %exp / %maxexp
+┃❄️ ➬ *TotalXp:* %totalexp
  ╰━─━─━─≪𖣘≫─━─━─━╯
 ╔──────¤◎¤──────╗
 ┋#    ✦   *𝐈 N F O  ‹‹❑ౄ*
 ╚──────¤◎¤──────╝
 ╭━─━─━─≪𖣘≫─━─━─━╮
-┃✰ ➬ *Modo:* %mode
-┃✰ ➬ *Prefijo:* [ *%_p* ]
-┃✰ ➬ *Rutina:* %muptime 
-┃✰ ➬ *Database:*  %totalreg
+┃❄️ ➬ *Modo:* %mode
+┃❄️ ➬ *Prefijo:* [ *%_p* ]
+┃❄️ ➬ *Rutina:* %muptime 
+┃❄️ ➬ *Database:*  %totalreg
 ╰━─━─━─≪𖣘≫─━─━─━╯
      
    ⏤͟͟͞͞★BarbozaBot-Aiꗄ➺
@@ -67,7 +67,7 @@ const defaultMenu = {
 \t\t\t_*Lista De Comandos*_ ﭢ
 `.trimStart(),
   header: '╭═════⊹⊱≼「 *%category* 」≽⊰⊹════•',
-  body: '┃👻› ✧ . %cmd %islimit %isPremium\n',
+  body: '┃⛄› ✧ . %cmd %islimit %isPremium\n',
   footer: '╰═════⊹⊱≼≽⊰⊹═════╯',
   after: `© ${textbot}`,
 }
@@ -192,7 +192,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     let pp14 = 'https://i.ibb.co/JmcS3kv/Sylph.jpg'
     let pp15 = 'https://i.ibb.co/Cs6Tt9V/Sylph.jpg'
     let img = 'https://i.ibb.co/f9kvM3S/file.jpg'
-    await m.react('👻')
+    await m.react('❄️')
    // await conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: estilo })
     await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null, rcanal)
    //await conn.sendAi(m.chat, botname, textbot, text.trim(), img, img, canal, estilo)
