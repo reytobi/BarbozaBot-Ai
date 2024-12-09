86c5a90bfb43c3c4769b29dcfa632a12a4b84fbb
Niño Piña <ninopina10@gmail.com>
2024-12-09 15:54:01 -0600

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -1,14 +1,89 @@
 import { sticker } from '../lib/sticker.js'
 import uploadFile from '../lib/uploadFile.js'
-import uploadImage from '../lib/uploadImage.js'
-import { webp2png } from '../lib/webp2mp4.js'
+import { addExif } from '../lib/sticker.js'
+import { Sticker } from 'wa-sticker-formatter'
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  try {
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || q.mediaType || ''
+
+    if (/video/g.test(mime)) {
+      // Jalankan kode untuk video di sini
+      if ((q.msg || q).seconds > 10) return m.reply('✧ Máximo 10 segundos.')
+      let img = await q.download?.()
+      if (!img) throw m.reply(`✧ Responde a un Vídeo con el comando*${usedPrefix + command}*`)
+      let stiker = false
+      try {
+        stiker = await sticker(img, false, global.stickpack, global.stickauth)
+      } catch (e) {
+        console.error(e)
+      } finally {
+        if (!stiker) {
+          let out = await uploadFile(img)
+          stiker = await sticker(false, out, global.stickpack, global.stickauth)
+        }
+      }
+      conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, null)
+    } else if (/image/g.test(mime)) {
+      // Jalankan kode untuk gambar di sini
+      let [packname, ...author] = args.join` `.split`|`
+      author = (author || []).join`|`
+      let img = await q.download?.()
+      let stiker = false
+      try {
+        let pack = global.stickpack
+        let author = global.stickauth
+        stiker = await addExif(img, pack, author)
+      } catch (e) {
+        console.error(e)
+      } finally {
+        if (!stiker) {
+          stiker = await createSticker(img, false, packname, author)
+        }
+      }
+      m.reply(stiker)
+    } else {
+      m.reply('     `✧ Responde a una Imagen o Video.`')
+    }
+  } catch (e) {
+    console.error(e)
+    m.reply('Error')
+  }
+}
+
+handler.help = ['sticker']
+handler.tags = ['sticker']
+
+handler.command = /^s(tic?ker)?(gif)?$/i
+handler.register = true
+
+export default handler
+
+async function createSticker(img, url, packName, authorName, quality) {
+  let stickerMetadata = {
+    type: 'full',
+    pack: stickpack,
+    author: stickauth,
+    quality
+  }
+  return (new Sticker(img ? img : url, stickerMetadata)).toBuffer()
+}
+
+
+
+
+
+/* import { sticker } from '../lib/sticker.js'
+import uploadFile from '../lib/uploadFile.js'
+import { addExif } from '../lib/sticker.js'
+import { Sticker } from 'wa-sticker-formatter'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   let stiker = false
-const usser = conn.getName(m.sender)
-const h = ` ㌹ BarbozaBot`;
-const i = 
-`By Staff De StarCore-Allisanse`
+       let stick = args.join(" ").split("|");
+       let f = stick[0] !== "" ? stick[0] : packname;
+       let g = typeof stick[1] !== "undefined" ? stick[1] : author;
   try {         
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || q.mediaType || ''
@@ -16,9 +91,10 @@ const i =
       if (/video/g.test(mime)) if ((q.msg || q).seconds > 11) return m.reply('Máximo 10 segundos')
       let img = await q.download?.()
       if (!img) throw `✳️ Responde a una imagen o video con*${usedPrefix + command}*`
+m.react('🐈‍⬛') 
       let out
       try {
-        stiker = await sticker(img, false, h, i)
+        stiker = await sticker(img, false, f, g)
       } catch (e) {
         console.error(e)
       } finally {
@@ -27,7 +103,7 @@ const i =
           else if (/image/g.test(mime)) out = await uploadImage(img)
           else if (/video/g.test(mime)) out = await uploadFile(img)
           if (typeof out !== 'string') out = await uploadImage(img)
-          stiker = await sticker(false, out, h, i)
+          stiker = await sticker(false, out, f, g)
         }
       }
     } else if (args[0]) {
@@ -39,15 +115,33 @@ const i =
     if (!stiker) stiker = e
   } finally {
     if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, null, rpl)
-    else throw `${mssg.stickError}`
+    else throw '➤ `𝗔𝗩𝗜𝗦𝗢` 🐈‍⬛\n\n*RESPONDE UNA IMG/VIDEO*';
   }
 }
 handler.help = ['sticker']
 handler.tags = ['sticker']
-handler.command = ['s', 'sticker'] 
+handler.command = /^s(tic?ker)?(gif)?$/i
 
 export default handler
 
 const isUrl = (text) => {
   return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))
 }
+
+handler.help = ['sticker']
+handler.tags = ['sticker']
+
+handler.command = /^s(tic?ker)?(gif)?$/i
+handler.register = true
+
+export default handler
+
+async function createSticker(img, url, packName, authorName, quality) {
+  let stickerMetadata = {
+    type: 'full',
+    pack: stickpack,
+    author: stickauth,
+    quality
+  }
+  return (new Sticker(img ? img : url, stickerMetadata)).toBuffer()
+} */
