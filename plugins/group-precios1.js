3716c2d4686a72ddf2755cd1636422114e7ac2dd
Niño Piña <ninopina10@gmail.com>
2024-12-09 17:05:34 -0600

    Actualizar group-precios1.js

--- plugins/group-precios1.js
+++ plugins/group-precios1.js
@@ -14,4 +14,5 @@ await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: message }, {
 handler.help = ['precios1'];
 handler.tags = ['main'];
 handler.command = ['precios1', 'p1', 'precio1'];
+handler.group = true;
 export default handler;
