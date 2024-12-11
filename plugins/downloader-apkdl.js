25a575cbd3f00eed93ba4dcb456d1b9afc5f4256
Niño Piña <ninopina10@gmail.com>
2024-12-11 09:34:52 -0600

    Actualizar downloader-apkdl.js

--- plugins/downloader-apkdl.js
+++ plugins/downloader-apkdl.js
@@ -11,7 +11,8 @@ let texto = ` \`\`\`
     ✦ 𝐓𝐚𝐦𝐚𝐧̃𝐨 : ⇢ ${size} ⚖️
     ✦ 𝐏𝐚𝐜𝐤𝐚𝐠𝐞 : ⇢ ${packe} 📦
     ✦ 𝐀𝐜𝐭𝐮𝐚𝐥𝐢𝐳𝐚𝐝𝐨 : ⇢ ${lastUpdate} 🗓️
-    ✦ 𝐂𝐫𝐞𝐚𝐝𝐨𝐫 :⇢  WillZek\n\n> https://whatsapp.com/channel/0029VakfOZfHFxP7rNrUQk2d
+    ✦ 𝐂𝐫𝐞𝐚𝐝𝐨𝐫 :⇢  WillZek
+> https://whatsapp.com/channel/0029VakfOZfHFxP7rNrUQk2d
 
 ## Su aplicación se enviará en un momento . . .
 
