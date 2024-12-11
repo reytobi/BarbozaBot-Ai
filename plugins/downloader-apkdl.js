5b984e997cccebe88868b43a6ee66135737cb015
Niño Piña <ninopina10@gmail.com>
2024-12-11 09:36:46 -0600

    Actualizar downloader-apkdl.js

--- plugins/downloader-apkdl.js
+++ plugins/downloader-apkdl.js
@@ -17,7 +17,7 @@ let texto = ` \`\`\`
 
 ## Su aplicación se enviará en un momento . . .
 
-   - ${wm} -               
+    ${packname}              
 `
 await conn.sendFile(m.chat, icon, name + '.jpg', texto, m)
 
