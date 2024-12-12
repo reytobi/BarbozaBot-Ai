03ede69fa3990d8658e4bd8326a747e8321181cf
Niño Piña <ninopina10@gmail.com>
2024-12-12 12:04:05 -0600

    Actualizar downlader-spotify.js

--- plugins/downlader-spotify.js
+++ plugins/downlader-spotify.js
@@ -1,20 +1,20 @@
 let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
-if (!args[0]) return conn.reply(m.chat, `\`\`\`[ 🌼 ] Por Favor ingresa un link de Spotify. Ejemplo:\n${usedPrefix + command} https://open.spotify.com/intl-es/track/0P0BTqkBQuAlzbwbTEV57m\`\`\``, null, fkontak)
-m.react('❄️');
-let api = await fetch(`https://api.dorratz.com/spotifydl?url=${args[0]}`);
-let res = await api.json();
-let { title, duration, url, thumbnail } = res.data;
-let { name: artistName} = res.data.artist;
-    let text = `╭━━━⊜ ⌊ \`Spotify Download\` ⌉⊜━━━\n`
-    text += `│  ≡ Nombre: \`${title}\`\n`
-    text += `│  ≡ Duración: \`${duration}\`\n`
-    text += `│  ≡ Artiste: \`${name}\`\n`
-    text += `╰━━━━━━━━━━━━━━⊜\n`
-    text += `  _Enviando el archivo . . . ._\n- \`${botname}\` -`
-conn.sendFile(m.chat, thumbnail, title + '.jpg', text, m, null, rpl)
-/*************/
-await conn.sendMessage(m.chat, { audio: { url: url }}, { quoted: fkontak });
-m.react('✅');
+    if (!args[0]) return conn.reply(m.chat, `\`\`\`[ 🌼 ] Por favor ingresa un link de Spotify. Ejemplo:\n${usedPrefix + command} https://open.spotify.com/intl-es/track/0P0BTqkBQuAlzbwbTEV57m\`\`\``, null, fkontak);
+    m.react('❄️');
+    let api = await fetch(`https://api.dorratz.com/spotifydl?url=${args[0]}`);
+    let res = await api.json();
+    let { title, duration, url, thumbnail } = res.data;
+    let { name: artistName } = res.data.artist; // Cambiar el nombre a artistName
+    let text = `╭━━━⊜ ⌊ \`Spotify Download\` ⌉⊜━━━\n`;
+    text += `│  ≡ Nombre: \`${title}\`\n`;
+    text += `│  ≡ Duración: \`${duration}\`\n`;
+    text += `│  ≡ Artista: \`${artistName}\`\n`; // Usar artistName aquí
+    text += `╰━━━━━━━━━━━━━━⊜\n`;
+    text += `  _Enviando el archivo . . . ._\n- \`${botname}\` -`;
+    conn.sendFile(m.chat, thumbnail, title + '.jpg', text, m, null, rpl);
+    /*************/
+    await conn.sendMessage(m.chat, { audio: { url: url } }, { quoted: fkontak });
+    m.react('✅');
 }
 handler.help = ['spotifydl', 'spotify'];
 handler.tags = ['downloader'];
