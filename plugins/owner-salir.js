17a3b6e345daab9d04eec4b435299f3b2647466c
Niño Piña <ninopina10@gmail.com>
2024-12-11 20:07:30 -0600

    Actualizar owner-salir.js

--- plugins/owner-salir.js
+++ plugins/owner-salir.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
-let img = 'https://i.ibb.co/s9N9QhG/file.jpg'
+let pp = 'https://i.ibb.co/s9N9QhG/file.jpg'
 await conn.sendMessage(m.chat, { video: { url: pp }, gifPlayback: true, caption: '*ADIOS, BOTBARBOZA SE DESPIDE!🌠👋*', mentions: [m.sender] }, { quoted: estilo })
 await conn.groupLeave(id)}
 handler.help = ['salir']
