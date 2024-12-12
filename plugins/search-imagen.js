2c932c473ec5cd88a110fe728d7304a20cda2a87
Niño Piña <ninopina10@gmail.com>
2024-12-11 22:48:53 -0600

    Actualizar search-imagen.js

--- plugins/search-imagen.js
+++ plugins/search-imagen.js
@@ -1,49 +1,18 @@
 import {googleImage} from '@bochilteam/scraper';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) return conn.reply(m.chat, `*💛 Uso Correcto: ${usedPrefix + command} Nakano Nino*`, m, rcanal);
-  conn.reply(m.chat, '💛 *Descargando su imagen...*', m, {
-    contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-      title: packname,
-      body: wm,
-      previewType: 0, thumbnail: icono,
-      sourceUrl: canal }}})
-  const res = await googleImage(text);
-  const image = await res.getRandom();
-  const link = image;
-  const messages = [
-    { 
-      "title": "Imagen 1",
-      "description": dev,
-      "url": await res.getRandom(),
-      "buttons": [[]],
-    },
-    { 
-      "title": "Imagen 2",
-      "description": dev,
-      "url": await res.getRandom(),
-      "buttons": [[]],
-    },
-    { 
-      "title": "Imagen 3",
-      "description": dev,
-      "url": await res.getRandom(),
-      "buttons": [[]],
-    },
-    { 
-      "title": "Imagen 4",
-      "description": dev,
-      "url": await res.getRandom(),
-      "buttons": [[]],
-    },
-  ];
-  const carousel = {
-    "type": "template",
-    "template": {
-      "type": "carousel",
-      "columns": messages,
-    },
-  };
-  await conn.sendMessage(m.chat, carousel, { quoted: m });
+if (!text) return conn.reply(m.chat, `*💛 Uso Correcto: ${usedPrefix + command} Nakano Nino*`, m, rcanal);
+conn.reply(m.chat, '💛 *Descargando su imagen...*', m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: wm,
+previewType: 0, thumbnail: icono,
+sourceUrl: canal }}})
+const res = await googleImage(text);
+const image = await res.getRandom();
+const link = image;
+const messages = [['Imagen 1', dev, await res.getRandom(),
+[[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 4', dev, await res.getRandom(), [[]], [[]], [[]], [[]]]]
+await conn.sendCarousel(m.chat, `💛 Resultado de ${text}`, '🔎 Imagen - Descargas', null, messages, m);
 };
 handler.help = ['imagen <query>'];
 handler.tags = ['buscador', 'tools', 'descargas'];
