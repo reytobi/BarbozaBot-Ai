d6b1f1f1d54bbf0972f5b71729b522a5bba3b768
Niño Piña <ninopina10@gmail.com>
2024-12-13 14:58:36 -0600

    Actualizar buscador-playstore.js

--- plugins/buscador-playstore.js
+++ plugins/buscador-playstore.js
@@ -1,14 +1,9 @@
 import gplay from "google-play-scraper";
 
 let handler = async (m, { conn, text }) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
-  const tradutor = _translate.plugins.buscador_playstore
-
-  if (!text) throw `*${tradutor.texto1}*`;
+  if (!text) throw "*[❗] Ingresa el nombre de app que quieres busc6*";
   let res = await gplay.search({ term: text });
-  if (!res.length) throw `*${tradutor.texto2}*`;
+  if (!res.length) throw `*[❗] Por favor ingresa el nombre de una app de la play store*`;
   let opt = {
     contextInfo: {
       externalAdReply: {
@@ -22,15 +17,15 @@ let handler = async (m, { conn, text }) => {
   await console.log(res);
   res = res.map(
     (v) =>
-      `${tradutor.texto3[0]} ${v.title}
-      ${tradutor.texto3[1]} ${v.developer}
-      ${tradutor.texto3[2]} ${v.priceText}
-      ${tradutor.texto3[3]} ${v.scoreText}
-      ${tradutor.texto3[4]}${v.url}`
+      `*🔍 Resultado:* ${v.title}
+       *✍️ Desarrollador:* ${v.developer}
+       *💸 Precio:* ${v.priceText}
+       *📈 Puntuacion:* ${v.scoreText}
+        *⛓️ Link:* ${v.url}`
   ).join`\n\n`;
   m.reply(res, null, opt);
 };
-handler.help = ['playstore <aplicacion>'];
-handler.tags = ['internet'];
+handler.help = ['playstore'];
+handler.tags = ['dl'];
 handler.command = /^(playstore)$/i;
 export default handler;
