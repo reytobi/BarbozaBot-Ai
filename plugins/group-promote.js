307449d3be1cc78682f42197898d298576d78fd8
Niño Piña <ninopina10@gmail.com>
2024-12-12 22:00:13 -0600

    Actualizar group-promote.js

--- plugins/group-promote.js
+++ plugins/group-promote.js
@@ -1,33 +1,42 @@
-let handler = async (m, { conn,usedPrefix, command, text}) => {
-if(isNaN(text) && !text.match(/@/g)){
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+    let number;
+    if (isNaN(text) && !text.match(/@/g)) {
+        return conn.reply(m.chat, `🚩 Menciona a una persona.`, m, rcanal);
+    } else if (isNaN(text)) {
+        number = text.split`@`[1];
+    } else {
+        number = text;
+    }
 
-}else if(isNaN(text)) {
-var number = text.split`@`[1]
-}else if(!isNaN(text)) {
-var number = text
+    if (!text && !m.quoted) return conn.reply(m.chat, `🚩 Menciona a una persona.`, m, rcanal);
+    if (number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `🚩 Menciona a una persona.`, m, rcanal);
+
+    try {
+        let user;
+        if (text) {
+            user = number + '@s.whatsapp.net';
+        } else if (m.quoted && m.quoted.sender) {
+            user = m.quoted.sender;
+        } else if (m.mentionedJidList.length > 0) {
+            user = m.mentionedJidList[0]; // Asegúrate de que haya al menos un mencionado
+        }
+
+        if (!user) return conn.reply(m.chat, `🚩 Menciona a una persona válida.`, m, rcanal);
+
+        await conn.groupParticipantsUpdate(m.chat, [user], 'promote');
+        await conn.reply(m.chat, `*[ ☃️ ] @${user.split('@')[0]} Fue promovido a administrador.*`, m, rcanal);
+        await m.react('✅');
+    } catch (e) {
+        console.error(e); // Registra el error
+    }
 }
-if(!text && !m.quoted) return conn.reply(m.chat, `🚩 Menciona a una persona.`, m, rcanal)
-if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `🚩 Menciona a una persona.`, m, rcanal)
-try {
-if(text) {
-var user = number + '@s.whatsapp.net'
-} else if(m.quoted.sender) {
-var user = m.quoted.sender
-} else if(m.mentionedJid) {
-var user = number + '@s.whatsapp.net'
-} 
-} catch (e) {
-} finally {
-conn.groupParticipantsUpdate(m.chat, [user], 'promote')
-await conn.reply(m.chat, `*[ ☃️ ] @⁨{user.split('@')[0]} Fue promovido a administrador.*`, m, rcanal)
-await m.react('✅')
-}}
-handler.help = ['promote *@user*']
-handler.tags = ['group']
-handler.command = ['promote', 'promover', 'daradmin'] 
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
-handler.fail = null
 
-export default handler
+handler.help = ['promote *@user*'];
+handler.tags = ['group'];
+handler.command = ['promote', 'promover', 'daradmin'];
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
+handler.fail = null;
+
+export default handler;
