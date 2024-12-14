9d49d47091ef519b6611af8779424ea4d9ce2cb0
Elrebelde1 <sebastianbarbaro82@gmail.com>
2024-12-13 22:53:03 -0400

    Actualizar economy-cf.js

--- plugins/economy-cf.js
+++ plugins/economy-cf.js
@@ -3,24 +3,24 @@ let users = {};
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     let [eleccion, cantidad] = text.split(' ');
     if (!eleccion || !cantidad) {
-        return m.reply(`🐉 Por favor, elige cara o cruz y una cantidad de dragones para apostar.\nEjemplo: *${usedPrefix + command} cara 50*`);
+        return m.reply(`💵 Por favor, elige cara o cruz y una cantidad de dragones para apostar.\nEjemplo: *${usedPrefix + command} cara 50*`);
     }
 
     eleccion = eleccion.toLowerCase();
     cantidad = parseInt(cantidad);
     if (eleccion !== 'cara' && eleccion !== 'cruz') {
-        return m.reply(`🐉 Elección no válida. Por favor, elige cara o cruz.\nEjemplo: *${usedPrefix + command} cara*`);
+        return m.reply(`💵 Elección no válida. Por favor, elige cara o cruz.\nEjemplo: *${usedPrefix + command} cara*`);
     }
 
     if (isNaN(cantidad) || cantidad <= 0) {
-        return m.reply(`🐉 Cantidad no válida. Por favor, elige una cantidad de dragones para apostar.\nEjemplo: *${usedPrefix + command} cara 50*`);
+        return m.reply(`💵 Cantidad no válida. Por favor, elige una cantidad de dragones para apostar.\nEjemplo: *${usedPrefix + command} cara 50*`);
     }
 
     let userId = m.sender;
     if (!users[userId]) users[userId] = { dragones: 100 };
     let user = global.db.data.users[m.sender];
     if (user.dragones < cantidad) {
-        return m.reply(`🐉 No tienes suficientes dragones para apostar. Tienes ${user.dragones} dragones.`);
+        return m.reply(`💵 No tienes suficientes dragones para apostar. Tienes ${user.dragones} dragones.`);
     }
 
     let resultado = Math.random() < 0.5 ? 'cara' : 'cruz';
