611e635fac773cb95346fec6eceddf90243677da
Elrebelde1 <sebastianbarbaro82@gmail.com>
2024-12-14 00:25:27 -0400

    Actualizar grupo-linea.js

--- plugins/grupo-linea.js
+++ plugins/grupo-linea.js
@@ -26,7 +26,7 @@ let handler = async (m, { conn, args }) => {
       m.chat,
       responseImg.data,
       "thumbnail.png",
-      `*🌐 Lista de usuarios en línea ahora ♡:*\n${listaEnLinea}\n\n\`ৎ୭࠭͢𝐊𝐚𝐤𝐚𝐫𝐨𝐭𝐨-𝐁𝐨𝐭-𝐌𝐃ⷭ𓆪͟͞ \``,
+      `*🌐 Lista de usuarios en línea ahora ♡:*\n${listaEnLinea}\n\n\`☃️bot Barboza-Ai By Barboza☃️
       m,
       {
         contextInfo: { mentionedJid: participantesOrdenados },
