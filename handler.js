e6ad108686925e173721e9545fdd9d7a3eb8bad9
Niño Piña <ninopina10@gmail.com>
2024-12-14 10:10:46 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -419,7 +419,7 @@ global.dfail = (type, m, conn, usedPrefix) => {
         private: "> _*`Ve a mi chat privado y usa este comando`*_",
         admin: "> _*`Quien eres?, tu no eres admin`*_",
         botAdmin: "> _*`Es necesario que sea admin primero para usar esta función`*_",
-        unreg: `> _*‼️USUARIO NO REGISTRADO‼️`*_\n\n`Para usar el comando  ${usedPrefix + command}registrese:\n\n> .reg nombre.edad\n\n`Ejemplo:`\n\n> .reg Barboza. 18`,
+        unreg: `> _*‼️USUARIO NO REGISTRADO‼️`*_\n\n`Para usar el comando  ${usedPrefix + command} registrese:\n\n> .reg nombre.edad\n\n`Ejemplo:`\n\n> .reg Barboza. 18`,
         restrict: "> _*`Comando desactivado por mi Owner`*_" 
     }[type]
     if (msg) return conn.reply(m.chat, msg, m, rcanal).then(_ => m.react('✖️'))
