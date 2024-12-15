0e385ffe5c8be5f807e44b8c40bb9b83d222422d
Niño Piña <ninopina10@gmail.com>
2024-12-14 20:51:05 -0600

    Actualizar group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -1,22 +1,33 @@
-const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command, usedPrefix}) => {
+const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command}) => {
 
-  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+let chat = global.db.data.chats[m.chat]
+let emoji = '';
+if (chat.emojiTag) {
+emoji = chat.emojiTag;
+} else {
+emoji = '🔱';
+}
 
   if (!(isAdmin || isOwner)) {
     global.dfail('admin', m, conn);
     throw false;
+    var sum = member.length;
+  } else {
+    var sum = 0;
+    const total = 0;
+    var member = 0;
   }
   const pesan = args.join` `;
-const oi = `👤 *MENSAJE :* ${pesan}`;
-  let teks = `✉️ *"MENCION GENERAL"*\n *MIEMBROS:* ${participants.length} 🎄\n\n ${oi}\n\n╭•┈┈•┈┈⊰🔥⃪᜔꫶┈•┈┈•◌ᜓ ݊ ᜒ𝅄\n`;
+  const oi = `${pesan}`;
+  let teks = `*BARBOZA-BOT-AI*\n\n *Integrantes :  ${participants.length}* ${oi}\n\n┌──⭓ activense ✨\n`;
   for (const mem of participants) {
-    teks += `│ ❄️. @${mem.id.split('@')[0]}\n`;
+    teks += `${emoji} @${mem.id.split('@')[0]}\n`;
   }
-  teks += `*${textbot}*
-        ₊˚꒷✧︶︶꒷︶︶꒷꒦︶✧꒷₊˚`;
+  teks += `└───────⭓`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
+
 };
-handler.help = ['todos <mesaje>'];
+handler.help = ['todos'];
 handler.tags = ['group'];
 handler.command = /^(tagall|invocar|marcar|todos|invocación)$/i;
 handler.admin = true;
