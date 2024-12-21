d2f85b5a83361ed800cb939ec290e70f9a3ad0c9
Elrebelde1 <sebastianbarbaro82@gmail.com>
2024-12-21 19:06:43 -0400

    Actualizar plugins

---
+++ plugins/owner-restart.js
@@ -0,0 +1,18 @@
+import { spawn } from 'child_process'
+let handler = async (m, { conn, isROwner, text }) => {
+    if (!process.send) return m.react('✖️')
+    if (conn.user.jid == conn.user.jid) {
+    await m.reply('*🧑‍💻 REINICIANDO BOTBARBOZA-AI☃️
+
+BY Barboza*')
+    process.send('reset')
+  } else return m.react('✖️')
+}
+
+handler.help = ['restart']
+handler.tags = ['owner']
+handler.command = ['restart','reiniciar'] 
+
+handler.rowner = true
+
+export default handler
