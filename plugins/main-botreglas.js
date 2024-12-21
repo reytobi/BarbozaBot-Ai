582e2df84013646b458a7faf43a56c64e4bfa5d6
Elrebelde1 <sebastianbarbaro82@gmail.com>
2024-12-21 12:11:20 -0400

    Actualizar main-botreglas.js

--- plugins/main-botreglas.js
+++ plugins/main-botreglas.js
@@ -2,15 +2,15 @@ let handler = async (m, { conn, usedPrefix, command}) => {
 
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
-let yaemori = `🌟 *Respeta las reglas de BotBarboza-Ai-Bot*\n
+let yaemori = `☃️ *Respeta las reglas de BotBarboza-Ai-Bot*\n
 ✰ No llamar Al Bot
 ✰ No hacer spam
 ✰ Contacta al creador si es necesario
 ✰ Pedir permiso para añadir al bot a un grupo
 
-✨ 𝗡𝗼𝘁𝗮: *Si no cumples con las reglas del bot, seras bloqueado.*
+☃️ 𝗡𝗼𝘁𝗮: *Si no cumples con las reglas del bot, seras bloqueado.*
 
-⚠️ 𝗔𝘃𝗶𝘀𝗼: *Puedes apoyarnos dejando una estrellita 🌟 al repositorio oficial de BotBarboza-Ai.*
+⚠️ 𝗔𝘃𝗶𝘀𝗼: *Puedes apoyarnos dejando una estrellita ☃️ al repositorio oficial de BotBarboza-Ai.*
 
 ${global.md}`.trim()
 await conn.reply(m.chat, yaemori, m, fake)
