83f130d22e9b57edc1a20c2e81ab212076e45b3e
Sisked <anuarmazenett@gmail.com>
2024-12-21 20:17:38 -0400

    Update serbot-bots.js

--- plugins/serbot-bots.js
+++ plugins/serbot-bots.js
@@ -1,26 +1,36 @@
-import ws from 'ws'
+import ws from 'ws';
 
 async function handler(m, { conn: stars, usedPrefix }) {
-  let uniqueUsers = new Map()
+  let uniqueUsers = new Map();
 
   global.conns.forEach((conn) => {
-    if (conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED) {
-      uniqueUsers.set(conn.user.jid, conn)
+    if (conn && conn.user && conn.ws && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED) {
+      uniqueUsers.set(conn.user.jid, conn);
     }
-  })
+  });
 
-  let users = [...uniqueUsers.values()]
+  let users = [...uniqueUsers.values()];
 
-  let message = users.map((v, index) => `*${index + 1}.-* @${v.user.jid.replace(/[^0-9]/g, '')}\n*Link:* https://wa.me/${v.user.jid.replace(/[^0-9]/g, '')}\n*Nombre:* ${v.user.name || '-'}`).join('\n\n')
-  
-  let replyMessage = message.length === 0 ? '' : message
-  let totalUsers = users.length
-  let responseMessage = `*Total de Bots* : ${totalUsers || '0'}\n\n${replyMessage.trim()}`.trim()
+  let message = users
+    .map((v, index) => {
+      let jid = v.user?.jid || '-';
+      let name = v.user?.name || '-';
+      return `*${index + 1}.-* @${jid.replace(/[^0-9]/g, '')}\n*Link:* https://wa.me/${jid.replace(/[^0-9]/g, '')}\n*Nombre:* ${name}`;
+    })
+    .join('\n\n');
 
-  await stars.sendMessage(m.chat, { text: responseMessage, mentions: stars.parseMention(responseMessage) }, { quoted: m })
+  let replyMessage = message.length === 0 ? '' : message;
+  let totalUsers = users.length;
+  let responseMessage = `*Total de Bots* : ${totalUsers || '0'}\n\n${replyMessage.trim()}`.trim();
+
+  await stars.sendMessage(
+    m.chat,
+    { text: responseMessage, mentions: stars.parseMention(responseMessage) },
+    { quoted: m }
+  );
 }
 
-handler.command = ['listjadibot', 'bots']
-handler.help = ['bots']
-handler.tags = ['serbot']
-export default handler
+handler.command = ['listjadibot', 'bots'];
+handler.help = ['bots'];
+handler.tags = ['serbot'];
+export default handler;
