7c67db5b685c3109b1674df243c7a37832d8efb0
Elrebelde1 <sebastianbarbaro82@gmail.com>
2024-12-25 12:49:05 -0400

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -5,7 +5,7 @@ handler.all = async function(m) {
 const chat = global.db.data.chats[m.chat];
 if (chat.isBaneed) return
 if (/^bot$/i.test(m.text)) {
-conn.reply(m.chat, `🌟 ¡Hola! Soy Sisked, en que puedo ayudarte hoy?\n\n✰ Usa *.menu* para ver mis comandos.`, m, rcanal, )
+conn.reply(m.chat, `🌟 ¡Hola! Soy BotBarboza-Ai, en que puedo ayudarte hoy?\n\n✰ Usa *.menu* para ver mis comandos.`, m, rcanal, )
 }
 if (/^que|q$/i.test(m.text)) {
 conn.reply(m.chat, `*so*`, m, rcanal, )
@@ -16,23 +16,23 @@ conn.reply(m.chat, `*sua*`, m, rcanal, )
 }
 
 if (/^.bermuda/i.test(m.text)) {
-conn.reply(m.chat, `*Mapa Eligido Bermuda 🐉*`, m, rcanal, )
+conn.reply(m.chat, `*Mapa Eligido Bermuda ☃️*`, m, rcanal, )
 }
 
 if (/^.alpes/i.test(m.text)) {
-conn.reply(m.chat, `*Mapa Eligido Alpes 🐉*`, m, rcanal, )
+conn.reply(m.chat, `*Mapa Eligido Alpes ☃️*`, m, rcanal, )
 }
 
 if (/^.purgatorio/i.test(m.text)) {
-conn.reply(m.chat, `*Mapa Eligido Purgatorio 🐉*`, m, rcanal, )
+conn.reply(m.chat, `*Mapa Eligido Purgatorio ☃️*`, m, rcanal, )
 }
 
 if (/^.kalahari$/i.test(m.text)) {
-conn.reply(m.chat, `*Mapa Eligido Kalahari 🐉*`, m, rcanal, )
+conn.reply(m.chat, `*Mapa Eligido Kalahari ☃️*`, m, rcanal, )
 }
 
 if (/^.nexterra$/i.test(m.text)) {
-conn.reply(m.chat, `*Mapa Eligido NexTerra 🐉*`, m, rcanal, )
+conn.reply(m.chat, `*Mapa Eligido NexTerra ☃️*`, m, rcanal, )
 }
 
 if (/^.menuff$/i.test(m.text)) {
