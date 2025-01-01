64b362efda9603b03266358b0fb0a8e4c5a379b0
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-01 17:28:26 -0400

    Actualizar barbozx-ytmp4.js

--- plugins/barbozx-ytmp4.js
+++ plugins/barbozx-ytmp4.js
@@ -1,36 +1,24 @@
-import fetch from 'node-fetch'
+import Scraper from "@SumiFX/Scraper"
 
-let handler = async (m, { conn, text }) => {
-  if (!text) {
-    return m.reply("🤍 Por favor, ingresa una URL válida de YouTube.")
-  }
-    await m.react('🕓')
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) return m.reply('🍭 Ingresa el enlace del vídeo de YouTube junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://youtu.be/QSvaCSt8ixs`)
+if (!args[0].match(/youtu/gi)) return conn.reply(m.chat, `Verifica que el enlace sea de YouTube.`, m)
 
-  let ytUrlRegex = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/.+$/;
-  if (!ytUrlRegex.test(text)) {
-    return m.reply("❀ La URL ingresada no es válida. Asegúrate de que sea un enlace de YouTube.")
-  }
-
-  try {
-    let api = await fetch(`https://api.giftedtech.my.id/api/download/dlmp4?apikey=gifted&url=${text}`)
-    let json = await api.json()
-    let { quality, title, download_url } = json.result
-
-    await m.react('✅')
-    await conn.sendMessage(m.chat, { 
-      video: { url: download_url }, 
-      caption: `_${title}_`, 
-      mimetype: 'video/mp4', 
-      fileName: `${title}.mp4` 
-    }, { quoted: m })
-  } catch (error) {
-    console.error(error)
-    m.reply("❀ Hubo un error al procesar la URL. Inténtalo nuevamente.")
-  }
-}
-
-handler.help = ['ytmp4 *<link yt>*']
-handler.tags = ['dl']
-handler.command = ['ytmp4', 'ytv', 'fgmp4']
-
+let user = global.db.data.users[m.sender]
+try {
+let { title, size, quality, thumbnail, dl_url } = await Scraper.ytmp4(args[0])
+if (size.includes('GB') || size.replace(' MB', '') > 300) { return await m.reply('El archivo pesa mas de 300 MB, se canceló la Descarga.')}
+let txt = `╭─⬣「 *YouTube Download* 」⬣\n`
+    txt += `│  ≡◦ *⭐ Titulo ∙* ${title}\n`
+    txt += `│  ≡◦ *🪴 Calidad ∙* ${quality}\n`
+    txt += `│  ≡◦ *⚖ Peso ∙* ${size}\n`
+    txt += `╰─⬣`
+await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', txt, m)
+await conn.sendFile(m.chat, dl_url, title + '.mp4', `*⭐ Titulo ∙* ${title}\n*🪴 Calidad ∙* ${quality}`, m, false, { asDocument: user.useDocument })
+} catch {
+}}
+handler.help = ['ytmp4 <yt url>']
+handler.tags = ['downloader']
+handler.command = ['ytmp4', 'yt', 'ytv'] 
+//handler.limit = 1
 export default handler
