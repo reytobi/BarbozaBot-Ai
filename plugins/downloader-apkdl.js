2278068186949934394fc8cecdb9c87a3c18610a
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-02 16:34:25 -0400

    Actualizar downloader-apkdl.js

--- plugins/downloader-apkdl.js
+++ plugins/downloader-apkdl.js
@@ -1,25 +1,29 @@
 let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
-if (!args[0]) throw `\`\`\`[ 🌟 ] Ingresa el nombre de la aplicación que quieres descargar. Ejemplo:\n${usedPrefix + command} Koruda\`\`\``
+if (!args[0]) throw `\`\`\`[ 🌟 ] Ingresa el nombre de la aplicación que quieres descargar. Ejemplo:\n${usedPrefix + command} Clash Royale\`\`\``
 let res = await fetch(`https://api.dorratz.com/v2/apk-dl?text=${args[0]}`);
 let result = await res.json();
 let { name, size, lastUpdate, icon } = result;
 let URL = result.dllink
 let packe = result.package
-let texto = `  ❯───「 𝗔𝗣𝗞 𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗 」───❮
-    🇷🇺 𝐍𝐨𝐦𝐛𝐫𝐞 : ⇢ ${name} 📩
-    🇷🇺 𝐓𝐚𝐦𝐚𝐧̃𝐨 : ⇢ ${size} ⚖️
-    🇷🇺 𝐏𝐚𝐜𝐤𝐚𝐠𝐞 : ⇢ ${packe} 📦
-    🇷🇺 𝐀𝐜𝐭𝐮𝐚𝐥𝐢𝐳𝐚𝐝𝐨 : ⇢ ${lastUpdate} 🗓️
-    
-## Su aplicación se enviará en un momento POR FAVOR ESPERE . . .`
+let texto = ` \`\`\`
+   ❯───「 𝗔𝗣𝗞 𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗 」───❮
+    ✦ 𝐍𝐨𝐦𝐛𝐫𝐞 : ⇢ ${name} 📛
+    ✦ 𝐓𝐚𝐦𝐚𝐧̃𝐨 : ⇢ ${size} ⚖️
+    ✦ 𝐏𝐚𝐜𝐤𝐚𝐠𝐞 : ⇢ ${packe} 📦
+    ✦ 𝐀𝐜𝐭𝐮𝐚𝐥𝐢𝐳𝐚𝐝𝐨 : ⇢ ${lastUpdate} 🗓️
+    ✦ 𝐂𝐫𝐞𝐚𝐝𝐨𝐫 :⇢  BotBarboza-Ai 
+\`\`\`
+> https://whatsapp.com/channel/0029Vaua0ZD3gvWjQaIpSy18
+
+## Su aplicación se enviará en un momento . . .
+
+${botname}              
+`
 await conn.sendFile(m.chat, icon, name + '.jpg', texto, m)
 
 await conn.sendMessage(m.chat, { document: { url: URL }, mimetype: 'application/vnd.android.package-archive', fileName: name + '.apk', caption: ''}, { quoted: m });
 }
+handler.command = ['apk', 'apkdl', 'modapk']
+handler.help = ['apkdl']
 handler.tags = ['downloader']
-handler.help = ['apkmod']
-handler.command = /^(apkmod|apk|dapk2|aptoide|aptoidedl)$/i
-handler.register = true
-handler.zenis = 10
-
 export default handler
