f1cfe8aa47a6133a007b9238566b20f0d9f47f53
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-02 17:18:21 -0400

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,23 +1,50 @@
-
 import {WAMessageStubType} from '@whiskeysockets/baileys'
 import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;
-  let img = imagen1
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://qu.ax/jYQH.jpg')
+  let img = await (await fetch(`${pp}`)).buffer()
   let chat = global.db.data.chats[m.chat]
 
-  if (chat.welcome && m.messageStubType == 27) {
-    let welcome = `┌─★ 𝐒𝐈𝐒𝐊𝐄𝐃 𝐁𝐎𝐓 𝐌𝐃  \n│「 𝗕𝗶𝗲𝗻𝘃𝗲𝗻𝗶𝗱𝗼 ⭐ 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │⭐  𝗕𝗶𝗲𝗻𝘃𝗲𝗻𝗶𝗱𝗼/𝗮\n   │⭐  ${groupMetadata.subject}\n   └───────────────┈ ⳹`
-await conn.sendLuffy(m.chat, packname, textbot, welcome, img, img, redes, fkontak)
+  if (chat.bienvenida && m.messageStubType == 27) {
+    if (chat.sWelcome) {
+      let user = `@${m.messageStubParameters[0].split`@`[0]}`
+      let welcome = chat.sWelcome
+        .replace('@user', () => user)
+        .replace('@group', () => groupMetadata.subject)
+        .replace('@desc', () => groupMetadata.desc || 'sin descripción');
+      await conn.sendAi(m.chat, botname, textbot, welcome, img, img, canal)
+    } else {
+      let bienvenida = `┌─★ 𝙊𝙉𝙔𝙓 𝘽𝙊𝙏  \n│「 Bienvenido 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Bienvenido a\n   │✑  ${groupMetadata.subject}\n   │✑  Descripción:\n${groupMetadata.desc || 'sin descripción'}\n   └───────────────┈ ⳹`
+      await conn.sendAi(m.chat, botname, textbot, bienvenida, img, img)
+    }
   }
 
-  if (chat.welcome && m.messageStubType == 28) {
-    let bye = `┌─★ 𝐒𝐈𝐒𝐊𝐄𝐃 𝐁𝐎𝐓 𝐌𝐃  \n│「 𝗔𝗗𝗜𝗢𝗦 ⭐ 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │⭐  𝗦𝗲 𝗳𝘂𝗲\n   │⭐ 𝗡𝘂𝗻𝗰𝗮 𝘁𝗲 𝗾𝘂𝗶𝘀𝗶𝗺𝗼𝘀 𝗮𝗾𝘂𝗶\n   └───────────────┈ ⳹`
-await conn.sendLuffy(m.chat, packname, textbot, bye, img, img, redes, fkontak)
+  if (chat.bienvenida && m.messageStubType == 28) {
+    if (chat.sBye) {
+      let user = `@${m.messageStubParameters[0].split`@`[0]}`
+      let bye = chat.sBye
+        .replace('@user', () => user)
+        .replace('@group', () => groupMetadata.subject)
+        .replace('@desc', () => groupMetadata.desc || 'sin descripción');
+      await conn.sendAi(m.chat, botname, textbot, bye, img, img)
+    } else {
+      let bye = `┌─★ 𝙊𝙉𝙔𝙓 𝘽𝙊𝙏  \n│「 BAYY 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Largate\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
+      await conn.sendAi(m.chat, botname, textbot, bye, img, img)
+    }
   }
 
-  if (chat.welcome && m.messageStubType == 32) {
-    let kick = `┌─★ 𝐒𝐈𝐒𝐊𝐄𝐃 𝐁𝐎𝐓 𝐌𝐃  \n│「 𝗔𝗗𝗜𝗢𝗦 ⭐ 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │⭐  𝗦𝗲 𝗳𝘂𝗲\n   │⭐ 𝗡𝘂𝗻𝗰𝗮 𝘁𝗲 𝗾𝘂𝗶𝘀𝗶𝗺𝗼𝘀 𝗮𝗾𝘂𝗶\n   └───────────────┈ ⳹`
-await conn.sendLuffy(m.chat, packname, textbot, kick, img, img, redes, fkontak)
+  if (chat.bienvenida && m.messageStubType == 32) {
+    if (chat.sBye) {
+      let user = `@${m.messageStubParameters[0].split`@`[0]}`
+      let bye = chat.sBye
+        .replace('@user', () => user)
+        .replace('@group', () => groupMetadata.subject)
+        .replace('@desc', () => groupMetadata.desc || 'sin descripción');
+      await conn.sendAi(m.chat, botname, textbot, bye, img, img)
+    } else {
+      let kick = `┌─★ 𝙊𝙉𝙔𝙓 𝘽𝙊𝙏  \n│「 BAYY 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Largate\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
+      await conn.sendAi(m.chat, botname, textbot, kick, img, img)
+    }
 }}
