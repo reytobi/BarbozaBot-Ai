771f168c86482708138f5791ed9b5253adfb00d7
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-02 16:47:49 -0400

    Actualizar downloader-apkdl.js

--- plugins/downloader-apkdl.js
+++ plugins/downloader-apkdl.js
@@ -1,34 +1,25 @@
-import { search, download } from 'aptoide-scraper'
+import {search, download} from 'aptoide-scraper';
 
-var handler = async (m, {conn, usedPrefix, command, text}) => {
-if (!text) return conn.reply(m.chat, '🌸 *Iɳɠɾҽʂҽ ҽʅ ɳσɱზɾҽ ԃҽ ʅα αρƙ ραɾα ԃҽʂƈαɾɠαɾʅσ.*', m, rcanal)
-try {
-await m.react(rwait)
-conn.reply(m.chat, '🌸 *Eʂρҽɾҽ υɳ ɱσɱҽɳƚσ...*', m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: dev,
-previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})
-let searchA = await search(text)
-let data5 = await download(searchA[0].id)
-let txt = `*DESCARGAS MOD APK* 乂\n\n`
-txt += `🌙 *Nombre* : ${data5.name}\n`
-txt += `🌙 *Package* : ${data5.package}\n`
-txt += `🌙 *Update* : ${data5.lastup}\n`
-txt += `🌙 *Peso* :  ${data5.size}`
-await conn.sendFile(m.chat, data5.icon, 'thumbnail.jpg', txt, m, null, rcanal) 
-await m.react(done)  
-if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
-return await conn.reply(m.chat, '☁ *El archivo es demaciado pesado*', m, rcanal )}
-await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: fkontak})
-} catch {
-return conn.reply(m.chat, '☁ *Ocurrió un fallo*', m, rcanal )}}
+const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const tradutor = _translate.plugins.downloader_modapk
 
-handler.tags = ['descargas']
-handler.help = ['apkmod']
-handler.command = ['apk', 'modapk', 'aptoide']
-handler.register = true
-handler.estrellas = 1
 
-export default handler
+ if (!text) throw `${tradutor.texto1}`;
+  try {    
+    const searchA = await search(text);
+    const data5 = await download(searchA[0].id);
+    let response = `${tradutor.texto2[0]} ${data5.name}\n${tradutor.texto2[1]}* ${data5.package}\n${tradutor.texto2[2]} ${data5.lastup}\n${tradutor.texto2[3]} ${data5.size}`
+    await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m});
+ if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
+      return await conn.sendMessage(m.chat, {text: `${tradutor.texto3}`}, {quoted: m});
+    }
+    await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m});
+  } catch {
+    throw `${tradutor.texto4}`;
+  }    
+};
+handler.command = /^(apkmod|modapk|dapk2|aptoide|aptoidedl)$/i;
+export default handler;
