3ba8ea9d607cebd89a403b4c50ca3a0d77be86cd
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-03 23:53:02 -0400

    Actualizar tools-barbzx.js

--- plugins/tools-barbzx.js
+++ plugins/tools-barbzx.js
@@ -1,22 +1,30 @@
-// Código Creado Por Barboza 
-
+// Código Creado  TECNO Wa.me/50488198573
 import fetch from 'node-fetch';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-if (!text) throw `*👻 ingrese un texto para generar poder generar su imagen*`;
+// Verificamos que el usuario haya ingresado un texto
+if (!text) throw `*🧑‍💻 Ingresa un texto para generar tu imagen a tu gusto*`;
+// Mostramos un emoji de reloj mientras generamos la imagen
 m.react('🕒');
-await conn.sendMessage(m.chat, {text: '*👻 Generando Imagen*'}, {quoted: m});
+await conn.sendMessage(m.chat, {text: '*🧑‍💻 Espere, Estamos Trabajando en su imagen*'}, {quoted: m});
 try {
-const response = await fetch(`https://eliasar-yt-api.vercel.app/api/canvas/logo?texto=${encodeURIComponent(text)}&url=${encodeURIComponent(profilePicture)}`);
+// Hacemos la solicitud a la API con el texto proporcionado
+const response = await fetch(`https://eliasar-yt-api.vercel.app/api/ai/text2img?prompt=${encodeURIComponent(text)}`);
+// Verificamos si la respuesta fue exitosa
 if (!response.ok) throw new Error('Network response was not ok');
+// Obtenemos el buffer de la imagen
 const buffer = await response.buffer();
+// Mostramos un emoji de éxito
 m.react('✔️');
+// Enviamos la imagen generada al chat
 await conn.sendMessage(m.chat, {image: buffer}, {quoted: m});
 } catch (error) {
 console.error(error);
-throw `*🚨 Lo Sentimos, ha ocurrido un error 😔*`;
+throw `*🚨 Lo sentimos, ha ocurrido un error 😔*`;
 }
 }
+// Definimos las etiquetas y comandos para el handler
 handler.tags = ['tools'];
-handler.help = ['genearimg <nombre> <link de la imagen>'];
-handler.command = ['genearimg','imgg'];
+handler.help = ['genearimg'];
+handler.command = ['genearimg2', 'imgg2'];
+// Exportamos el handler
 export default handler;
