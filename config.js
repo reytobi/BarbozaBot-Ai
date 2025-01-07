571ecb7dc20973642eac6c0319e7ba79c838ad15
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-07 19:45:56 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -11,6 +11,7 @@ import axios from 'axios'
 global.owner = [
   ['584246582666', 'BarbozaBot-Ai', 
 true],
+  ['584146277368', 'Barboza OFC',true],
 ]
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
