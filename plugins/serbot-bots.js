0a747b9c6a8d00cd45cde83b1feb6b04515f25ed
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-07 22:02:45 -0400

    Actualizar serbot-bots.js

--- plugins/serbot-bots.js
+++ plugins/serbot-bots.js
@@ -1,36 +1,67 @@
 import ws from 'ws';
 
-async function handler(m, { conn: stars, usedPrefix }) {
-  let uniqueUsers = new Map();
+async function handler(m, { usedPrefix }) {
 
-  global.conns.forEach((conn) => {
-    if (conn && conn.user && conn.ws && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED) {
-      uniqueUsers.set(conn.user.jid, conn);
-    }
-  });
+  const users = [...new Set([...global.conns
+    .filter((conn) => conn && conn.user && conn.ws && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED)
+    .map((conn) => conn)
+  ])];
 
-  let users = [...uniqueUsers.values()];
 
-  let message = users
-    .map((v, index) => {
-      let jid = v.user?.jid || '-';
-      let name = v.user?.name || '-';
-      return `*${index + 1}.-* @${jid.replace(/[^0-9]/g, '')}\n*Link:* https://wa.me/${jid.replace(/[^0-9]/g, '')}\n*Nombre:* ${name}`;
-    })
-    .join('\n\n');
+  function dhms(ms) {
+    const segundos = Math.floor(ms / 1000) % 60;
+    const minutos = Math.floor(ms / (1000 * 60)) % 60;
+    const horas = Math.floor(ms / (1000 * 60 * 60)) % 24;
+    const días = Math.floor(ms / (1000 * 60 * 60 * 24));
 
-  let replyMessage = message.length === 0 ? '' : message;
-  let totalUsers = users.length;
-  let responseMessage = `*Total de Bots* : ${totalUsers || '0'}\n\n${replyMessage.trim()}`.trim();
+    const resultado = [];
+    if (días) resultado.push(`${días}d`);
+    if (horas) resultado.push(`${horas}h`);
+    if (minutos) resultado.push(`${minutos}m`);
+    if (segundos) resultado.push(`${segundos}s`);
 
-  await stars.sendMessage(
+    return resultado.join(' ');
+  }
+
+  // Generar mensaje de respuesta
+  const message = users.map((v, index) => {
+    if (!v || !v.user || !v.user.jid) return ''; 
+    const userJid = v.user.jid.replace(/[^0-9]/g, '');
+    const userName = v.user.name || '-';
+    const uptime = v.uptime ? dhms(Date.now() - v.uptime) : 'Desconocido';
+
+    return `*\`🐉 SonGoku | Subbot: ${index + 1}\`* 
+*\`Tag:\` @${userJid}*
+*• wa.me/${userJid}?text=${usedPrefix}menu*
+*\`👥 Nombre: ${userName}\`*
+*\`⌛ Uptime: ${uptime}\`*`;
+  }).filter(Boolean).join('\n\n*─ ── ──  ── ── ── ── ──  ── ── ─*\n\n'); 
+
+  const replyMessage = message.length === 0 ? 'No hay bots activos.' : message;
+  const totalUsers = users.length;
+  const responseMessage = `
+*\`🐉Subbots activos de SonGoku🐉: ${totalUsers || '0'}\`*
+
+${replyMessage.trim()}
+
+_*\`Subots SonGoku~\`*_`.trim();
+
+
+  const gifUrl = 'https://i.ibb.co/hy3G9Rz/file.jpg'; 
+  await conn.sendMessage(
     m.chat,
-    { text: responseMessage, mentions: stars.parseMention(responseMessage) },
-    { quoted: m }
+    {
+      video: { url: gifUrl },
+      gifPlayback: true, 
+      caption: responseMessage,
+      mentions: conn.parseMention(responseMessage), 
+    },
+    { quoted: m } 
   );
 }
 
-handler.command = ['listjadibot', 'bots'];
-handler.help = ['bots'];
-handler.tags = ['serbot'];
+handler.help = ['botlist'];
+handler.tags = ['bebot'];
+handler.command = ['listbot', 'listbots', 'bots', 'bebots', 'botlist'];
+
 export default handler;
