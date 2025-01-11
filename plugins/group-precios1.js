c9770c910186201ed28b3a43a50200ed27613692
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-10 20:24:00 -0400

    Actualizar group-precios1.js

--- plugins/group-precios1.js
+++ plugins/group-precios1.js
@@ -14,5 +14,4 @@ await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: message }, {
 handler.help = ['precios1'];
 handler.tags = ['main'];
 handler.command = ['precios1', 'p1', 'precio1'];
-handler.group = true;
 export default handler;
