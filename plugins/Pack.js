6a5c4bb3fd67bd431351b884602fe51ee866068c
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-10 22:55:07 -0400

    Actualizar Pack.js

--- plugins/Pack.js
+++ plugins/Pack.js
@@ -41,6 +41,6 @@ let handler = async (m, { conn }) => {
 handler.help = ['pechos', 'tetas'];
 handler.tags = ['nsfw'];
 handler.command = ['pechos', 'tetas'];
-handler.group = true;
+
 
 export default handler;
