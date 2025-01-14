16b1afaa35530be5508ac1585494fc20cb5d84eb
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-13 21:24:18 -0400

    Actualizar plugins

---
+++ plugins/tools-menujuegos.js
@@ -0,0 +1,131 @@
+import fs, { promises } from 'fs'
+import fetch from 'node-fetch'
+let handler = async (m, { conn, usedPrefix, command }) => {
+try {
+let d = new Date(new Date + 3600000)
+let locale = 'es'
+let week = d.toLocaleDateString(locale, { weekday: 'long' })
+let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
+let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime)
+let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
+let more = String.fromCharCode(8206)
+let readMore = more.repeat(850)   
+let taguser = conn.getName(m.sender)
+let user = global.db.data.users[m.sender]
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+let menu = `
+¡Hola! 👋🏻 @${m.sender.split("@")[0]}
+ \`\`\`${week}, ${date}\`\`\`
+
+╭──𝗠𝗘𝗡𝗨 𝗝𝗨𝗘𝗚𝗢𝗦───
+│ 𝘉𝘪𝘦𝘯𝘷𝘦𝘯𝘪𝘥𝘰 ...
+│ 𝘋𝘪𝘷𝘪𝘦́𝘳𝘵𝘦𝘵𝘦 𝘤𝘰𝘯 𝘵𝘶𝘴 𝘢𝘮𝘪𝘨𝘰𝘴 
+│ 𝘤𝘰𝘯 𝘦𝘭 𝘤𝘢𝘵𝘢́𝘭𝘰𝘨𝘰 𝘫𝘶𝘦𝘨𝘰𝘴.
+╰────────────────
+» 𝙀𝙓𝙋 𝙅𝙐𝙀𝙂𝙊𝙎 ]━⬣
+┃➺ 👤.𝘣𝘢𝘯𝘤𝘰
+┃➺ ⚖️ .𝘣𝘢𝘭𝘢𝘯𝘤𝘦
+┃➺ ⚖️ .𝘣𝘢𝘭𝘢𝘯𝘤𝘦2
+┃➺ 💎 .𝘮𝘪𝘯𝘢𝘳
+┃➺ 🧨 .𝘤𝘭𝘢𝘪𝘮
+┃➺ 🔫 .𝘳𝘰𝘣𝘢𝘳 @𝘵𝘢𝘨
+┃➺ 🎁 .𝘤𝘰𝘧𝘳𝘦
+┃➺ 🛒 .𝘣𝘶𝘺 𝘤𝘢𝘯𝘵𝘪𝘥𝘢𝘥 
+┃➺ 💵 .𝘵𝘳𝘢𝘯𝘴𝘧𝘦𝘳
+┃➺ 🎰 .𝘢𝘱𝘰𝘴𝘵𝘢𝘳
+┃➺ 📉 .𝘵𝘳𝘢𝘣𝘢𝘫𝘢𝘳 
+┃➺ 💎 .𝘥𝘢𝘳𝘥𝘪𝘢𝘮𝘢𝘯𝘵𝘦𝘴 
+┃➺ 📈 .𝘥𝘢𝘳𝘹p
+╰━━━━━━⋆★⋆━━━━━━⬣
+
+» 𝗝𝗨𝗘𝗚𝗢𝗦 𝗧𝗘𝗫𝗧𝗢 
+┃🎲➺ .𝘴𝘰𝘱𝘢
+┃🎲➺ .𝘴𝘶𝘦𝘳𝘵𝘦
+┃🎲➺ .𝘳𝘦𝘵𝘰
+┃🎲➺ .𝘷𝘦𝘳𝘥𝘢𝘥
+┃🎲➺ .𝘢𝘤𝘦𝘳𝘵𝘪𝘫𝘰
+┃🎲➺ .𝘥𝘰𝘹𝘦𝘢𝘳 𝙩𝙖𝙜
+┃🎲➺ .𝘥𝘰𝘹𝘹𝘦𝘢𝘮𝘦
+┃🎲➺ .𝘥𝘢𝘥𝘰
+┃🎲➺ .𝘮𝘢𝘵𝘦𝘴
+┃🎲➺ .𝘱𝘦𝘭𝘦𝘢
+╰━━━━━━⋆★⋆━━━━━━⬣
+
+» 𝗝𝗨𝗘𝗚𝗢𝗦 𝗛𝗢𝗧 
+┃🔥➺ .𝘧𝘰𝘳𝘮𝘢𝘳𝘵𝘳𝘪𝘰 
+┃🔥➺ .𝘤𝘶𝘭𝘦𝘢𝘳 𝙩𝙖𝙜
+┃🔥➺ .𝘤𝘰𝘭𝘰𝘳𝘤𝘢𝘳𝘵𝘰𝘯 𝙩𝙖𝙜
+┃🔥➺ .𝘧𝘰𝘭𝘭𝘢𝘳 𝙩𝙖𝙜
+┃🔥➺ .𝘱𝘦𝘯𝘦𝘵𝘳𝘢𝘳 𝙩𝙖𝙜
+┃🔥➺ .𝘤𝘰𝘨𝘦𝘳 𝙩𝙖𝙜
+┃🔥➺ .𝘱𝘢𝘫𝘦𝘳𝘰 𝙩𝙖𝙜
+┃🔥➺ .𝘱𝘢𝘫𝘦𝘳𝘢 𝙩𝙖𝙜
+┃🔥➺ .𝘱𝘶𝘵𝘰 𝙩𝙖𝙜
+┃🔥➺ .𝘱𝘶𝘵𝘢 𝙩𝙖𝙜
+┃🔥➺ .𝘴𝘢𝘭𝘶𝘥𝘢𝘳 𝙩𝙖𝙜
+┃🔥➺ .𝘣𝘢𝘪𝘭𝘢𝘳 𝙩𝙖𝙜
+┃🔥➺ .𝘣𝘢𝘯̃𝘢𝘳 𝙩𝙖𝙜
+╰━━━━━━⋆★⋆━━━━━━⬣
+
+» 𝗝𝗨𝗘𝗚𝗢𝗦 𝗧𝗢𝗣
+┃📍➺ .𝘵𝘰𝘱 *𝘱𝘢𝘭𝘢𝘣𝘳𝘢𝘴 𝘳𝘢𝘯𝘥𝘰𝘮*
+┃📍➺ .𝘵𝘰𝘱𝘢𝘭𝘤𝘰𝘩𝘰𝘭𝘪𝘤𝘰𝘴
+┃📍➺ .𝘵𝘰𝘱𝘤𝘢𝘤𝘩𝘶𝘥𝘰𝘴
+┃📍➺ .𝘵𝘰𝘱𝘤𝘩𝘪𝘤𝘩𝘰𝘯𝘢𝘴
+┃📍➺ .𝘵𝘰𝘱𝘪𝘯𝘧𝘪𝘦𝘭𝘦𝘴
+┃📍➺ .𝘵𝘰𝘱𝘧𝘪𝘦𝘭𝘦𝘴
+┃📍➺ .𝘵𝘰𝘱𝘤𝘶𝘭𝘰𝘯𝘢𝘴
+┃📍➺ .𝘵𝘰𝘱𝘴𝘪𝘥𝘰𝘴𝘰𝘴
+┃📍➺ .𝘵𝘰𝘱𝘧𝘦𝘰𝘴
+┃📍➺ .𝘵𝘰𝘱𝘨𝘢𝘺𝘴
+┃📍➺ .𝘵𝘰𝘱𝘰𝘵𝘢𝘬𝘶𝘴
+┃📍➺ .𝘵𝘰𝘱𝘱𝘢𝘫𝘦𝘳@𝘴
+┃📍➺ .𝘵𝘰𝘱𝘱𝘶𝘵@𝘴
+┃📍➺ .𝘵𝘰𝘱𝘭𝘢𝘨𝘳𝘢𝘴𝘢
+┃📍➺ .𝘵𝘰𝘱𝘱𝘢𝘯𝘢𝘧𝘳𝘦𝘴𝘤𝘰𝘴
+┃📍➺ .𝘵𝘰𝘱𝘴𝘩𝘪𝘱𝘰𝘴𝘵𝘦𝘳𝘴
+┃📍➺ .𝘵𝘰𝘱𝘭𝘪𝘯𝘥𝘰𝘴
+┃📍➺ .𝘵𝘰𝘱𝘧𝘢𝘮𝘰𝘴𝘰𝘴
+┃📍➺ .𝘨𝘢𝘺 *@𝘵𝘢𝘨*
+┃📍➺ .𝘨𝘢𝘺2 *@𝘵𝘢𝘨*
+┃📍➺ .𝘭𝘦𝘴𝘣𝘪𝘢𝘯𝘢 *@𝘵𝘢𝘨*
+┃📍➺ .𝘳𝘢𝘵𝘢 *@𝘵𝘢𝘨*
+╰━━━━━━⋆★⋆━━━━━━⬣
+
+ 
+ `.trim()
+
+const vi = ['https://d.uguu.se/XOdDvUoj.mp4',
+'https://d.uguu.se/XOdDvUoj.mp4',
+'https://d.uguu.se/XOdDvUoj.mp4']
+
+try {
+await conn.sendMessage(m.chat, { video: { url: vi.getRandom() }, gifPlayback: true, caption: menu, mentions: [m.sender, global.conn.user.jid] }, { quoted: fkontak }) 
+} catch (error) {
+try {
+await conn.sendMessage(m.chat, { image: { url: gataMenu.getRandom() }, gifPlayback: false, caption: menu, mentions: [m.sender, global.conn.user.jid] }, { quoted: fkontak }) 
+} catch (error) {
+try {
+await conn.sendMessage(m.chat, { image: gataImg.getRandom(), gifPlayback: false, caption: menu, mentions: [m.sender, global.conn.user.jid] }, { quoted: fkontak }) 
+} catch (error) {
+try{
+await conn.sendFile(m.chat, imagen5, 'menu.jpg', menu, fkontak, false, { mentions: [m.sender, global.conn.user.jid] })
+} catch (error) {
+return 
+}}}} 
+} catch (e) {
+await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
+console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+console.log(e)}}
+
+handler.command = /^(menujuego|menujuegos|juegos)$/i
+handler.register = false
+handler.group = true
+export default handler
+
+function clockString(ms) {
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
