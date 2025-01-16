431df24e4c5f74b337053fb9394db03e826fc6dc
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-15 20:39:14 -0400

    Actualizar main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -1,5 +1,5 @@
 let handler = async (m, { isPrems, conn }) => {
-let img = 'https://f.uguu.se/KHvFzwMD.jpg' 
+let img = 'https://qu.ax/llZLr.jpg' 
 let texto = `*☁️ _M E N U - A U D I O S_ ☁️*
 
 ° _Tunometecabrasaramambiche_.
