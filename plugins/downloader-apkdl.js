522ed15a4f38cf6b985da44b29c27809082564e1
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-16 01:22:14 -0400

    Actualizar downloader-apkdl.js

--- plugins/downloader-apkdl.js
+++ plugins/downloader-apkdl.js
@@ -1,23 +1,29 @@
-import fetch from 'node-fetch'
+import { search, download } from 'aptoide-scraper'
 
-let handler = async (m, { conn, text }) => {
-if (!text) {
-return m.reply("❀ Ingresa el nomnbre de la app que quieres buscar")
-}
-
+var handler = async (m, {conn, usedPrefix, command, text}) => {
+if (!text) return conn.reply(m.chat, '🚩 *Ingrese el nombre de la apk para descargarlo.*', m, rcanal)
 try {
-let api = await fetch(`https://api.giftedtech.my.id/api/download/apkdl?apikey=gifted&appName=${text}`)
-let json = await api.json()
-let { appname, appicon, developer, download_url, mimetype } = json.result
-let txt = `- *Nombre* : ${appname}
-- *Creador* : ${developer}`
+await m.react(rwait)
+conn.reply(m.chat, '🚩 *Descargando su aplicación...*', m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: dev,
+previewType: 0, thumbnail: icons,
+sourceUrl: channel }}})
+let searchA = await search(text)
+let data5 = await download(searchA[0].id)
+let txt = `*乂  APTOIDE - DESCARGAS* 乂\n\n`
+txt += `🍟 *Nombre* : ${data5.name}\n`
+txt += `🚩 *Package* : ${data5.package}\n`
+txt += `🪴 *Update* : ${data5.lastup}\n`
+txt += `⚖ *Peso* :  ${data5.size}`
+await conn.sendFile(m.chat, data5.icon, 'thumbnail.jpg', txt, m, null, rcanal) 
+await m.react(done)  
+if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
+return await conn.reply(m.chat, '🛑 *El archivo es demaciado pesado*', m, rcanal )}
+await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: fkontak})
+} catch {
+return conn.reply(m.chat, '🛑 *Ocurrió un fallo*', m, rcanal )}}
 
-await conn.sendFile(m.chat, appicon, 'HasumiBotFreeCodes.jpg', txt, m)
-await conn.sendMessage(m.chat, { document: { url: download_url }, mimetype: mimetype, fileName: appname + '.apk', caption: null }, {quoted: m})
-} catch (error) {
-console.error(error)
-}}
-
-handler.command = /^(apk)$/i
-
-export default handler
+handler.tags = ['descargas']
+handler.help = ['apkmod']
