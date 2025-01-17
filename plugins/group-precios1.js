59bd32d6021cf55fc7565751977af3e5a98bf0ac
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-16 21:58:09 -0400

    Actualizar group-precios1.js

--- plugins/group-precios1.js
+++ plugins/group-precios1.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn }) => {
     const message = `AQUI ESTAN LOS PRECIOS.\n\n> 1 semana de spma = 1k de diamantes\n> 5 días = 800 diamantes\n> 3 días = 500 diamantes`;
 
     if (m.isGroup) {
-        const imageUrl = '';
+        const imageUrl = 'https://qu.ax/llZLr.jpg';
 
         await conn.sendMessage(m.chat, { 
             image: { url: imageUrl }, 
