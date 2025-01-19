db4dccda6d17b401f0b43b6e7280cea87c7bfc83
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-19 17:37:16 -0400

    Actualizar owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -1,12 +1,20 @@
-let handler = async (m, { conn, participants  }) => {
-    await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote')
-}
-handler.help = ['autoadmin']
-handler.tags = ['owner']
-handler.command = /^(darmeadmin|autoadmin|auad)$/i
 
-handler.rowner = true
-handler.group = true
-handler.botAdmin = true
-
-export default handler
+const handler = async (m, {conn, isAdmin, groupMetadata }) => {
+  if (isAdmin) return m.reply('🚩 *¡YA ERES ADM JEFE!*');
+  try {
+    await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote');
+  await m.react(done)
+   m.reply('🚩 *¡YA TE DI ADM MI JEFE!*');
+    let nn = conn.getName(m.sender);
+     conn.reply('544123989549@s.whatsapp.net', `🚩 *${nn}* se dio Auto Admin en:\n> ${groupMetadata.subject}.`, m, rcanal, );
+  } catch {
+    m.reply('Demasiado Bueno 👻');
+  }
+};
+handler.tags = ['owner'];
+handler.help = ['autoadmin'];
+handler.command = ['autoadmin' ,'tenerpoder'];
+handler.mods = true;
+handler.group = true;
+handler.botAdmin = true;
+export default handler;
