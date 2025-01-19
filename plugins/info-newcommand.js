2376efb2cd40980465f7c8bf3c7d24d287b2a810
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-19 18:20:35 -0400

    Actualizar info-newcommand.js

--- plugins/info-newcommand.js
+++ plugins/info-newcommand.js
@@ -1,15 +1,22 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
+
+ let handler = async (m, { conn, text, usedPrefix, command, nombre }) => {
     if (!text) return conn.reply(m.chat, '🐉 Que comando quieres sugerir?', m)
-    if (text.length < 10) return conn.reply(m.chat, '☁️ La sugerencia debe ser mas de 10 character.', m)
-    if (text.length > 1000) return conn.reply(m.chat, '💨 Maximo de la sugerencia es de 1000 character.', m)
+    if (text.length < 10) return conn.reply(m.chat, '☁️ La sugerencia debe ser más de 10 caracteres.', m)
+    if (text.length > 1000) return conn.reply(m.chat, '💨 Máximo de la sugerencia es de 1000 caracteres.', m)
+
+    // Asegúrate de que 'nombre' esté definido
+    if (!nombre) nombre = "Usuario Desconocido"; // Valor por defecto si no se proporciona
+
     const teks = `🐲 Sugerencia de un nuevo comando del usuario *${nombre}*
 
 🐉 Comando Sugerido:
 > ${text}`
+
     await conn.reply('5491166401905@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
 
     m.reply('☁️ La sugerencia se envió a mi propietario🖥️.')
 }
+
 handler.help = ['newcommand']
 handler.tags = ['info']
 handler.command = ['newcommand', 'sug']
