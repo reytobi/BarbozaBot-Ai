d3df238f32756d70cef3d8f170c6f56b9f5003b3
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-19 17:32:13 -0400

    Actualizar main-desarrolladores.js

--- plugins/main-desarrolladores.js
+++ plugins/main-desarrolladores.js
@@ -1,5 +1,5 @@
 const handler = async (m, { conn }) => {
-  let gifUrl = "https://qu.ax/JgmPo.mp4";
+  let gifUrl = "https://qu.ax/xNkYX.mp4";
 
   let text = `
  ╭────────⚔──────╮  
@@ -7,19 +7,19 @@ const handler = async (m, { conn }) => {
 ╰────────⚔──────╯  
 
 🔹 **SOBRE EL BOT:**  
-Son Goku Bot es una herramienta creada con el objetivo de mejorar la interacción y experiencia de los usuarios en diversas plataformas, ofreciendo funcionalidades avanzadas y soporte constante.
+Bot Barboza es una herramienta creada con el objetivo de mejorar la interacción y experiencia de los usuarios en diversas plataformas, ofreciendo funcionalidades avanzadas y soporte constante.
 
 🔹 **CONTACTO DE LOS DESARROLLADORES:**  
 ╭─────────────────────────╮  
-│🏆 **Son Goku Bot - Equipo Oficial**  
+│🏆 Bot Barboza- Equipo Oficial**  
 │  
-│📌 **Tesis**: [+52 243 126 8546]  
-│📌 **Matías-Crypto**: [+54 9 221 502-2907]  
-│📌 **YerTX2**: [+51 907 376 960]  
+│📌 Tesis: [+58 424 658 2666]  
+│
+│
 ╰─────────────────────────╯  
 
 🔹 **AGRADECIMIENTOS:**  
-Un agradecimiento especial a todos los colaboradores y usuarios que hacen posible la constante evolución de Son Goku Bot.  
+Un agradecimiento especial a todos los colaboradores y usuarios que hacen posible la constante evolución de Barboza.  
 
 🔹 **¿DUDAS O SUGERENCIAS?**  
 No dudes en contactar a cualquiera de los desarrolladores para resolver dudas, enviar sugerencias o reportar problemas.  
