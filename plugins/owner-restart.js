25e922d73f889d3d5ba52dc3fa267766beb79754
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-19 20:43:23 -0400

    Actualizar owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -2,7 +2,7 @@ import { spawn } from 'child_process'
 let handler = async (m, { conn, isROwner, text }) => {
     if (!process.send) return m.react('✖️')
     if (conn.user.jid == conn.user.jid) {
-    await m.reply('*☃️ REINICIANDO BOTBARBOZA-AI*')
+    await m.reply('*🔥 REINICIANDO BOTBARBOZA-AI*')
     process.send('reset')
   } else return m.react('✖️')
 }
