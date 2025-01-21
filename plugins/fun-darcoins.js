3d42304e77109631672c003ff618efe4ded11cee
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-20 21:52:44 -0400

    Actualizar plugins

---
+++ plugins/fun-darcoins.js
@@ -0,0 +1,26 @@
+import MessageType from '@whiskeysockets/baileys';
+const pajak = 0;
+const handler = async (m, {conn, text}) => {
+  let who;
+  if (m.isGroup) who = m.mentionedJid[0];
+  else who = m.chat;
+  if (!who) throw 'Etiqueta a la persona y escribe la cantidad.\nEjemplo :\n\n.darxp @kevin 50';
+  const txt = text.replace('@' + who.split`@`[0], '').trim();
+  if (!txt) throw '[❗] 𝙄𝙉𝙂𝙍𝙀𝙎𝘼 𝙇𝘼 𝘾𝘼𝙉𝙏𝙄𝘿𝘼𝘿 𝘿𝙀 𝙀𝙓𝙋𝙀𝙍𝙄𝙀𝙉𝘾𝙄𝘼 (𝙓𝙋) 𝙌𝙐𝙀 𝘿𝙀𝙎𝙀𝘼𝙎 𝘼𝙂𝙍𝙀𝙂𝘼𝙍';
+  if (isNaN(txt)) throw '[❗] 𝙎𝙄𝙈𝘽𝙊𝙇𝙊 𝙉𝙊 𝘼𝘿𝙈𝙄𝙏𝙄𝘿𝙊, 𝙎𝙊𝙇𝙊 𝙉𝙐𝙈𝙀𝙍𝙊𝙎!';
+  const xp = parseInt(txt);
+  let exp = xp;
+  const pjk = Math.ceil(xp * pajak);
+  exp += pjk;
+  if (exp < 1) throw '[❗] 𝙀𝙇 𝙉𝙐𝙈𝙀𝙍𝙊 𝙈𝙄𝙉𝙄𝙈𝙊 𝘿𝙀 𝙀𝙓𝙋𝙀𝙍𝙄𝙀𝙉𝘾𝙄𝘼 (𝙓𝙋) 𝙋𝘼𝙍𝘼 𝘼𝙂𝙍𝙀𝙂𝘼𝙍 𝙀𝙎 1';
+  const users = global.db.data.users;
+  users[who].exp += xp;
+  m.reply(`≡ *𝙓𝙋 𝘼𝙂𝙍𝙀𝙂𝘼𝘿𝙊*
+┌──────────────
+▢  *𝙏𝙊𝙏𝘼𝙇:* ${xp}
+©2025 Bot Barboza  -
+└──────────────`);
+};
+handler.command = ['darxp', 'addexp'];
+handler.admin = true;
+export default handler;
