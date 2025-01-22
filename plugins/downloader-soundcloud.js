5e43eb5f8c43048cc4ede9f5614119db7b587024
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-21 21:18:06 -0400

    Actualizar downloader-soundcloud.js

--- plugins/downloader-soundcloud.js
+++ plugins/downloader-soundcloud.js
@@ -1,6 +1,6 @@
 import yts from 'yt-search';
 const handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `\`\`\`[ 🌴 ] Por favor ingresa un texto. Ejemplo:\n${usedPrefix + command} Did i tell u that i miss you\`\`\``;
+  if (!text) throw `\`\`\`[ 🌴 ] Por favor ingresa un texto. Ejemplo:\n${usedPrefix + command} Gaita otrora \`\`\``;
 
   const isVideo = /vid|2|mp4|v$/.test(command);
   const search = await yts(text);
