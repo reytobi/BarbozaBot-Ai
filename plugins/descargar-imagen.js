30c64c069c33ba1446dce3b1c467e499f497d019
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-23 18:14:06 -0400

    Actualizar descargar-imagen.js

--- plugins/descargar-imagen.js
+++ plugins/descargar-imagen.js
@@ -1,19 +1,22 @@
 import {googleImage} from '@bochilteam/scraper';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-if (!text) return m.reply(`𝗤𝘂𝗲 𝗯𝘂𝘀𝗰𝗮𝗿? 🤔️ 𝙪𝘀𝙖𝙧 𝙙𝙚 𝙡𝙖 𝙨𝙞𝙜𝙪𝙞𝙚𝙣𝙩𝙚 𝙢𝙖𝙣𝙚𝙧𝙖\n• 𝗘𝗷𝗲𝗺𝗽𝗹𝗼\n*${usedPrefix + command} Loli*`) 
-if (m.text.includes('gore') || m.text.includes('cp')|| m.text.includes('porno')|| m.text.includes('Gore')|| m.text.includes('rule')|| m.text.includes('CP') || m.text.includes('Rule34') || m.text.includes('xxx')) throw '🙄 No voy a buscar tu pendejadas....'
-try {
+if (!text) return conn.reply(m.chat, '🔎 Por favor, ingresa un término de búsqueda.', m, rcanal);
+await m.react(rwait)
+conn.reply(m.chat, '『⇓⇓』 Descargando su imagen, espere un momento...', m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: dev,
+previewType: 0, thumbnail: icons,
+sourceUrl: channel }}})
 const res = await googleImage(text);
 const image = await res.getRandom();
 const link = image;
-conn.sendFile(m.chat, link, 'error.jpg', `_🔎 𝙍𝙚𝙨𝙪𝙡𝙩𝙖𝙙𝙤𝙨 𝙙𝙚: ${text}_`, m, null, fake);
-//conn.sendButton(m.chat, ``, botname, link, [['🔄 𝐒𝐈𝐆𝐔𝐈𝐄𝐍𝐓𝐄 🔄', `/${command} ${text}`]], null, null, m, null, fake)
-} catch (e) {
-console.log(e) 
-}}
-handler.help = ['gimage <query>', 'imagen <query>'];
-handler.tags = ['buscadores'];
-handler.command = /^(gimage|image|imagen)$/i;
-handler.register = true 
-handler.limit = 1
+const messages = [['Imagen 1', dev, await res.getRandom(),
+[[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 3', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 4', dev, await res.getRandom(), [[]], [[]], [[]], [[]]]]
+await conn.sendCarousel(m.chat, `✎ Resultado de ${text}`, '⪛✰ Imagen - Búsqueda ✰⪜', null, messages, m);
+};
+handler.help = ['imagen'];
+handler.tags = ['buscador', 'tools', 'descargas'];
+handler.command = ['image', 'imagen'];
+handler.register = true
 export default handler;
