224f4386c83434c71014ac2a3262b3a924609088
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-23 21:09:28 -0400

    Actualizar group-precios2.js

--- plugins/group-precios2.js
+++ plugins/group-precios2.js
@@ -1,26 +1,33 @@
 
-// Código Hecho Por Carlos
+// Código Hecho Por Barboza
 let handler = async (m, { conn }) => {
     // React con un emoji al mensaje
-    m.react('⭐');
+    await m.react('⭐');
+
     // Mensaje que se enviará
-    const message = "*AQUI ESTAN LOS PRECIOS.*\n\n" +
-                    "1 BOT = 3$\n" +
-                    "2 BOT = 6$\n" +
-                    "3 BOT = 9$\n" +
-                    "4 BOT = 12$\n" +
-                    "5 BOT = 15$\n\n" +
-                    "> Bot Para Grupos⭐";
+    const message = `
+*¡AQUÍ ESTÁN LOS PRECIOS!*
+
+1 BOT = 3$
+2 BOT = 6$
+3 BOT = 9$
+4 BOT = 12$
+5 BOT = 15$
+
+> Bot Para Grupos ⭐`;
+
     if (m.isGroup) {
         // URL de la imagen
         const imageUrl = 'https://f.uguu.se/lGOFAzGM.jpg'; // Cambia esta URL por la de la imagen que deseas enviar
-        // Envía el mensaje
-        // Envía la imagen
+
+        // Envía la imagen con el mensaje
         await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: message }, { mimetype: 'image/jpeg' });
     }
 }
+
 handler.help = ['precios2'];
 handler.tags = ['main'];
 handler.group = true;
 handler.command = ['precios2', 'p2'];
+
 export default handler;
