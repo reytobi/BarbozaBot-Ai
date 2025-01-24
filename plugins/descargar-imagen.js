fbef227d048ec9e76913946faf4c6b8b749f6418
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-24 13:18:33 -0400

    Actualizar descargar-imagen.js

--- plugins/descargar-imagen.js
+++ plugins/descargar-imagen.js
@@ -1,120 +1,21 @@
-
-import axios from 'axios';
-const {
-  generateWAMessageContent,
-  generateWAMessageFromContent,
-  proto
-} = (await import("@whiskeysockets/baileys"))["default"];
-
-let handler = async (message, { conn, text, usedPrefix, command }) => {
-  if (!text) {
-    return conn.reply(message.chat, "🖼️ *¿Qué quieres buscar en Pinterest?*", message);
-  }
-
-  await message.react(rwait);
-  conn.reply(message.chat, '⏬ *Descargando su imagen...*', message, {
-    contextInfo: {
-      externalAdReply: {
-        mediaUrl: null,
-        mediaType: 1,
-        showAdAttribution: true,
-        title: packname, // Asegúrate de que esta variable esté definida
-        body: wm, // Asegúrate de que esta variable esté definida
-        previewType: 0,
-        thumbnail: icons, // Asegúrate de que esta variable esté definida
-        sourceUrl: channel // Asegúrate de que esta variable esté definida
-      }
-    }
-  });
-
-  async function fetchImage(url) {
-    const {
-      imageMessage
-    } = await generateWAMessageContent({
-      'image': {
-        'url': url
-      }
-    }, {
-      'upload': conn.waUploadToServer
-    });
-    return imageMessage;
-  }
-
-  function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-  }
-
-  let imageCards = [];
-  let { data } = await axios.get(`https://www.pinterest.com/resource/BaseSearchResource/get/?source_url=%2Fsearch%2Fpins%2F%3Fq%3D${text}&data=%7B%22options%22%3A%7B%22isPrefetch%22%3Afalse%2C%22query%22%3A%22${text}%22%2C%22scope%22%3A%22pins%22%2C%22no_fetch_context_on_resource%22%3Afalse%7D%2C%22context%22%3A%7B%7D%7D&_=1619980301559`);
-
-  let imageUrls = data.resource_response.data.results.map(item => item.images.orig.url);
-  shuffleArray(imageUrls);
-
-  let selectedImages = imageUrls.splice(0, 5);
-  let index = 1;
-
-  for (let imageUrl of selectedImages) {
-    imageCards.push({
-      'body': proto.Message.InteractiveMessage.Body.fromObject({
-        'text': "Imagen - " + index++
-      }),
-      'footer': proto.Message.InteractiveMessage.Footer.fromObject({
-        'text': textbot // Asegúrate de que esta variable esté definida
-      }),
-      'header': proto.Message.InteractiveMessage.Header.fromObject({
-        'title': '',
-        'hasMediaAttachment': true,
-        'imageMessage': await fetchImage(imageUrl)
-      }),
-      'nativeFlowMessage': proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({
-        'buttons': [{
-          'name': "cta_url",
-          'buttonParamsJson': `{"display_text":"url 📫","Url":"https://www.pinterest.com/search/pins/?rs=typed&q=${text}","merchant_url":"https://www.pinterest.com/search/pins/?rs=typed&q=${text}"}` 
-        }]
-      })
-    });
-  }
-
-  const responseMessage = generateWAMessageFromContent(message.chat, {
-    'viewOnceMessage': {
-      'message': {
-        'messageContextInfo': {
-          'deviceListMetadata': {},
-          'deviceListMetadataVersion': 0x2
-        },
-        'interactiveMessage': proto.Message.InteractiveMessage.fromObject({
-          'body': proto.Message.InteractiveMessage.Body.create({
-            'text': "🔍 Resultado de: " + text
-          }),
-          'footer': proto.Message.InteractiveMessage.Footer.create({
-            'text': "🔎 Pinterest - Búsquedas"
-          }),
-          'header': proto.Message.InteractiveMessage.Header.create({
-            'hasMediaAttachment': false
-          }),
-          'carouselMessage': proto.Message.InteractiveMessage.CarouselMessage.fromObject({
-            'cards': [...imageCards]
-          })
-        })
-      }
-    }
-  }, {
-    'quoted': message
-  });
-
-  await message.react(done);
-  await conn.relayMessage(message.chat, responseMessage.message, {
-    'messageId': responseMessage.key.id
-  });
-};
-
-handler.help = ["pinterest"];
-handler.tags = ["descargas"];
-handler.group = true;
-handler.register = true;
-handler.command = ['pinterest'];
-
-export default handler;
+import { googleImage } from '@bochilteam/scraper'
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) throw `️${lenguajeGB['smsAvisoMG']()}${mid.smsMalused7} *${usedPrefix + command} Gata*`
+const prohibited = ['caca', 'polla', 'porno', 'porn', 'gore', 'cum', 'semen', 'puta', 'puto', 'culo', 'putita', 'putito','pussy', 'hentai', 'pene', 'coño', 'asesinato', 'zoofilia', 'mia khalifa', 'desnudo', 'desnuda', 'cuca', 'chocha', 'muertos', 'pornhub', 'xnxx', 'xvideos', 'teta', 'vagina', 'marsha may', 'misha cross', 'sexmex', 'furry', 'furro', 'furra', 'xxx', 'rule34', 'panocha', 'pedofilia', 'necrofilia', 'pinga', 'horny', 'ass', 'nude', 'popo', 'nsfw', 'femdom', 'futanari', 'erofeet', 'sexo', 'sex', 'yuri', 'ero', 'ecchi', 'blowjob', 'anal', 'ahegao', 'pija', 'verga', 'trasero', 'violation', 'violacion', 'bdsm', 'cachonda', '+18', 'cp', 'mia marin', 'lana rhoades', 'cepesito', 'hot', 'buceta', 'xxx', 'Violet Myllers', 'Violet Myllers pussy', 'Violet Myllers desnuda', 'Violet Myllers sin ropa', 'Violet Myllers culo', 'Violet Myllers vagina', 'Pornografía', 'Pornografía infantil', 'niña desnuda', 'niñas desnudas', 'niña pussy', 'niña pack', 'niña culo', 'niña sin ropa', 'niña siendo abusada', 'niña siendo abusada sexualmente' , 'niña cogiendo', 'niña fototeta', 'niña vagina', 'hero Boku no pico', 'Mia Khalifa cogiendo', 'Mia Khalifa sin ropa', 'Mia Khalifa comiendo polla', 'Mia Khalifa desnuda']
+if (prohibited.some(word => m.text.toLowerCase().includes(word))) return m.reply('⚠️😾')      
+try {
+const res = await googleImage(text)
+let image = res.getRandom()
+let link = image
+//conn.sendButton(m.chat, `💞 ${mid.buscador}: ${text}`, wm, link, [['🔄 𝙎𝙞𝙜𝙪𝙞𝙚𝙣𝙩𝙚 | 𝙉𝙚𝙭𝙩', `/imagen ${text}`]], null, null, m )
+conn.sendFile(m.chat, link, 'error.jpg', `💞 ${mid.buscador}: ${text}`, m)
+} catch (e) {
+console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+console.log(e)
+handler.money = false
+}}
+handler.help = ['gimage <query>', 'imagen <query>']
+handler.tags = ['internet', 'tools']
+handler.command = /^(gimage|image|imagen|jpg)$/i
+handler.money = 50
+export default handler
