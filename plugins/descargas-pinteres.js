84e8b00d5255d4bbd7f1a01f3a70e2b8cd932b5c
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-24 15:23:49 -0400

    Actualizar descargas-pinteres.js

--- plugins/descargas-pinteres.js
+++ plugins/descargas-pinteres.js
@@ -1,21 +1,30 @@
-import { googleImage } from '@bochilteam/scraper'
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `️${lenguajeGB['smsAvisoMG']()}${mid.smsMalused7} *${usedPrefix + command} Gata*`
-const prohibited = ['caca', 'polla', 'porno', 'porn', 'gore', 'cum', 'semen', 'puta', 'puto', 'culo', 'putita', 'putito','pussy', 'hentai', 'pene', 'coño', 'asesinato', 'zoofilia', 'mia khalifa', 'desnudo', 'desnuda', 'cuca', 'chocha', 'muertos', 'pornhub', 'xnxx', 'xvideos', 'teta', 'vagina', 'marsha may', 'misha cross', 'sexmex', 'furry', 'furro', 'furra', 'xxx', 'rule34', 'panocha', 'pedofilia', 'necrofilia', 'pinga', 'horny', 'ass', 'nude', 'popo', 'nsfw', 'femdom', 'futanari', 'erofeet', 'sexo', 'sex', 'yuri', 'ero', 'ecchi', 'blowjob', 'anal', 'ahegao', 'pija', 'verga', 'trasero', 'violation', 'violacion', 'bdsm', 'cachonda', '+18', 'cp', 'mia marin', 'lana rhoades', 'cepesito', 'hot', 'buceta', 'xxx', 'Violet Myllers', 'Violet Myllers pussy', 'Violet Myllers desnuda', 'Violet Myllers sin ropa', 'Violet Myllers culo', 'Violet Myllers vagina', 'Pornografía', 'Pornografía infantil', 'niña desnuda', 'niñas desnudas', 'niña pussy', 'niña pack', 'niña culo', 'niña sin ropa', 'niña siendo abusada', 'niña siendo abusada sexualmente' , 'niña cogiendo', 'niña fototeta', 'niña vagina', 'hero Boku no pico', 'Mia Khalifa cogiendo', 'Mia Khalifa sin ropa', 'Mia Khalifa comiendo polla', 'Mia Khalifa desnuda']
-if (prohibited.some(word => m.text.toLowerCase().includes(word))) return m.reply('⚠️😾')      
+import Starlights from '@StarlightsTeam/Scraper';
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) return conn.reply(m.chat,`🚩 Ingrese un enlace de Pinterest\n\nEjemplo:\n> *${usedPrefix + command}* https://ar.pinterest.com/pin/588142032613788991/`, m);
+
+await m.react('🕓');
 try {
-const res = await googleImage(text)
-let image = res.getRandom()
-let link = image
-//conn.sendButton(m.chat, `💞 ${mid.buscador}: ${text}`, wm, link, [['🔄 𝙎𝙞𝙜𝙪𝙞𝙚𝙣𝙩𝙚 | 𝙉𝙚𝙭𝙩', `/imagen ${text}`]], null, null, m )
-conn.sendFile(m.chat, link, 'error.jpg', `💞 ${mid.buscador}: ${text}`, m)
-} catch (e) {
-console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
-console.log(e)
-handler.money = false
-}}
-handler.help = ['gimage <query>', 'imagen <query>']
-handler.tags = ['internet', 'tools']
-handler.command = /^(gimage|image|imagen|jpg)$/i
-handler.money = 50
-export default handler
+let { dl_url, quality, size, duration, url } = await Starlights.pinterestdl(args[0]);
+
+let txt = '`乂  P I N T E R E S T  -  D L`\n\n'
+txt += `  ✩   *Calidad* : ${quality}\n`;
+txt += `  ✩   *Tamaño* : ${size}\n`;
+txt += `  ✩   *Duracion* : ${duration}\n`;
+txt += `  ✩   *Url* : ${url}\n\n`
+txt += `> 🚩 *${textbot}*`
+
+
+await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: txt, mimetype: 'video/mp4', fileName:  `pinterest.mp4`}, {quoted: m })
+await m.react('✅');
+} catch {
+await m.react('✖️');
+}
+};
+
+handler.help = ['pinterestdl *<url pin>*'];
+handler.tags = ['downloader'];
+handler.command = ['pinterestdl', 'pindl'];
+handler.register = true;
+
+export default handler;
