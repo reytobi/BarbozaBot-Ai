66b02741a195bbf81cce81ab8fc5fd5fcfc4e602
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-24 22:59:24 -0400

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -68,7 +68,7 @@ Hola *%name* soy *Barboza*
 \t\t\t⚙️_*𝐋𝐈𝐒𝐓𝐀 𝐃𝐄 𝐂𝐎𝐌𝐀𝐍𝐃𝐎𝐒*_ 🚀
 `.trimStart(),
   header: '*╭╍╍╍╍❖【 *%category* 】',
-  body: '┋🪄›【 %cmd %islimit %isPremium\n',
+  body: '┋💨›【 %cmd %islimit %isPremium\n',
   footer: '*╰╍╍╍╍❖•ೋ° °ೋ•❖╍╍╍╍╯*',
   after: `© ${textbot}`,
 }
