b57ac5f76f5a1a574b936b3c3f8c04e1d0047a98
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-25 14:43:49 -0400

    Actualizar rg-reg.js

--- plugins/rg-reg.js
+++ plugins/rg-reg.js
@@ -24,10 +24,10 @@ let handler = async function (m, { conn, text, usedPrefix, command }) {
   let txt = ` –  *𝐑 𝐄 𝐆 𝐈 𝐒 𝐓 𝐑 𝐎  -  𝐁 𝐀 𝐑 𝐁*\n\n`
       txt += `╔  👤  *NOMBRE* : ${name}\n`
       txt += `╠  💎  *EDAD* : ${age} años\n`
-      txt += `╠  _Recompensa💰_\n`
+      txt += `╠  _Recompensas💰_\n`
       txt += `╠  245 experiencia\n`
       txt += `╠  15 estrellas⭐\n`
-      txt += `╠  Barbozacoins🪙\n`
+      txt += `╠  Coins 25🪙\n`
       txt += `╚ 𝐁𝐈𝐄𝐍𝐕𝐄𝐍𝐈𝐃𝐎 𝐀 𝐁𝐀𝐑𝐁𝐎𝐙𝐀-𝐁𝐎𝐓☁️`
   await conn.sendAi(m.chat, botname, textbot, txt, img, img, canal, m)
   await m.react('✅')
