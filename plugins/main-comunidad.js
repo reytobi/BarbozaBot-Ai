d51a3f606dd80bc5b2dd947ffc9f7c34f5d39ef5
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-24 22:51:53 -0400

    Actualizar main-comunidad.js

--- plugins/main-comunidad.js
+++ plugins/main-comunidad.js
@@ -1,25 +1,25 @@
 const handler = async (m, { conn }) => {
-  let gifUrl = "https://qu.ax/llZLr.jpg";
+  let gifUrl = "https://qu.ax/LJEVX.jpg";
 
   let text = `
  ──────── ⚔ ────────  
-     **COMUNIDAD**  
+     *COMUNIDAD*  
 ──────── ⚔ ────────  
 
-**Bot barboza**  
+*Bot barboza*  
 • ,👥➤ **Grupo de WhatsApp de la comunidad de Bot Barboza Ai**  
    Únete para compartir y resolver dudas con otros usuarios. 
   ➤https://whatsapp.com/channel/0029Vaua0ZD3gvWjQaIpSy18
 
-• 📢 ➤ **Canal de Bot Barboza Ai**  
+• 📢 ➤ *Canal de Bot Barboza Ai*  
    Recibe actualizaciones, noticias y lanzamientos del bot.  
 https://whatsapp.com/channel/0029Vaua0ZD3gvWjQaIpSy18
-• 💬 ➤ **Grupo de WhatsApp activo**  
+• 💬 ➤ *Grupo de WhatsApp activo*  
    Chatea con usuarios en tiempo real y sé parte de la conversación y usa al bot que esta de uso libre.  
 ➤https://chat.whatsapp.com/E1kx7olE0RpA18BdALdaWV
 
 ──────── ⚔ ────────  
-🔍 **¿Sabías que...?**  
+🔍 *¿Sabías que...?* 
 - El bot Barboza Ai es actualizado regularmente para mejorar su desempeño.  
 - Puedes sugerir mejoras o reportar errores directamente en los grupos.  
 - Nuestra comunidad sigue creciendo y cuenta con soporte activo.  
