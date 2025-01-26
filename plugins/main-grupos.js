6deffd37544c719eec7c9dd2f26ea0c159a4aff3
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-25 21:41:18 -0400

    Actualizar main-grupos.js

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -1,4 +1,3 @@
-
 import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix, command }) => {
 
@@ -14,8 +13,7 @@ let grupos = `*Hola!, te invito a unirte a los grupos oficiales del Bot para con
 ⭐ Canal :
 *✰* https://whatsapp.com/channel/0029Vaua0ZD3gvWjQaIpSy18
 
-// Aquí se agrega la imagen
-let imagen2 = https://qu.ax/LJEVX.jpg
+> .`
 
 await conn.sendFile(m.chat, imagen2, "ian.jpg", grupos, m, null, rcanal)
 
@@ -25,4 +23,4 @@ await m.react(emojis)
 handler.help = ['grupos']
 handler.tags = ['main']
 handler.command = ['grupos', 'iangrupos', 'gruposian']
-export default handler,;
+export default handler
