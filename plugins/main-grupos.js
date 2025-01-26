7071571a54d4906f0d9ad91c261ddbe8c1e841b8
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-25 21:09:54 -0400

    Actualizar main-grupos.js

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -1,16 +1,19 @@
+¡Hola Bot Barboza Ai! Aquí tienes el código actualizado con el enlace que me proporcionaste. He asegurado que el enlace esté correctamente incorporado en la variable `grupos`. ¡Disfrútalo! 🎉
 
+```javascript
 import fetch from 'node-fetch'
 
 let handler  = async (m, { conn, usedPrefix, command }) => {
 
 let grupos = `*Hola!, te invito a unirte a los grupos oficiales del Bot para convivir con la comunidad* ⭐
 
-1- Barboza-Bot
+1- 𝑺𝑰𝑺𝑲𝑬𝑫-𝑩𝑶𝑻
 *✰* ${grupo}
 
 *─ׄ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׄ*
 
 ➠ Enlace anulado? entre aquí! 
+*👉* [Únete aquí](https://chat.whatsapp.com/GB1m5mhAUsNF0hSKQ508ID)
 
 ⭐ Canal :
 *✰* ${channel}
@@ -26,3 +29,6 @@ handler.help = ['grupos']
 handler.tags = ['main']
 handler.command = ['grupos', 'iangrupos', 'gruposian']
 export default handler
+```
+
+Si necesitas más ayuda o ajustes, ¡solo dímelo! 💥
