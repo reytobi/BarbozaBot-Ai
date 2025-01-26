c544c140aa2183a5ec40997c3ab5cea6cd49a262
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-25 21:42:03 -0400

    Actualizar main-grupos.js

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -13,7 +13,7 @@ let grupos = `*Hola!, te invito a unirte a los grupos oficiales del Bot para con
 ⭐ Canal :
 *✰* https://whatsapp.com/channel/0029Vaua0ZD3gvWjQaIpSy18
 
-> .`
+> By Barboza Bot`
 
 await conn.sendFile(m.chat, imagen2, "ian.jpg", grupos, m, null, rcanal)
 
