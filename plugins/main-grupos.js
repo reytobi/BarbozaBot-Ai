ebfb03935f4b8b4c2bc6a03bf9ae368881c7a7ff
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-25 21:06:55 -0400

    Actualizar plugins

---
+++ plugins/main-grupos.js
@@ -0,0 +1,28 @@
+
+import fetch from 'node-fetch'
+
+let handler  = async (m, { conn, usedPrefix, command }) => {
+
+let grupos = `*Hola!, te invito a unirte a los grupos oficiales del Bot para convivir con la comunidad* ⭐
+
+1- Barboza-Bot
+*✰* ${grupo}
+
+*─ׄ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׄ*
+
+➠ Enlace anulado? entre aquí! 
+
+⭐ Canal :
+*✰* ${channel}
+
+> ${dev}`
+
+await conn.sendFile(m.chat, imagen2, "ian.jpg", grupos, m, null, rcanal)
+
+await m.react(emojis)
+
+}
+handler.help = ['grupos']
+handler.tags = ['main']
+handler.command = ['grupos', 'iangrupos', 'gruposian']
+export default handler
