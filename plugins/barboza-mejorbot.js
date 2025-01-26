b19828c7b1a2dbd82ef46b09afcc25b25f0ee585
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-26 13:18:14 -0400

    Actualizar plugins

---
+++ plugins/barboza-mejorbot.js
@@ -0,0 +1,11 @@
+
+// Creando el objeto mejorbot
+const mejorbot = {
+    nombre: "Bot Barboza-Ai",
+    saludar: function() {
+        console.log(this.nombre);
+    }
+};
+
+// Llamando a la función para que imprima el nombre
+mejorbot.saludar();
