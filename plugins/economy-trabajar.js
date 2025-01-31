279fe758de9a67d9e215ab60a162a6a676ce1c36
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-31 19:25:54 -0400

    Actualizar economy-trabajar.js

--- plugins/economy-trabajar.js
+++ plugins/economy-trabajar.js
@@ -1,49 +1,54 @@
+¡Hola Sebastián! Claro, puedo ayudarte a modificar el código para que en lugar de ganar XP, los usuarios ganen dulces. Aquí te dejo el código adaptado:
+
+```javascript
 let cooldowns = {}
 
 let handler = async (m, { conn, isPrems }) => {
-let user = global.db.data.users[m.sender]
-let tiempo = 5 * 60
-if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempo * 1000) {
-const tiempo2 = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempo * 1000 - Date.now()) / 1000))
-conn.reply(m.chat, `🤚 Espera ⏱️ *${tiempo2}* para volver a Trabajar.`, m, rcanal)
-return
-}
-let rsl = Math.floor(Math.random() * 5000)
-cooldowns[m.sender] = Date.now()
-await conn.reply(m.chat, `👨‍🏭 ${pickRandom(trabajo)} *${toNum(rsl)}* ( *${rsl}* ) XP ✨.`, m, rcanal)
-user.exp += rsl
+    let user = global.db.data.users[m.sender]
+    let tiempo = 5 * 60
+    if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempo * 1000) {
+        const tiempo2 = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempo * 1000 - Date.now()) / 1000))
+        conn.reply(m.chat, `🤚 Espera ⏱️ *${tiempo2}* para volver a Trabajar.`, m, rcanal)
+        return
+    }
+    let rsl = Math.floor(Math.random() * 5000)
+    cooldowns[m.sender] = Date.now()
+    await conn.reply(m.chat, `🍬 ${pickRandom(trabajo)} *${toNum(rsl)}* ( *${rsl}* ) dulces 🍬✨.`, m, rcanal)
+    user.dulces += rsl // Cambié esto para que sume dulces
 }
 
 handler.help = ['trabajar']
 handler.tags = ['economy']
-handler.command = ['w','work','chambear','chamba', 'trabajar']
+handler.command = ['w', 'work', 'chambear', 'chamba', 'trabajar']
 handler.group = true;
 handler.register = true
 export default handler
 
 function toNum(number) {
-if (number >= 1000 && number < 1000000) {
-return (number / 1000).toFixed(1) + 'k'
-} else if (number >= 1000000) {
-return (number / 1000000).toFixed(1) + 'M'
-} else if (number <= -1000 && number > -1000000) {
-return (number / 1000).toFixed(1) + 'k'
-} else if (number <= -1000000) {
-return (number / 1000000).toFixed(1) + 'M'
-} else {
-return number.toString()}}
+    if (number >= 1000 && number < 1000000) {
+        return (number / 1000).toFixed(1) + 'k'
+    } else if (number >= 1000000) {
+        return (number / 1000000).toFixed(1) + 'M'
+    } else if (number <= -1000 && number > -1000000) {
+        return (number / 1000).toFixed(1) + 'k'
+    } else if (number <= -1000000) {
+        return (number / 1000000).toFixed(1) + 'M'
+    } else {
+        return number.toString()
+    }
+}
 
 function segundosAHMS(segundos) {
-let minutos = Math.floor((segundos % 3600) / 60)
-let segundosRestantes = segundos % 60
-return `${minutos} minutos y ${segundosRestantes} segundos`
+    let minutos = Math.floor((segundos % 3600) / 60)
+    let segundosRestantes = segundos % 60
+    return `${minutos} minutos y ${segundosRestantes} segundos`
 }
 
 function pickRandom(list) {
-return list[Math.floor(list.length * Math.random())];
+    return list[Math.floor(list.length * Math.random())];
 }
 
-// Thanks to FG98
+// Gracias a FG98
 const trabajo = [
    "Trabajas como cortador de galletas y ganas",
    "Trabaja para una empresa militar privada, ganando",
@@ -77,5 +82,8 @@ const trabajo = [
    "Resolviste el misterio del brote de cólera y el gobierno te recompensó con una suma de",
    "Trabajas como zoólogo y ganas",
    "Vendiste sándwiches de pescado y obtuviste",
-   "Reparas las máquinas recreativas y recibes",
-] 
+   "Reparas las máquinas recreativas y recibes"
+]
+```
+
+He cambiado las partes donde se menciona XP a dulces, así que ahora tus usuarios ganarán dulces cuando trabajen. ¡Espero que esto te sirva! Si necesitas más cambios o ayuda, aquí estoy.
