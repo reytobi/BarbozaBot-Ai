218fd9cacf4f026e597395c28b16c1a8f09542f8
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-02-01 18:53:54 -0400

    Actualizar rpg-quitardulces.js

--- plugins/rpg-quitardulces.js
+++ plugins/rpg-quitardulces.js
@@ -1,30 +1,28 @@
 
-import db from '../lib/database.js'
-
 let handler = async (m, { conn, text }) => {
-    let who;
-    if (m.isGroup) who = m.mentionedJid[0];
-    else who = m.chat;
+    let quien;
+    if (m.isGroup) quien = m.mentionedJid[0];
+    else quien = m.chat;
 
-    if (!who) throw '🚩 Menciona al usuario con *@user.*';
+    if (!quien) throw '🚩 Menciona al usuario con *@user.*';
 
-    let txt = text.replace('@' + who.split('@')[0], '').trim();
-    if (!txt) throw '🚩 Ingrese la cantidad de *🍬 Dulces* que quiere quitar.';
+    let txt = text.replace('@' + quien.split('@')[0], '').trim();
+    if (!txt) throw '🚩 Ingresa la cantidad de *🍬 Dulces* que quieres quitar.';
     
-    if (isNaN(txt)) throw 'Sólo números.';
+    if (isNaN(txt)) throw 'Solo se permiten números.';
     
     let dulcesQuitados = parseInt(txt);
     
     let users = global.db.data.users;
 
     // Verificar si el usuario tiene suficientes dulces para quitar
-    if (users[who].dulces < dulcesQuitados) throw `${who.split('@')[0]} no tiene suficientes *🍬 Dulces* para quitar.`;
+    if (users[quien].dulces < dulcesQuitados) throw `${quien.split('@')[0]} no tiene suficientes *🍬 Dulces* para quitar.`;
     
     // Restar dulces al usuario mencionado
-    users[who].dulces -= dulcesQuitados;
+    users[quien].dulces -= dulcesQuitados;
 
     // Respuesta al usuario
-    await m.reply(`🔻 Has quitado *${dulcesQuitados}* dulces a ${who.split('@')[0]}!`);
+    await m.reply(`🔻 Has quitado *${dulcesQuitados}* dulces a ${quien.split('@')[0]}!`);
 }
 
 handler.help = ['quitarDulces *@user <cantidad>*'];
