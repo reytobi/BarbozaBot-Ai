a2a4025cec1cf88dc817352feda5e22700bdb034
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-02-01 18:26:52 -0400

    Actualizar economy-shop.js

--- plugins/economy-shop.js
+++ plugins/economy-shop.js
@@ -1,18 +1,18 @@
 const xpperyenes = 350;
 const handler = async (m, {conn, command, args}) => {
   let count = command.replace(/^buy/i, '');
-  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperyenes) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
+  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperdulces) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
   count = Math.max(1, count);
-  if (global.db.data.users[m.sender].exp >= xpperyenes * count) {
-    global.db.data.users[m.sender].exp -= xpperyenes * count;
-    global.db.data.users[m.sender].yenes += count;
+  if (global.db.data.users[m.sender].exp >= xpperdulces * count) {
+    global.db.data.users[m.sender].exp -= xpperdulces * count;
+    global.db.data.users[m.sender].dulces += count;
     conn.reply(m.chat, `
 ╔═══════⩽✰⩾═══════╗
 ║    𝐍𝐨𝐭𝐚 𝐃𝐞 𝐏𝐚𝐠𝐨 
 ╠═══════⩽✰⩾═══════╝
 ║╭──────────────┄
 ║│ *Compra Nominal* : + ${count} 🍬
-║│ *Gastado* : -${xpperyenes * count} XP
+║│ *Gastado* : -${xpperdulces * count} XP
 ║╰──────────────┄
 ╚═══════⩽✰⩾═══════╝`, m, rcanal);
   } else conn.reply(m.chat, `😔 Lo siento, no tienes suficiente *XP* para comprar *${count}* Dulces 🍬`, m, rcanal);
