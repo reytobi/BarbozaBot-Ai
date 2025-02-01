681e49dab9d3a5afdfaeb0503b936b160964dcb6
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-31 20:23:12 -0400

    Actualizar search-imagen.js

--- plugins/search-imagen.js
+++ plugins/search-imagen.js
@@ -1,30 +1,34 @@
-import Starlights from '@StarlightsTeam/Scraper';
+/* Imagen Search By WillZek 
+- Free Codes Titan 
+- https://whatsapp.com/channel/0029ValMlRS6buMFL9d0iQ0S
+*/
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) return conn.reply(m.chat,`🚩 Ingrese un enlace de Pinterest\n\nEjemplo:\n> *${usedPrefix + command}* https://ar.pinterest.com/pin/588142032613788991/`, m);
+import fetch from 'node-fetch';
 
-await m.react('🕓');
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+
+if (!text) return m.reply('🍭 Ingrese Un Texto Para Buscar Una Imagen');
+
 try {
-let { dl_url, quality, size, duration, url } = await Starlights.pinterestdl(args[0]);
+let api = `https://api.dorratz.com/v3/ai-image?prompt=${text}`;
+let response = await fetch(api);
+let json = await response.json();
+let res = json.data;
 
-let txt = '`乂  P I N T E R E S T  -  D L`\n\n'
-txt += `  ✩   *Calidad* : ${quality}\n`;
-txt += `  ✩   *Tamaño* : ${size}\n`;
-txt += `  ✩   *Duracion* : ${duration}\n`;
-txt += `  ✩   *Url* : ${url}\n\n`
-txt += `> 🚩 *${textbot}*`
+m.react('🕑');
+let txt = `> *Resultado De: ${text}*`;
+let img = res.image_link;
+let link = img;
 
+await conn.sendMessage(m.chat, { image: { url: link }, caption: txt }, {quoted: fkontak});   
+m.react('✅');
 
-await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: txt, mimetype: 'video/mp4', fileName:  `pinterest.mp4`}, {quoted: m })
-await m.react('✅');
-} catch {
-await m.react('✖️');
+} catch (e) {
+m.reply(`Error: ${e.message}`);
+m.react('✖️');
+ }
 }
-};
 
-handler.help = ['pinterestdl *<url pin>*'];
-handler.tags = ['downloader'];
-handler.command = ['pinterestdl', 'pindl'];
-handler.register = true;
+handler.command = ['imagen', 'image'];
 
 export default handler;
