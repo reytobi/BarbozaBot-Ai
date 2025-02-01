78d5fa151eecd3141ab11b1234cfd34bb827430d
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-02-01 19:47:25 -0400

    Actualizar main-menu5.js

--- plugins/main-menu5.js
+++ plugins/main-menu5.js
@@ -1,19 +1,20 @@
+
 let handler = async (m, { conn }) => {
     let menu5 = `
 🌟 *Menú 5* 🌟
-`Bienvenido al mundo de las mascotas 🐇`
+Bienvenido al mundo de las mascotas 🐇
 
-`1 Consigue tu mascota con . comprar y la mascota Pero ante de eso tiene que conseguir dulces con .claim .w y . minar`
+1. Consigue tu mascota con .comprar. Pero antes de eso, tienes que conseguir dulces con .claim, .w y .minar.
 
-`2 . mimascota para ver el estado de tu mascota`
+2. Usa .mimascota para ver el estado de tu mascota.
 
-`3 .comprarcomida compra comida para tu mascota y alimentarla`
+3. Usa .comprarcomida para comprar comida para tu mascota y alimentarla.
 
-`4 .alimentar alimenta a tu mascota` 
+4. Usa .alimentar para alimentar a tu mascota.
 
-`5.cotos para ver el costo de cada mascota`
+5. Usa .cotos para ver el costo de cada mascota.
 
-`6 Importante para poder todo eso tenés que conseguir dulces con los comandos.claim .crimen.w .trabajar .Buy y minar`
+6. Importante: Para poder hacer todo eso, tienes que conseguir dulces con los comandos: .claim, .crimen, .w, .trabajar, .buy y minar.
 
 Para más información, utiliza el comando correspondiente.
     `.trim();
