c468e7cae6a2b222305216b3e22b8aa47749d545
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-02-02 13:27:44 -0400

    Actualizar fun-mamaguevo.js

--- plugins/fun-mamaguevo.js
+++ plugins/fun-mamaguevo.js
@@ -1,14 +1,14 @@
 
 const handler = async (m) => {
-    if (m.text.startsWith('.mamaguevo') || m.text.startsWith('.mmgvo')) {
+    if (m.text.startsWith('.mamaguevo')) {
         const usuario = m.sender; // Obtener el identificador del usuario
 
         // Mensaje a enviar
-        const mensaje = `💫 *CALCULADORA*\n\n💅🏻 Los cálculos han arrojado que @${usuario} es *%* mmgvo 🏳️‍🌈\n> ✰ La Propia Puta Mamando!\n\n➤ ¡Sorpresa!`;
+        const mensaje = `💫 *CALCULADORA*\n\n💅🏻 Los cálculos han arrojado que @${usuario} *%* mmgvo 🏳️‍🌈\n> ✰ La Propia Puta Mamando!\n\n➤ ¡Sorpresa!`;
 
         return m.reply(mensaje);
     }
 };
 
-handler.command = /^(mamaguevo|mmgvo)$/i;
+handler.command = /^(mamaguevo)$/i;
 export default handler;
