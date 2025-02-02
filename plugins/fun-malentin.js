41e85b48404738f40fcb682821c692a0d914d3c1
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-02-02 13:33:46 -0400

    Actualizar plugins

---
+++ plugins/fun-malentin.js
@@ -0,0 +1,17 @@
+
+const handler = async (m) => {
+    if (m.text.startsWith('.maletín')) {
+        // Detalles de la sorpresa
+        const monedas = 20;
+        const dulces = 10;
+        const xp = 100;
+
+        // Mensaje a enviar
+        const mensaje = `🎒 *Maletín Abierto*\n\n¡Sorpresa! Has recibido:\n💰 ${monedas} Monedas\n🍬 ${dulces} Dulces\n✨ ${xp} XP\n\n➤ ¡Disfruta tu recompensa!`;
+
+        return m.reply(mensaje);
+    }
+};
+
+handler.command = /^(maletin)$/i;
+export default handler;
