62d785745876a21b4d10c075cf5fc879193343d3
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-02-03 19:07:57 -0400

    Actualizar img-pinterest.js

--- plugins/img-pinterest.js
+++ plugins/img-pinterest.js
@@ -1,37 +1,25 @@
-/* 
 
-*❀ By JTxs*
-
-[ Canal Principal ] :
-https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n
-
-[ Canal Rikka Takanashi Bot ] :
-https://whatsapp.com/channel/0029VaksDf4I1rcsIO6Rip2X
-
-[ Canal StarlightsTeam] :
-https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
-
-[ HasumiBot FreeCodes ] :
-https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
-*/
-
-// *[ ❀ PINTEREST SEARCH ]*
 import axios from 'axios'
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `❀ Ingresa el texto de lo que quieras buscar`, m)
-
-
+let HS = async (m, { conn, text }) => {
+if (!text) return conn.reply(m.chat, `❀ Ingresa un texto para buscar en pinterest`, m)
+  
 try {
-let api = await axios.get(`https://restapi.apibotwa.biz.id/api/search-pinterest?message=${text}`)
+let api = await axios.get(`https://api.siputzx.my.id/api/s/pinterest?query=${text}`)
 let json = api.data
+let data = json.data[Math.floor(Math.random() * json.data.length)]
 
-await conn.sendFile(m.chat, json.data.response, 'HasumiBotFreeCodes.jpg', `❀ Resultado de : *${text}*`, m)
-
+let { pin, created_at, images_url, grid_title } = data
+let HS = `- *Titulo :* ${grid_title}
+- *Creado :* ${created_at}
+- *link :* ${pin}`
+await conn.sendMessage(m.chat, { image: { url: images_url }, caption: HS }, { quoted: m })
+//await conn.sendMessage(m.chat, { image: images_url, caption: HS, footer: '', buttons: [ { buttonId: `.pinterest ${text}`, buttonText: { displayText: 'Siguiente' } }, ], viewOnce: true, headerType: 4 }, { quoted: m })
+    
 } catch (error) {
-console.error(error)    
-}}    
+console.error(error)
+}}
 
-handler.command = ['pinterest', 'pinterestsearch']
+HS.command = ['pinterest', 'pinterestsearch']
 
-export default handler
+export default HS
