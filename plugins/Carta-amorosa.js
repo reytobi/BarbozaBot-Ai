3ff4c81e722a78cc39a1d8b872c4ada24351c740
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-02-03 20:24:42 -0400

    Actualizar Carta-amorosa.js

--- plugins/Carta-amorosa.js
+++ plugins/Carta-amorosa.js
@@ -5,8 +5,9 @@ let handler = async (m, { conn, text }) => {
         return conn.sendMessage(m.chat, { text: "Por favor, menciona a un usuario. Ejemplo: .carta2 @usuario" }, { quoted: m });
     }
 
-    let user = text.replace(/[^0-9]/g, ''); // Extraer el ID del usuario mencionado
-    let cartaMessage = `🌹✨ *De Sebastián para @⁨${user}/⁩* ✨🌹\n\nQuerido/a @⁨${user}/⁩,\n\nEres la luz que ilumina mis días y la razón detrás de mis sonrisas. Cada momento contigo es un regalo que atesoro en mi corazón. Espero que siempre sientas lo especial que eres para mí.\n\nCon todo mi cariño,\nSebastián 💖`;
+    let userMentioned = text.replace(/[^0-9]/g, ''); // Extraer el ID del usuario mencionado
+    let userSender = m.sender.split('@')[0]; // Obtener el nombre del usuario que envía el mensaje
+    let cartaMessage = `🌹✨ *De @⁨${userSender}/⁩ para @⁨${userMentioned}/⁩* ✨🌹\n\nQuerido/a @⁨${userMentioned}/⁩,\n\nEres la razón de mi felicidad y cada día contigo es un nuevo capítulo lleno de amor. Espero que siempre sientas lo especial que eres para mí. Gracias por ser tú.\n\nCon todo mi cariño,\n@⁨${userSender}/⁩ 💖`;
 
     // Enviamos el mensaje al chat
     await conn.sendMessage(m.chat, { text: cartaMessage }, { quoted: m });
