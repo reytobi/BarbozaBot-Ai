e0caa64138c5bdbcce80702b994af3d8fd1e5e39
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-02-04 19:52:37 -0400

    Actualizar rpg-dayli.js

--- plugins/rpg-dayli.js
+++ plugins/rpg-dayli.js
@@ -9,7 +9,7 @@ let handler = async (m) => {
     return
   }
 
-   let claim = `Felicidades 🎉, reclamaste *100 🍬 Dulces*.`
+   let claim = `Felicidades 🎉, reclamaste *20 🍬 Dulces*.`
    user.limit += 20
    await m.reply(claim)
    cooldowns[m.sender] = Date.now()
