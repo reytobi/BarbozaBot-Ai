13702feededad211143cbf361c7a6d0d18063d85
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-02-04 22:41:26 -0400

    Actualizar rpg-paseo.js

--- plugins/rpg-paseo.js
+++ plugins/rpg-paseo.js
@@ -1,14 +1,14 @@
 
 let handler = async (m) => {
     let user = global.db.data.users[m.sender];
-    
+
     // Simular el paseo
     await m.reply(`${user.nombre}, ¡estás paseando a tu mascota! 🐾`);
-    
+
     // Dar la recompensa
     user.dulces += 50; // Aumentar los dulces en 50
     user.exp += 50; // Aumentar la experiencia en 50
-    
+
     await m.reply(`🎉 ¡Felicidades! Has recibido *50 dulces* y *50 EXP* por pasear a tu mascota.`);
 }
 
