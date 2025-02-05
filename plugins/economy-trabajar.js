c4f74030a406ff7269a2d94f5bbc8327b521ea04
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-02-04 20:17:16 -0400

    Actualizar economy-trabajar.js

--- plugins/economy-trabajar.js
+++ plugins/economy-trabajar.js
@@ -1,92 +1,65 @@
+const cooldowns = {}
 
-import fs from 'fs';
-
-const filePath = './mineria.json';
-let cooldowns = {};
-
-let handler = async (m, { conn, isPrems }) => {
-    // Cargar datos de mineria.json
-    let data = fs.existsSync(filePath) ? JSON.parse(fs.readFileSync(filePath)) : {};
-
-    let who = m.sender;
-    let tiempo = 5 * 60; // 5 minutos de cooldown
-
-    if (cooldowns[who] && Date.now() - cooldowns[who] < tiempo * 1000) {
-        const tiempoRestante = segundosAHMS(Math.ceil((cooldowns[who] + tiempo * 1000 - Date.now()) / 1000));
-        conn.reply(m.chat, `🤚 Espera ⏱️ *${tiempoRestante}* para volver a Trabajar.`, m);
-        return;
-    }
-
-    // Generar recompensas aleatorias
-    let xp = Math.floor(Math.random() * 5000);
-    let barbozaCoins = Math.floor(Math.random() * (100 - 50 + 1)) + 50;
-    let diamantes = Math.floor(Math.random() * (40 - 20 + 1)) + 20;
-    let dulces = Math.floor(Math.random() * (50 - 10 + 1)) + 10; // Cambié de 'dulce' a 'dulces'
-
-    // Si el usuario no está en el JSON, se inicializa
-    if (!data[who]) {
-        data[who] = { xp: 0, barbozaCoins: 0, diamantes: 0, dulces: 0 };
-    }
-
-    // Sumar recompensas al usuario
-    data[who].xp += xp; // Cambié 'exp' a 'xp'
-    data[who].barbozaCoins += barbozaCoins;
-    data[who].diamantes += diamantes;
-    data[who].dulces += dulces; // Cambié '${dulces}' a 'dulces'
-
-    // Guardar datos actualizados en mineria.json
-    fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
-
-    let mensaje = `👨‍🏭 *${pickRandom(trabajo)}*  
-🎖 *Recompensas obtenidas:*  
-▢ *${xp}* 💫 XP  
-▢ *${barbozaCoins}* 🪙 Monedas  
-▢ *${diamantes}* 💎 Diamantes  
-▢ *${dulces}* 🍬 Dulces`; // Cambié '${dulce}' a '${dulces}'
-
-    cooldowns[who] = Date.now();
-    await conn.reply(m.chat, mensaje, m);
+let handler = async (m, { conn, usedPrefix, command }) => {
+   let user = global.db.data.users[m.sender]
+   let amount = Math.floor(Math.random() * (5 - 10) + 10) + 1
+   const tiempoEspera = 5 * 60 // 5 minutos
+  if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempoEspera * 1000) {
+    const tiempoRestante = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempoEspera * 1000 - Date.now()) / 1000))
+    m.reply(`🕜 Espera *${tiempoRestante}* para volver a Trabajar.`)
+    return
+  }
+   let work = works.getRandom()
+   user.limit += amount
+   await m.reply(`${work} *${amount} 🍬 Dulces.*`)
+   cooldowns[m.sender] = Date.now()
 }
 
-handler.help = ['trabajar'];
-handler.tags = ['economy'];
-handler.command = ['w', 'work', 'chambear', 'chamba', 'trabajar'];
-handler.group = true;
-handler.register = true;
-
-export default handler;
+handler.help = ['work']
+handler.tags = ['rpg']
+handler.command = ['work', 'trabajar', 'w']
+handler.register = true 
+export default handler
 
 function segundosAHMS(segundos) {
-    let minutos = Math.floor((segundos % 3600) / 60);
-    let segundosRestantes = segundos % 60;
-    return `${minutos} minutos y ${segundosRestantes} segundos`; // Cambié ${minutos} por `${minutos}`
+  const minutos = Math.floor((segundos % 3600) / 60)
+  const segundosRestantes = segundos % 60
+  return `${minutos} minutos y ${segundosRestantes} segundos`
 }
 
-function pickRandom(list) {
-    return list[Math.floor(list.length * Math.random())];
-}
-
-// Lista de trabajos
-const trabajo = [
-   "Trabajaste como cortador de galletas y ganaste",
-   "Trabajaste en una empresa militar privada, ganando",
-   "Organizaste un evento de cata de vinos y obtuviste",
-   "Limpiaste una chimenea y encontraste",
-   "Desarrollaste juegos y ganaste",
+// Thanks to FG98
+const works = [
+   "Trabajas como cortador de galletas y ganas",
+   "Trabaja para una empresa militar privada, ganando",
+   "Organiza un evento de cata de vinos y obtienes",
+   "Limpias la chimenea y encuentras",
+   "Desarrollas juegos para ganarte la vida y ganas",
    "Trabajaste en la oficina horas extras por",
-   "Trabajaste en un Pizza Hut local y ganaste",
-   "Diseñaste un logo para una empresa y recibiste",
+   "Trabajas como secuestrador de novias y ganas",
+   "Alguien vino y representó una obra de teatro. Por mirar te dieron",
+   "Compraste y vendiste artículos y ganaste",
+   "Trabajas en el restaurante de la abuela como cocinera y ganas",
+   "Trabajas 10 minutos en un Pizza Hut local. Ganaste",
+   "Trabajas como escritor(a) de galletas de la fortuna y ganas",
+   "Revisas tu bolso y decides vender algunos artículos inútiles que no necesitas. Resulta que toda esa basura valía",
+   "Desarrollas juegos para ganarte la vida y ganas",
+   "Trabajas todo el día en la empresa por",
+   "Diseñaste un logo para una empresa por",
+   "¡Trabajó lo mejor que pudo en una imprenta que estaba contratando y ganó su bien merecido!",
+   "Trabajas como podador de arbustos y ganas",
+   "Trabajas como actor de voz para Bob Esponja y te las arreglaste para ganar",
+   "Estabas cultivando y Ganaste",
+   "Trabajas como constructor de castillos de arena y ganas",
+   "Trabajas como artista callejera y ganas",
+   "¡Hiciste trabajo social por una buena causa! por tu buena causa Recibiste",
+   "Reparaste un tanque T-60 averiado en Afganistán. La tripulación te pagó",
+   "Trabajas como ecologista de anguilas y ganas",
+   "Trabajas en Disneyland como un panda disfrazado y ganas",
+   "Reparas las máquinas recreativas y recibes",
+   "Hiciste algunos trabajos ocasionales en la ciudad y ganaste",
+   "Limpias un poco de moho tóxico de la ventilación y ganas",
+   "Resolviste el misterio del brote de cólera y el gobierno te recompensó con una suma de",
+   "Trabajas como zoólogo y ganas",
    "Vendiste sándwiches de pescado y obtuviste",
-   "Trabajaste como actor de voz para Bob Esponja y ganaste",
-   "Reparaste un tanque en Afganistán y la tripulación te pagó",
-   "Trabajaste en Disneyland disfrazado de panda y ganaste",
-   "Hiciste trabajos ocasionales y ganaste",
-   "Limpiaste moho tóxico de la ventilación y recibiste",
-   "Resolvió el misterio del brote de cólera y el gobierno te recompensó",
-   "Trabajaste como ecologista de anguilas y ganaste",
-   "Reparaste máquinas recreativas y recibiste",
-   "Trabajaste como artista callejero y ganaste",
-   "Hiciste trabajo social y recibiste una recompensa",
-   "Fuiste agricultor y obtuviste",
-   "Trabajaste como constructor de castillos de arena y ganaste"
-];
+   "Reparas las máquinas recreativas y recibes",
+] 
