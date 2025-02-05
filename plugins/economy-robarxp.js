82e2d2f48ef0e31c39576f2e12be809e59e95500
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-02-05 19:09:24 -0400

    Actualizar economy-robarxp.js

--- plugins/economy-robarxp.js
+++ plugins/economy-robarxp.js
@@ -1,26 +1,24 @@
-
 else who = m.chat;
-if (!who) {
+  if (!who) {
   conn.reply(m.chat, *👤 𝑬𝒕𝒊𝒒𝒖𝒆𝒕𝒂 𝒂 𝒂𝒍𝒈𝒖𝒊𝒆𝒏 𝒑𝒂𝒓𝒂 𝒓𝒐𝒃𝒂𝒓.*, m, rcanal)
   return;
-};
-if (!(who in global.db.data.users)) { 
+    };
+  if (!(who in global.db.data.users)) { 
   conn.reply(m.chat, *👤 𝑬𝒍 𝒖𝒔𝒖𝒂𝒓𝒊𝒐 𝒏𝒐 𝒔𝒆 𝒆𝒏𝒄𝒖𝒆𝒏𝒕𝒓𝒂 𝒆𝒏 𝒎𝒊 𝒃𝒂𝒔𝒆 𝒅𝒆 𝒅𝒂𝒕𝒐𝒔 😕.*, m, rcanal)
-  return;
-}
-const users = global.db.data.users[who];
-const rob = Math.floor(Math.random() * ro);
-if (users.candies < rob) return conn.reply(m.chat, 😔 @${who.split@[0]} 𝒕𝒊𝒆𝒏𝒆 𝒎𝒆𝒏𝒐𝒔 𝒅𝒆 *${ro} dulces*\n𝑵𝒐 𝒓𝒐𝒃𝒆𝒔 𝒂 𝒖𝒏 𝒑𝒐𝒃𝒓𝒆 v":, m, {mentions: [who]});
-global.db.data.users[m.sender].candies += rob;
-global.db.data.users[who].candies -= rob;
-conn.reply(m.chat, *✨ 𝑹𝒐𝒃𝒂𝒔𝒕𝒆 ${rob} 𝑫𝒖𝒍𝒄𝒆𝒔 𝒂 @${who.split@[0]}*, m, {mentions: [who]});
-global.db.data.users[m.sender].lastrob = new Date * 1;
+return;
+  }
+  const users = global.db.data.users[who];
+  const rob = Math.floor(Math.random() * ro);
+  if (users.exp < rob) return conn.reply(m.chat, 😔 @${who.split@[0]} 𝒕𝒊𝒆𝒏𝒆 𝒎𝒆𝒏𝒐𝒔 𝒅𝒆 *${ro} xp*\n𝑵𝒐 𝒓𝒐𝒃𝒆𝒔 𝒂 𝒖𝒏 𝒑𝒐𝒃𝒓𝒆 v":, m, {mentions: [who]});
+  global.db.data.users[m.sender].exp += rob;
+  global.db.data.users[who].exp -= rob;
+  conn.reply(m.chat, *✨ 𝑹𝒐𝒃𝒂𝒔𝒕𝒆 ${rob} XP 𝒂 @${who.split@[0]}*, m, {mentions: [who]});
+  global.db.data.users[m.sender].lastrob = new Date * 1;
 };
-handler.help = ['robar'];
+handler.help = ['rob'];
 handler.tags = ['economy'];
 handler.command = ['robar', 'rob'];
 export default handler;
-
 function msToTime(duration) {
   const milliseconds = parseInt((duration % 1000) / 100);
   let seconds = Math.floor((duration / 1000) % 60);
