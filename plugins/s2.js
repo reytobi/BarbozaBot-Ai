7cb3499bac490970c3b195a4d4f3a1d06dd3cb05
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-02-09 22:10:08 -0400

    Actualizar s2.js

--- plugins/s2.js
+++ plugins/s2.js
@@ -1,17 +1,16 @@
 
 let handler = async (m) => {
     const videos = [
-        
-
-         'https://qu.ax/kZlnj.mp4',
-
+        'https://qu.ax/kZlnj.mp4',
     ];
 
     // Elegir un video aleatorio
     const randomVideo = videos[Math.floor(Math.random() * videos.length)];
 
-    // Enviar el video al chat
-    await conn.sendMessage(m.chat, { video: { url: randomVideo }, caption: "¡Aquí tienes un video para disfrutar!" }, { quoted: m });
+    // Enviar el video varias veces
+    for (let i = 0; i < 3; i++) { // Cambia 3 por el número de veces que desees
+        await conn.sendMessage(m.chat, { video: { url: randomVideo }, caption: "¡Aquí tienes un video para disfrutar!" }, { quoted: m });
+    }
 }
 
 handler.help = ['videoxxx'];
