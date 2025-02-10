f544809eaba21c5bf30bfee8989453b36c21fc2e
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-02-10 17:25:10 -0400

    Actualizar fun-memes.js

--- plugins/fun-memes.js
+++ plugins/fun-memes.js
@@ -1,14 +1,36 @@
-/* ig : https://www.instagram.com/fg98._/ */
-import hispamemes from 'hispamemes'
-let handler = async (m, { conn, usedPrefix, command }) => {
-const meme = hispamemes.meme()
-conn.sendFile(m.chat, meme, '', '', fkontak)
-m.react(emoji2)
+
+let handler = async (m) => {
+    const memes = [
+        'https://i.imgur.com/1.jpg', // Reemplaza con enlaces a tus memes
+        'https://qu.ax/dpYLN.jpg',
+        'https://qu.ax/YvLWt.jpg',
+        'https://qu.ax/FxBzq.jpg',
+        'https://qu.ax/oRkAi.jpg',
+        'https://qu.ax/Gfnrz.jpg',
+        'https://qu.ax/UFWsB.jpg',
+        'https://qu.ax/rubYe.jpg',
+        'https://qu.ax/UFWsB.jpg',
+        'https://qu.ax/uyjpK.jpg',
+        'https://qu.ax/RcxFR.jpg',
+        'https://qu.ax/MctMj.jpg',
+        'https://qu.ax/znbWC.jpg',
+        'https://qu.ax/lLJMP.jpg'
+        'https://qu.ax/HhOVP.jpg',
+        'https://qu.ax/yQoQW.jpg',
+        'https://qu.ax/msDFZ.jpg',
+        'https://qu.ax/MTDhM.jpg',
+        'https://qu.ax/hFQOL.jpg',
+    ];
+
+    // Elegir un meme aleatorio
+    const randomMeme = memes[Math.floor(Math.random() * memes.length)];
+
+    // Enviar el meme al chat
+    await conn.sendMessage(m.chat, { image: { url: randomMeme }, caption: "¡Aquí tienes un meme para alegrar tu día! 😂" }, { quoted: m });
 }
-handler.help = ['meme']
-handler.tags = ['fun']
-handler.command = ['meme', 'memes']
-handler.yenes = 1
-handler.group = true;
-handler.register = true
-export default handler
+
+handler.help = ['meme'];
+handler.tags = ['diversión'];
+handler.command = ['meme'];
+
+export default handler;
