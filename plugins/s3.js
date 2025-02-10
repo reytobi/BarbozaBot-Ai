0a0363c9a38c95f96f6cfac9cd5562137b4f27ed
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-02-10 17:49:58 -0400

    Actualizar s3.js

--- plugins/s3.js
+++ plugins/s3.js
@@ -2,8 +2,11 @@
 let handler = async (m) => {
     const memes = [
         'https://qu.ax/DksQt.mp4',
-        '',
-        '',
+        'https://qu.ax/vDgQd.mp4',
+        'https://qu.ax/jpiBe.mp4',
+        'https://qu.ax/TiecS.mp4',
+        'https://qu.ax/LWJCF.mp4',
+        'https://qu.ax/euTXj.mp4',
         // Agrega más enlaces de memes aquí
     ];
 
