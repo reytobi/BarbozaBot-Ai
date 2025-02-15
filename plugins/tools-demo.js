71f63998b15fbeebb9b9458dadd6b00c39473f6a
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-02-15 17:12:12 -0400

    Actualizar tools-demo.js

--- plugins/tools-demo.js
+++ plugins/tools-demo.js
@@ -14,7 +14,7 @@ isForwarded: false,
 "externalAdReply": {
 "showAdAttribution": true,
 "containsAutoReply": true,
-title: `[ CrowBot -By|Niño Piña ]`,
+title: `[ Barboza -By|Bot Barboza ]`,
 body: ``,
 "previewType": "PHOTO",
 thumbnailUrl: 'https://tinyurl.com/2awg2bch', 
