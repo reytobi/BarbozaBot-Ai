ce5e54573da5fe15925222a55ea6bf786fbf43a6
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-02-17 12:57:31 -0400

    Actualizar downloader-soundcloud.js

--- plugins/downloader-soundcloud.js
+++ plugins/downloader-soundcloud.js
@@ -175,7 +175,6 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 
 handler.help = ['play'];
 handler.tags = ['downloader'];
-handler.limit = 3;
 handler.command = /^(applemusicplay|play|song)$/i;
 
 export default handler;
