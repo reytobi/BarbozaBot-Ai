d2d87965f1435dc47dc6bc7a36e9ec8affad1f93
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-02-21 22:09:30 -0400

    Actualizar rg-reg.js

--- plugins/rg-reg.js
+++ plugins/rg-reg.js
@@ -1,25 +1,59 @@
-const registerHandler = async (m, { conn, usedPrefix }) => {
-    // Botón de Menú
-    const buttons = [
-        {
-            buttonId: `${usedPrefix}menu`,
-            buttonText: { displayText: "📜 Menú" },
-            type: 1
-        }
-    ];
 
-    await conn.sendMessage(
-        m.chat,
-        {
-            text: "🎉 ¡Te has registrado!\n\nPresiona el botón para ver el menú.",
-            buttons: buttons,
-            viewOnce: true
+import { createHash } from 'crypto'
+
+let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
+let handler = async function (m, { conn, text, usedPrefix, command }) {
+    let user = global.db.data.users[m.sender]
+    let name2 = conn.getName(m.sender)
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
+    let pp = await this.profilePictureUrl(who, 'image').catch(_ => 'https://i.ibb.co/1fx3bv01/file.jpg')
+
+    if (user.registered === true) throw *⚠️ Ya estás registrado*\n\n¿Quiere volver a registrarse?\n\n💬 Use este comando para *eliminar su registro*\n*${usedPrefix}unreg* <Número de serie>
+    if (!Reg.test(text)) throw *⚠️ Formato incorrecto*\n\n📝 Uso del comando: *${usedPrefix + command} nombre.edad*\n💡 Ejemplo : *${usedPrefix + command}* ${name2}.18
+
+    let [_, name, splitter, age] = text.match(Reg)
+    if (!name) throw '*📝 El nombre no puede estar vacío*'
+    if (!age) throw '*📝 La edad no puede estar vacía*'
+    if (name.length >= 30) throw '*⚠️ El nombre es demasiado largo*' 
+    age = parseInt(age)
+    if (age > 100) throw '*👴🏻 Wow el abuelo quiere jugar al bot*'
+    if (age < 5) throw '*👀 hay un bebé jsjsj*'
+
+    user.name = name.trim()
+    user.age = age
+    user.regTime = + new Date
+    user.registered = true
+
+    if (!user.limit) user.limit = 0;
+    user.limit += 10;
+
+    let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)        
+    m.react('📩') 
+
+    let regbot =🗃️ *R E G I S T R A D O* 🗃️\n
+💌 *Nombre:* ${name}
+📆 *Edad* : ${age} años
+🍬 *Dulces añadidos:* 10
+
+    // Agregar el botón de menú
+    const menuButton = {
+        buttonText: {
+            displayText: '.menu'
         },
-        { quoted: m }
-    );
-};
+        type: 1,
+        url: 'https://f.uguu.se/TQHDjOFW.jpg' // Cambia esto por la URL o mensaje que quieras enviar como menú.
+    };
 
-// Asignar comando "register"
-registerHandler.command = /^register$/i;
+    await conn.sendMessage(m.chat, { 
+        image: { url: pp }, 
+        caption: regbot,
+        buttons: [menuButton],
+        footer: 'Presiona el botón para ver el menú.'
+    }, { quoted: m })
+}
 
-export default registerHandler;
+handler.help = ['reg']
+handler.tags = ['rg']
+handler.command = ['verify', 'reg', 'verificar'] 
+
+export default handler
