8e852375b73cd749a39bb28fa1ac0f9aa17bbfd8
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-02-22 19:02:31 -0400

    Actualizar fun-memes.js

--- plugins/fun-memes.js
+++ plugins/fun-memes.js
@@ -1,5 +1,4 @@
-
-let handler = async (m) => {
+let handler = async (m, { conn, usedPrefix }) => {
     const memes = [
         'https://qu.ax/dpYLN.jpg',
         'https://qu.ax/YvLWt.jpg',
@@ -8,12 +7,11 @@ let handler = async (m) => {
         'https://qu.ax/Gfnrz.jpg',
         'https://qu.ax/UFWsB.jpg',
         'https://qu.ax/rubYe.jpg',
-        'https://qu.ax/UFWsB.jpg',
         'https://qu.ax/uyjpK.jpg',
         'https://qu.ax/RcxFR.jpg',
         'https://qu.ax/MctMj.jpg',
         'https://qu.ax/znbWC.jpg',
-        'https://qu.ax/lLJMP.jpg'
+        'https://qu.ax/lLJMP.jpg',
         'https://qu.ax/HhOVP.jpg',
         'https://qu.ax/yQoQW.jpg',
         'https://qu.ax/msDFZ.jpg',
@@ -21,12 +19,27 @@ let handler = async (m) => {
         'https://qu.ax/hFQOL.jpg',
     ];
 
-    // Elegir un meme aleatorio
     const randomMeme = memes[Math.floor(Math.random() * memes.length)];
 
-    // Enviar el meme al chat
-    await conn.sendMessage(m.chat, { image: { url: randomMeme }, caption: "¡Aquí tienes un meme para alegrar tu día! 😂" }, { quoted: m });
-}
+    const buttons = [
+        {
+            buttonId: `${usedPrefix}meme`,
+            buttonText: { displayText: "🔄 Ver más" },
+            type: 1
+        }
+    ];
+
+    await conn.sendMessage(
+        m.chat,
+        {
+            image: { url: randomMeme },
+            caption: "¡Aquí tienes un meme para alegrar tu día! 😂",
+            buttons: buttons,
+            viewOnce: true
+        },
+        { quoted: m }
+    );
+};
 
 handler.help = ['meme'];
 handler.tags = ['diversión'];
