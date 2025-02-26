80c40dc8b9f7ce54d03d116660f90ad885f00bfa
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-02-25 21:08:55 -0400

    Actualizar Barboza-playdoc.js

--- plugins/Barboza-playdoc.js
+++ plugins/Barboza-playdoc.js
@@ -69,7 +69,7 @@ const ddownr = {
 const handler = async (m, { conn, text, usedPrefix, command }) => {
   try {
     if (!text.trim()) {
-      return conn.reply(m.chat, `🍬 y el link?😧`, m);
+      return conn.reply(m.chat, `✎ ingresa el nombre de la música a descargar.`, m);
     }
 
     const search = await yts(text);
@@ -80,7 +80,13 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
     const videoInfo = search.all[0];
     const { title, thumbnail, timestamp, views, ago, url } = videoInfo;
     const vistas = formatViews(views);
-    const infoMessage = `*YOUTUBE - MP3*\n\n🎬 Título: *${title}*\n*°.⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.°*\n> 🕒 Duración: *${timestamp}*\n*°.⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.°*\n> 👀 Vistas: *${vistas}*\n*°.⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.°*\n> 🍬 Canal: *${videoInfo.author.name || 'Desconocido'}*\n*°.⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.°*\n> 📆 Publicado: *${ago}*\n*°.⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.°*\n> 🔗 Enlace: ${url}`;
+    const infoMessage = `•••] ✞ ◜YouTube Play◞ ✞ [•••
+
+   📌 *🎬  Título : » ${videoInfo.title}
+   👀 *💯 Vistas: » ${videoInfo.views}
+   😍 *⏳ Duración : » ${videoInfo.timestamp}
+   📅 *🕒 Publicado : » ${videoInfo.ago}
+   🔗 *🌐 URL : » ${videoInfo.url}`;
     const thumb = (await conn.getFile(thumbnail))?.data;
 
     const JT = {
@@ -98,14 +104,14 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
       },
     };
 
-    await conn.reply(m.chat, infoMessage, m, JT);
+    //await conn.reply(m.chat, infoMessage, m, JT);
 
-    if (command === 'yta' || command === 'ytmp3') {
+    if (command === 'play' || command === 'yta' || command === 'ytmp3') {
         const api = await ddownr.download(url, 'mp3');
         const result = api.downloadUrl;
         await conn.sendMessage(m.chat, { audio: { url: result }, mimetype: "audio/mpeg" }, { quoted: m });
 
-    } else if (command === 'ytv' || command === 'ytmp4') {
+    } else if (command === 'play2' || command === 'ytv' || command === 'ytmp4') {
       let sources = [
         `https://api.siputzx.my.id/api/d/ytmp4?url=${url}`,
         `https://api.zenkey.my.id/api/download/ytmp4?apikey=zenkey&url=${url}`,
@@ -126,7 +132,7 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
               video: { url: downloadUrl },
               fileName: `${title}.mp4`,
               mimetype: 'video/mp4',
-              caption: `${dev}`,
+              caption: `✎﹏Aqui tienes tu video`,
               thumbnail: thumb
             }, { quoted: m });
             break;
@@ -137,7 +143,7 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
       }
 
       if (!success) {
-        return m.reply(`🍭︎ *No se pudo descargar el video:* No se encontró un enlace de descarga válido.`);
+        return m.reply(` ✱ *No se pudo descargar el video:* No se encontró un enlace de descarga válido.`);
       }
     } else {
       throw "Comando no reconocido.";
@@ -147,9 +153,8 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
   }
 };
 
-handler.command = handler.help = ['ytmp3', 'yta'];
-handler.tags = ['descargas'];
-handler.estrellas = 6;
+handler.command = handler.help = ['ytmp3', 'yta', 'ytmp4', 'ytv'];
+handler.tags = ['downloader'];
 
 export default handler;
 
