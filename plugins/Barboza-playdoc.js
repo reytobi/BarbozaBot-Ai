eb269ed62069619ef9c7e96d7537a1a6c26e0f20
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-02-28 12:28:38 -0400

    Actualizar Barboza-playdoc.js

--- plugins/Barboza-playdoc.js
+++ plugins/Barboza-playdoc.js
@@ -132,7 +132,6 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
 
 handler.command = handler.help = ['ytmp3', 'yta', 'ytmp4', 'ytv'];
 handler.tags = ['downloader'];
-handler.group = true;
 
 export default handler;
 
