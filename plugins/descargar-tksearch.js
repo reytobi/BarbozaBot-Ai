f2e7e82b27cd4ee32fa4269ff0b157bd4399f6cc
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-02-28 16:28:17 -0400

    Actualizar descargar-tksearch.js

--- plugins/descargar-tksearch.js
+++ plugins/descargar-tksearch.js
@@ -62,7 +62,7 @@ async function sendVideoWithButtons(session, m, conn, usedPrefix) {
 
     const caption = session.currentIndex === 0 
         ? `✅ Usa el botón para ver más videos.\n\n_*©Prohibido La Copia, Código Oficial De MediaHub™*_`
-        : `_*©Prohibido La Copia, Código Oficial De MediaHub™*_`;
+        : `_*©Prohibido La Copia, Código Oficial De Barboza Bot™*_`;
 
     try {
         const buttons = [];
