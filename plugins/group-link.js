821c030f87cda6443aa097e8509fea50034ec7b4
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-03-05 21:44:36 -0400

    Actualizar group-link.js

--- plugins/group-link.js
+++ plugins/group-link.js
@@ -1,15 +1,19 @@
+
 var handler = async (m, { conn, args }) => {
-
-let group = m.chat
-let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-conn.reply(m.chat, '🔗\v' + link, m, rcanal, { detectLink: true })
-
+    try {
+        let group = m.chat;
+        let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group);
+        conn.reply(m.chat, '🔗 ' + link, m, { detectLink: true });
+    } catch (error) {
+        conn.reply(m.chat, 'Error al obtener el enlace del grupo. Asegúrate de que soy administrador y estoy en un grupo.', m);
+    }
 }
-handler.help = ['link']
-handler.tags = ['grupo']
-handler.command = ['link','linkgroup']
 
-handler.group = true
-handler.botAdmin = true
+handler.help = ['link'];
+handler.tags = ['grupo'];
+handler.command = ['link', 'linkgroup'];
 
-export default handler
+handler.group = true;
+handler.botAdmin = true;
+
+export default handler;
