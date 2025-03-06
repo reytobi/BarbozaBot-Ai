09855f45b98f05ed07ec4d2da9e77abaa1a479dc
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-03-05 21:31:39 -0400

    Actualizar owner-reunion.js

--- plugins/owner-reunion.js
+++ plugins/owner-reunion.js
@@ -1,15 +1,13 @@
 let handler = async(m, { conn, command, text }) => {
-  if (!text) return m.reply(`❗𝙸𝙽𝙶𝚁𝙴𝚂𝙰 𝙴𝙻 𝙼𝙾𝚃𝙸𝚅𝙾 𝙿𝙰𝚁𝙰 𝙻𝙰 𝚁𝙴𝚄𝙽𝙸𝙾𝙽 𝙳𝙴𝙻 𝚂𝚃𝙰𝙵𝙵`)
-    if (text.length < 11) return m.reply(`❗ 𝙸𝙽𝙶𝚁𝙴𝚂𝙰 𝙰𝙻 𝙼𝙴𝙽𝙾𝚂 11 𝙲𝙰𝚁𝙰𝙲𝚃𝙴𝚁𝙴𝚂 𝙿𝙰𝚁𝙰 𝙴𝙻 𝙼𝙾𝚃𝙸𝚅𝙾 𝙳𝙴 𝙻𝙰 𝚁𝙴𝚄𝙽𝙸𝙾𝙽`)
+  if (!text) return m.reply(`🍬 Por favor, ingresa el motivo de la reunión.`)
+    if (text.length < 11) return m.reply(`🍭 Por favo, ingresé al menos 11 caracteres.`)
 
-let texto = `*_🐶 𝙴𝙻 𝙾𝚆𝙽𝙴𝚁 @${m.sender.split`@`[0]} 𝙰 𝙴𝙽𝙿𝙴𝚉𝙰𝙳𝙾 𝚄𝙽𝙰 𝚁𝙴𝚄𝙽𝙸𝙾𝙽 𝙴𝙽𝚃𝚁𝙰𝚁 𝙻𝙾 𝙼𝙰𝚂 𝙿𝚁𝙾𝙽𝚃𝙾 𝙰𝙻 𝙶𝚁𝚄𝙿𝙾 𝙳𝙴𝙻 𝚂𝚃𝙰𝙵𝙵 𝙳𝙴 Bot Barboza _*\n*➪ 𝙼𝙾𝚃𝙸𝚅𝙾: ${text}*`
-m.reply('*_🚀 Enviando mensaje de reunión a todos los owners de Bot Barboza._*')
+let texto = `🍨 El Owner @${m.sender.split`@`[0]} a empezado una reunión entra lo más pronto al grupo del staff...\n*➪ Motivo: ${text}*`
+m.reply('🍭 Enviando mensaje de reunión a todos los owners.')
 for (let [jid] of global.owner.filter(([number, _, isDeveloper]) => isDeveloper && number)) {
                         let data = (await conn.onWhatsApp(jid))[0] || {}
                         if (data.exists)
                         conn.sendPayment(data.jid, '999999999', texto, m)
-
-                           // conn.reply(data.jid, text, m, { mentions: [m.sender] })
                     }
 
 }
