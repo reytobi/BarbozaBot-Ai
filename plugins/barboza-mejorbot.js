c0cb74835f7b1984868ac16b1e5a0dfb9997b39b
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-03-07 17:07:38 -0400

    Delete plugins/barboza-mejorbot.js

--- plugins/barboza-mejorbot.js
+++
@@ -1,6 +0,0 @@
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-let str = `Hola🤨🚩`.trim()
-conn.sendMessage(m.chat, { video: { url: './media/edar.mp4' }, gifPlayback: true, caption: str.trim(), mentions: [m.sender] }, { quoted: m })
-}
-handler.command = /^(Barboza)$/i;
-export default handler;
