99b047a96003f351d032b583345fb708d69ac499
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-03-14 17:09:11 -0400

    Actualizar rg-reg.js

--- plugins/rg-reg.js
+++ plugins/rg-reg.js
@@ -1,88 +1,102 @@
-import { createHash } from 'crypto'
+import db from '../lib/database.js'
+import fs from 'fs'
 import PhoneNumber from 'awesome-phonenumber'
-// import _ from "lodash"
+import { createHash } from 'crypto'  
+import fetch from 'node-fetch'
+
 let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
+
 let handler = async function (m, { conn, text, usedPrefix, command }) {
-let user = global.db.data.users[m.sender]
-let name2 = conn.getName(m.sender)
-  /*let delirius = await axios.get(`https://deliriussapi-oficial.vercel.app/tools/country?text=${PhoneNumber('+' + m.sender.replace('@s.whatsapp.net', '')).getNumber('international')}`)
-  let paisdata = delirius.data.result
-  let mundo = paisdata ? `${paisdata.name} ${paisdata.emoji}` : 'Desconocido'*/
-  let perfil = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://files.catbox.moe/22mlg6.jpg')
-  let bio = 0, fechaBio
- // let who2 = m.isGroup ? _.get(m, "mentionedJid[0]", m.quoted?.sender || m.sender) : m.sender
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let mentionedJid = [who]
+ let bio = 0, fechaBio
   let sinDefinir = '😿 Es privada'
   let biografia = await conn.fetchStatus(m.sender).catch(() => null)
   if (!biografia || !biografia[0] || biografia[0].status === null) {
-  bio = sinDefinir
-  fechaBio = "Fecha no disponible"
-  } else {
-  bio = biografia[0].status || sinDefinir
-  fechaBio = biografia[0].setAt ? new Date(biografia[0].setAt).toLocaleDateString("es-ES", { day: "2-digit", month: "2-digit", year: "numeric", }) : "Fecha no disponible"
-  }
-if (user.registered === true) {
-  return m.reply(`『 ✎ 』 *YA ESTÁS REGISTRADO.*\n\n*¿QUIERES HACERLO DE NUEVO?*\n\n> Usa este comando para eliminar tu registro:\n*${usedPrefix}unreg*`)
+   bio = sinDefinir
+   fechaBio = "Fecha no disponible"
+} else {
+bio = biografia[0].status || sinDefinir
+fechaBio = biografia[0].setAt ? new Date(biografia[0].setAt).toLocaleDateString("es-ES", { day: "2-digit", month: "2-digit", year: "numeric", }) : "Fecha no disponible"
 }
-if (!Reg.test(text)) throw `*『✦』El comando ingresado es incorrecto, úsalo de la siguiente manera:*\n\n#reg *Nombre.edad*\n\n\`\`\`Ejemplo:\`\`\`\n#reg *${name2}.10000*`
-let [_, name, splitter, age] = text.match(Reg)
-if (!name) throw '*『✦』No puedes registrarte sin nombre, el nombre es obligatorio. Inténtelo de nuevo.*'
-if (!age) throw '*『✦』No puedes registrarte sin la edad, la edad es opcional. Inténtelo de nuevo.*'
-if (name.length >= 30) throw '*『✦』El nombre no debe de tener más de 30 caracteres.*' 
-age = parseInt(age)
-if (age > 10000) throw '*『😏』Viejo/a Sabroso/a*'
-if (age < 5) throw '*『🍼』Ven aquí, ¡te adoptaré!*'
-user.name = name.trim()
-user.age = age
-user.descripcion = bio
-// user.persona = age >= 18? '(Persona adulta)' : '(Persona joven)'
-user.regTime = + new Date
-user.registered = true
-global.db.data.users[m.sender].money += 600
-global.db.data.users[m.sender].dragones += 10
-global.db.data.users[m.sender].exp += 245
-global.db.data.users[m.sender].joincount += 5
-let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 20)        
-m.react('📩') 
-let regbot = `👤 𝗥 𝗘 𝗚 𝗜 𝗦 𝗧 𝗥 𝗢 👤
-⌬━━━━▣━━◤◢━━▣━━━━━⌬
-「👑」𝗡𝗼𝗺𝗯𝗿𝗲: ${name}
-「⭐」𝗘𝗱𝗮𝗱: ${age} años
-⌬━━━━▣━━◤◢━━▣━━━━━⌬
-「🎁」𝗥𝗲𝗰𝗼𝗺𝗽𝗲𝗻𝘀𝗮𝘀:
-• 15 monedas 
-• 5 Coins 🪙
-• 245 Experiencia ✨
-• 12 Tokens ⚜️
+  let perfil = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://qu.ax/Tpmzb.jpg')
+  let pp = await conn.profilePictureUrl(who, 'image').catch((_) => 'https://qu.ax/Tpmzb.jpg')
+  let user = global.db.data.users[m.sender]
+  let name2 = conn.getName(m.sender)
+  if (user.registered === true) return m.reply(`✧ Ya estás registrado.\n\n*¿Quiere volver a registrarse?*\n\nUse este comando para eliminar su registro.\n*${usedPrefix}unreg*`)
+  if (!Reg.test(text)) return m.reply(`✧ Formato incorrecto.\n\nUso del comamdo: *${usedPrefix + command} nombre.edad*\nEjemplo : *${usedPrefix + command} ${name2}.18*`)
+  let [_, name, splitter, age] = text.match(Reg)
+  if (!name) return m.reply('✧ El nombre no puede estar vacío.')
+  if (!age) return m.reply('✧ La edad no puede estar vacía.')
+  if (name.length >= 100) return m.reply('🍭 El nombre es demasiado largo.' )
+  age = parseInt(age)
+  if (age > 1000) return m.reply('✧ Wow el abuelo quiere jugar al bot.')
+  if (age < 5) return m.reply('✧ hay un abuelo bebé jsjsj. ')
+  user.name = name + '✓'.trim()
+  user.age = age
+  user.descripcion = bio 
+  user.regTime = + new Date      
+  user.registered = true
+  global.db.data.users[m.sender].coin += 500
+  global.db.data.users[m.sender].exp += 600
+  global.db.data.users[m.sender].joincount += 50
+  let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 20)
+let regbot = `❀ 𝗥 𝗘 𝗚 𝗜 𝗦 𝗧 𝗥 𝗔 𝗗 𝗢 ❀\n`
+regbot += `•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•\n`
+regbot += `「✰」𝗡𝗼𝗺𝗯𝗿𝗲 » ${name}\n`
+regbot += `「✦」𝗘𝗱𝗮𝗱 » ${age} años\n`
+regbot += `•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•\n`
+regbot += `「𖣔」 𝗥𝗲𝗰𝗼𝗺𝗽𝗲𝗻𝘀𝗮𝘀:\n`
+regbot += `> • ⛁ *${moneda}* » 500\n`
+regbot += `> • ✩ *Experiencia* » 600\n`
+regbot += `> • ❀ *Tokens* » 50\n`
+regbot += `•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•\n`
+regbot += `${dev}
 
-ᴠᴇʀɪғɪᴄᴀ ᴛᴜ ʀᴇɢɪᴛʀᴏ ᴀϙᴜɪ:
-${120363414007802886@newsletter}
-⌬━━━━▣━━◤◢━━▣━━━━━⌬
-${packname}`
-await conn.sendMini(m.chat, '⊱『✅𝆺𝅥 𝗥𝗘𝗚𝗜𝗦𝗧𝗥𝗔𝗗𝗢(𝗔) 𝆹𝅥✅』⊰', textbot, regbot, imagen1, imagen1, channel, m)
-//await m.reply(`${sn}`)   
+❥⊱〰︎ *sigue el canal oficial* 〰︎⊱❥
+https://whatsapp.com/channel/0029Vagdmfv1SWt5nfdR4z3w`
 
-let chtxt = `
-✐ *𝚄𝚜𝚎𝚛* » ${m.pushName || 'Anónimo'}
-⌨ *𝚅𝚎𝚛𝚒𝚏𝚒𝚌𝚊𝚌𝚒𝚘́𝚗* » ${user.name}
-⍰ *𝙴𝚍𝚊𝚍* » ${user.age} años
-✐ *𝙳𝚎𝚜𝚌𝚛𝚒𝚙𝚌𝚒𝚘𝚗* » ${user.descripcion} 
-♛ *𝚄𝚕𝚝𝚒𝚖𝚊 𝙼𝚘𝚍𝚒𝚏𝚒𝚌𝚊𝚌𝚒𝚘𝚗* » ${fechaBio}
-߷︎ *𝙵𝚎𝚌𝚑𝚊* » ${moment.tz('America/Bogota').format('DD/MM/YY')}
-⚡ *𝙽𝚞𝚖𝚎𝚛𝚘 𝚍𝚎 𝚛𝚎𝚐𝚒𝚜𝚝𝚛𝚘* »
-⤷ ${sn}
-`.trim()
+await m.react('📩')
+//await m.reply(mini)
+await conn.sendMessage(m.chat, {
+        text: regbot,
+        contextInfo: {
+            externalAdReply: {
+                title: '✧ Usuario Verificado ✧',
+                body: textbot,
+                thumbnailUrl: pp,
+                sourceUrl: channel,
+                mediaType: 1,
+                showAdAttribution: true,
+                renderLargerThumbnail: true
+            }
+        }
+    }, { quoted: m });
 
-await conn.sendMessage(global.idchannel, { text: chtxt, contextInfo: {
-externalAdReply: {
-title: "【 🔔 𝐍𝐎𝐓𝐈𝐅𝐈𝐂𝐀𝐂𝐈𝐎́𝐍 🔔 】",
-body: '🥳 ¡𝚄𝚗 𝚞𝚜𝚞𝚊𝚛𝚒𝚘 𝚗𝚞𝚎𝚟𝚘 𝚎𝚗 𝚖𝚒 𝚋𝚊𝚜𝚎 𝚍𝚎 𝚍𝚊𝚝𝚘𝚜!',
-thumbnailUrl: perfil,
-sourceUrl: redes,
-mediaType: 1,
-showAdAttribution: false,
-renderLargerThumbnail: false
-}}}, { quoted: null })
-}
+    let chtxt = `👤 *𝚄𝚜𝚎𝚛* » ${m.pushName || 'Anónimo'}
+🗂 *𝚅𝚎𝚛𝚒𝚏𝚒𝚌𝚊𝚌𝚒𝚘́𝚗* » ${user.name}
+⭐️ *𝙴𝚍𝚊𝚍* » ${user.age} años
+👀 *𝙳𝚎𝚜𝚌𝚛𝚒𝚙𝚌𝚒𝚘𝚗* » ${user.descripcion} 
+⏳ *𝚄𝚕𝚝𝚒𝚖𝚊 𝙼𝚘𝚍𝚒𝚏𝚒𝚌𝚊𝚝𝚒𝚘𝚗* » ${fechaBio}
+📆 *𝙵𝚎𝚌𝚑𝚊* » ${moment.tz('America/Bogota').format('DD/MM/YY')}
+☁️ *𝙽𝚞𝚖𝚎𝚛𝚘 𝚍𝚎 𝚛𝚎𝚐𝚒𝚜𝚝𝚛𝚘* »
+⤷ ${sn}`;
+
+    await conn.sendMessage(global.idchannel, {
+        text: chtxt,
+        contextInfo: {
+            externalAdReply: {
+                title: "【 🔔 𝐍𝐎𝐓𝐈𝐅𝐈𝐂𝐀𝐂𝐈𝐎́𝐍 🔔 】",
+                body: '🥳 ¡𝚄𝚗 𝚞𝚜𝚞𝚊𝚛𝚒𝚘 𝚗𝚞𝚎𝚟𝚘 𝚎𝚗 𝚖𝚒 𝚋𝚊𝚜𝚎 𝚍𝚎 𝚍𝚊𝚝𝚘𝚜!',
+                thumbnailUrl: perfil,
+                sourceUrl: redes,
+                mediaType: 1,
+                showAdAttribution: false,
+                renderLargerThumbnail: false
+            }
+        }
+    }, { quoted: null });
+};
 handler.help = ['reg']
 handler.tags = ['rg']
 handler.command = ['verify', 'verificar', 'reg', 'register', 'registrar'] 
