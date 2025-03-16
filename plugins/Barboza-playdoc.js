a5be939957486a7bbac9dd6c633fe44041c49e0e
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-03-16 19:59:00 -0400

    Actualizar Barboza-playdoc.js

--- plugins/Barboza-playdoc.js
+++ plugins/Barboza-playdoc.js
@@ -132,7 +132,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     const fileSizeInMB = fileSize / (1024 * 1024);
 
     const videoInfo = `
-⌘━─━─[August-Ai]─━─━⌘
+⌘━─━─[Barboza]─━─━⌘
 
 ➷ *Título⤿:* ${apiTitle}
 ➷ *Subido⤿:* ${ago}
