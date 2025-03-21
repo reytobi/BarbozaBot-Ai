df2e53d34a77e10825b4905c59c0e7b6690dff31
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-03-21 16:37:19 -0400

    Actualizar mapa-purgatorio.js

--- plugins/mapa-purgatorio.js
+++ plugins/mapa-purgatorio.js
@@ -3,7 +3,7 @@ let handler = async (m, { isPrems, conn }) => {
 let time = global.db.data.users[m.sender].lastcofre + 0 // 36000000 10 Horas //86400000 24 Horas
 if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
 
-let img = 'https://qu.ax/NgYg.jpeg' 
+let img = 'https://qu.ax/CvgEw.jpg' 
 let texto = `» 𝙈𝘼𝙋𝘼 𝘿𝙀 𝙋𝙐𝙍𝙂𝘼𝙏𝙊𝙍𝙄𝙊 𝙁𝙍𝙀𝙀 𝙁𝙄𝙍𝙀 ✅`
 
 const fkontak = {
