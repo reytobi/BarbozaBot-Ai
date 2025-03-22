2abd5473c5926b8ca5fa83d0db03bc232116f9f8
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-03-21 22:15:48 -0400

    Actualizar info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -32,7 +32,7 @@ let handler = async (m, { conn, isRowner }) => {
 
 handler.help = ['status'];
 handler.tags = ['info'];
-handler.command = /^(estado|status|estate|state|stado|stats)$/i;
+handler.command = /^(estado|info|estate|state|stado|stats)$/i;
 handler.register = true;
 export default handler;
 
