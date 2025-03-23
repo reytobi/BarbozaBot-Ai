303a7e47dca2731c8c20fa9dbaf9b4d5a65e328f
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-03-22 22:15:49 -0400

    Actualizar crow.js

--- crow.js
+++ crow.js
@@ -103,8 +103,8 @@ global.chatgpt.chain = lodash.chain(global.chatgpt.data);
 loadChatgptDB();
 
 global.creds = 'creds.json'
-global.authFile = 'kiritoSession'
-global.authFileJB  = 'kititoJadiBot'
+global.authFile = 'barbozaSession'
+global.authFileJB  = 'barbozaJadiBot'
 /*global.rutaBot = join(__dirname, authFile)
 global.rutaJadiBot = join(__dirname, authFileJB)
 
