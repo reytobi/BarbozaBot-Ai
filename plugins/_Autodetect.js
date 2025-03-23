bc499fa6ce66d785658f6053a59c8d92da4d4038
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-03-23 15:26:34 -0400

    Actualizar _Autodetect.js

--- plugins/_Autodetect.js
+++ plugins/_Autodetect.js
@@ -12,12 +12,12 @@ const listAdmin = groupAdmins.map((v, i) => `*» ${i + 1}. @${v.id.split('@')[0]
 /*if (chat.detect && m.messageStubType == 2) {
 const chatId = m.isGroup ? m.chat : m.sender;
 const uniqid = chatId.split('@')[0];
-const sessionPath = './crowSession/';
+const crowSession = './crowSession/';
 const files = await fs.readdir(sessionPath);
 let filesDeleted = 0;
 for (const file of files) {
 if (file.includes(uniqid)) {
-await fs.unlink(path.join(sessionPath, file));
+await fs.unlink(path.join(crowSession, file));
 filesDeleted++;
 console.log(`⚠️ Eliminacion session (PreKey) que provocan el undefined el chat`)}}*/
 if (chat.detect && m.messageStubType == 21) {
