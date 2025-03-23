8a1c00c88bfb8665dcd3740e3d83e230710b8ee3
deylinqff <maycraftminecraft4@gmail.com>
2025-03-22 20:12:36 -0600

    Actualizar crow.js

--- crow.js
+++ crow.js
@@ -103,8 +103,8 @@ global.chatgpt.chain = lodash.chain(global.chatgpt.data);
 loadChatgptDB();
 
 global.creds = 'creds.json'
-global.authFile = 'crowSession'
-global.authFileJB  = 'crowJadiBot'
+global.authFile = 'kiritoSession'
+global.authFileJB  = 'kititoJadiBot'
 /*global.rutaBot = join(__dirname, authFile)
 global.rutaJadiBot = join(__dirname, authFileJB)
 
