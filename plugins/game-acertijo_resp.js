e0d87f9a07f34e71033522c61cbed89fd6fe87dd
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-03-23 14:21:13 -0400

    Actualizar game-acertijo_resp.js

--- plugins/game-acertijo_resp.js
+++ plugins/game-acertijo_resp.js
@@ -1,34 +1,22 @@
-
-import similarity from 'similarity'
-
+import similarity from 'similarity';
 const threshold = 0.72;
-let handler = m => m;
-
-handler.before = async function (m) {
-    let id = m.chat;
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0;
-
-    this.tekateki = this.tekateki ? this.tekateki : {};
-    if (!(id in this.tekateki)) return m.reply('⚠️ 𝗘𝗦𝗧𝗘 𝗔𝗖𝗘𝗥𝗧𝗜𝗝𝗢 𝗬𝗔 𝗛𝗔 𝗧𝗘𝗥𝗠𝗜𝗡𝗔𝗗𝗢.');
-
-    if (m.quoted.id == this.tekateki[id][0].id) {
-        let json = JSON.parse(JSON.stringify(this.tekateki[id][1]));
-        
-        // Verifica la respuesta del acertijo
-        if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
-            global.db.data.users[m.sender].exp += this.tekateki[id][2];
-            m.reply(`✅ 𝗖𝗢𝗥𝗥𝗘𝗖𝗧𝗢, 𝗘𝗥𝗘𝗦 𝗨𝗡 𝗚𝗘𝗡𝗜𝗢!\n+${this.tekateki[id][2]} Exp`);
-            clearTimeout(this.tekateki[id][3]);
-            delete this.tekateki[id];
-        } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) {
-            m.reply('Casi lo logras!');
-        } else {
-            m.reply('❌ 𝗜𝗡𝗖𝗢𝗥𝗥𝗘𝗖ＴO, 𝗦𝗜𝐆𝐔𝐄 𝑃𝑎𝑟𝑡𝑖𝑐𝑖𝑝𝑎𝑛𝑑𝑜.');
-        }
-    }
-    return !0;
-}
-
+const handler = (m) => m;
+handler.before = async function(m) {
+  const id = m.chat;
+  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0;
+  this.tekateki = this.tekateki ? this.tekateki : {};
+  if (!(id in this.tekateki)) return m.reply('✨️ Ese acertijo ya ha terminado!');
+  if (m.quoted.id == this.tekateki[id][0].id) {
+    const json = JSON.parse(JSON.stringify(this.tekateki[id][1]));
+    if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
+      global.db.data.users[m.sender].estrellas += this.tekateki[id][2];
+      m.reply(`[🌠] *Respuesta correcta!*\n+${this.tekateki[id][2]} Estrellas`);
+      clearTimeout(this.tekateki[id][3]);
+      delete this.tekateki[id];
+    } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`Casi lo logras!`);
+    else m.reply('Respuesta incorrecta!');
+  }
+  return !0;
+};
 handler.exp = 0;
-
 export default handler;
