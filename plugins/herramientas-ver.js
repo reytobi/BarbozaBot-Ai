c2eb99545314740f65ffc383097aa915379d5b4d
Wichi <sebastianbarbaro82@gmail.com>
2025-03-30 11:07:46 -0400

    Actualizar herramientas-ver.js

--- plugins/herramientas-ver.js
+++ plugins/herramientas-ver.js
@@ -1,21 +1,19 @@
-
 import uploadFile from '../lib/uploadFile.js';
 import uploadImage from '../lib/uploadImage.js';
+import fetch from 'node-fetch';
 
 const handler = async (m, { conn }) => {
-    const q = m.quoted ? m.quoted : m;
-    const mime = q.mimetype || ''; // Asegúrate de usar q.mimetype directamente
-    if (!mime) throw '❌ No se detectó un archivo válido';
-
-    const media = await q.download();
-    try {
-        const isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
-        const link = await (isTele ? uploadImage : uploadFile)(media);
-        await conn.sendFile(m.chat, link, '', '© Prohibido la copia, Código Oficial de MediaHub™', m);
-    } catch (e) {
-        console.log(e);
-        await conn.reply(m.chat, '❌ Ocurrió un error al procesar el archivo.', m);
-    }
+  const q = m.quoted ? m.quoted : m;
+  const mime = (q.msg || q).mimetype || '';
+  if (!mime) throw 'No se detectó un archivo válido';
+  const media = await q.download();
+  try {
+    const isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
+    const link = await (isTele ? uploadImage : uploadFile)(media);
+    await conn.sendFile(m.chat, link, '', '© Prohibido la copia, Código Oficial de MediaHub™', m);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 handler.help = ['ver'];
