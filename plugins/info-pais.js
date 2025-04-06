0f0e61999455203fe2022b77eb770d2379f9419b
Wichi <sebastianbarbaro82@gmail.com>
2025-04-06 15:47:41 -0400

    Actualizar info-pais.js

--- plugins/info-pais.js
+++ plugins/info-pais.js
@@ -5,7 +5,7 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
 if (!text) return m.reply('🍭 Ingresa Un Nombre De Un Pais');
 
 try {
-let api = `https://delirius-apiofc.vercel.app/tools/flaginfo?query=${text}`;
+let api = `https://api.siputzx.my.id/api/tools/countryInfo?name=${text}`;
 
 let response = await fetch(api);
 let json = await response.json();
