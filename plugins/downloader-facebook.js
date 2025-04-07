d41a7584b8e423938b3c99dc482a9b0734783234
Wichi <sebastianbarbaro82@gmail.com>
2025-04-06 22:12:27 -0400

    Actualizar downloader-facebook.js

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -1,17 +1,17 @@
+//Barboza 
 
 import fetch from 'node-fetch';
 import axios from 'axios';
 
 let HS = async (m, { conn, text }) => {
-    if (!text) return conn.reply(m.chat, '❀ Ingresa un link de facebook', m);
+    if (!text) return conn.reply(m.chat, '📌Atención futuro cliente proporcione un link de facebook para descargar su video', m);
 
     try {
         let api = await fetch(`https://vapis.my.id/api/fbdl?url=https://www.facebook.com/share/r/12BFZAtjpS8/?mibextid=qDwCgo{text}`);
         let json = await api.json();
-        
-        // Asegúrate de que la respuesta tenga los datos esperados
+    
         if (!json.data) {
-            return conn.reply(m.chat, '❀ No se pudo obtener la información del video. Verifica el enlace.', m);
+            return conn.reply(m.chat, '📌 No se descargo el vídeo . Verifica el enlace.', m);
         }
 
         let { title, durasi, hd_url } = json.data;
