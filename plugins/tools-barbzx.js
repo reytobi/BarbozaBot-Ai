ebd0042b8c6cdaca8f0ef0f70313b96610ee2974
Wichi <sebastianbarbaro82@gmail.com>
2025-04-07 13:54:54 -0400

    Actualizar tools-barbzx.js

--- plugins/tools-barbzx.js
+++ plugins/tools-barbzx.js
@@ -4,7 +4,7 @@ if (!text) throw `*👻 ingrese un texto para generar poder generar su imagen*`;
 m.react('🕒');
 await conn.sendMessage(m.chat, {text: '*👻 Generando Imagen*'}, {quoted: m});
 try {
-const response = await fetch(`https://archive-ui.tanakadomp.biz.id/maker/text2img?text=
+const response = await fetch(`https://archive-ui.tanakadomp.biz.id/maker/carbon?text=
 =${encodeURIComponent(text)}`);
 if (!response.ok) throw new Error('Network response was not ok');
 const buffer = await response.buffer();
