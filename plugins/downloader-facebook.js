9d607d6d3493289037af03a510eb0d965ca2df0d
Wichi <sebastianbarbaro82@gmail.com>
2025-04-07 06:10:19 -0400

    Actualizar downloader-facebook.js

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -16,6 +16,6 @@ MF.command = ['fb', 'facebook'];
 export default MF;
 
 async function facebookdl(url) {
-let moon = await(await fetch(`https://vapis.my.id/api/fbdl?url=${url}`)).json();
+let moon = await(await fetch(`https://vapis.my.id/api/fbdl?url=https://www.facebook.com/share/r/12BFZAtjpS8/?mibextid=qDwCgo{url}`)).json();
 return moon;
 }
