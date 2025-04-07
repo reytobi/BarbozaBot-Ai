8f08c4a6cabd6a6a73b192585f87185913c0e57d
Wichi <sebastianbarbaro82@gmail.com>
2025-04-07 13:38:46 -0400

    Actualizar downloader-facebook.js

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -10,7 +10,7 @@ let HS = async (m, { conn, text }) => {
     if (!text) return conn.reply(m.chat, '📌Atención futuro cliente proporcione un link de facebook para descargar su video', m);
 
     try {
-        let api = await fetch(`https://vapis.my.id/api/fbdl?url=https://www.facebook.com/share/r/12BFZAtjpS8/?mibextid=qDwCgo{text}`);
+        let api = await fetch(`https://api.dorratz.com/fbvideo?url=https://www.facebook.com/share/v/12DoEUCoFji/?mibextid=rS40aB7S9Ucbxw6v{text}`);
         let json = await api.json();
 
         if (!json.data) {
