5714693c8254eaf216b4070a5e7e90943cec9d1c
Wichi <sebastianbarbaro82@gmail.com>
2025-04-07 20:23:44 -0400

    Actualizar barbozx-ytmp4.js

--- plugins/barbozx-ytmp4.js
+++ plugins/barbozx-ytmp4.js
@@ -1,42 +1,16 @@
-import fetch from 'node-fetch';
-import fg from 'senna-fg';
+import fetch from 'node-fetch'
 
-let handler = async(m, { conn, args, text }) => {
+let HS = async (m, { conn, command, text, usedPrefix }) => {
+if (!text) return conn.reply(m.chat, '🪐 ingresa un link de youtube', m, null, rcanal)
+try {
+let api = await fetch(`https://api.davidcyriltech.my.id/download/ytmp4?url=${text}`)
+let json = await api.json()
+let { title, quality, thumbnail, download_url } = json.result
+await conn.sendMessage(m.chat, { video: { url: download_url }, caption: title }, { quoted: m })
+} catch (error) {
+console.error(error)
+}}
 
-if (!text) return m.reply(`🍭 Ingresa Un Link De YouTube\n> *Ejemplo:* https://youtube.com/shorts/ZisXJqH1jtw?si=0RZacIJU5zhoCmWh`);
+HS.command = ['ytmp4']
 
-m.react(rwait);
-
-
-// let video = await (await fetch(`https://api.agungny.my.id/api/youtube-video?url=${text}`)).json();
-
-let data = await fg.ytmp4(text);
-let url = data.dl_url;
-// let link = video.result.result.download;
-
-if (!url) return m.reply('《✧》Hubo un error al intentar acceder al link.\n> Si el problema persiste, reportalo en el grupo de soporte.');
-
-/* let limit = 5 * 1024 * 1024; // 5MB porque si
-
-if (video?.data?.size > limit) {
-await conn.sendMessage(m.chat, {
-      document: { url: url },
-      fileName: `${data.title}.mp4`,
-      mimetype: 'video/mp4', caption: '✅ Descargado Con Exito.',
-      thumbnail: video.thumbnail },          
-      { quoted: m })
-
-} else { 
-*/
-
-await conn.sendMessage(m.chat, {
-      video: { url: url },
-      mimetype: "video/mp4",
-      caption: `${dev}`,
-    }, { quoted: m });
-    m.react(done);
- }
-
-handler.command = ['ytv', 'ytmp4', 'ymp4']
-
-export default handler;
+export default HS
