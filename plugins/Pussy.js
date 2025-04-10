2faf3d944c95ad017df3de5775d0db8fb2f67380
Wichi <sebastianbarbaro82@gmail.com>
2025-04-10 13:59:05 -0400

    Actualizar Pussy.js

--- plugins/Pussy.js
+++ plugins/Pussy.js
@@ -2,12 +2,12 @@ import fetch from "node-fetch";
 
 let handler = async (m, { conn, usedPrefix, command }) => {
   try {
-    // Verificar que el comando sea válido
+   
     if (command !== Buffer.from("cHVzc3k=", "base64").toString("utf-8")) {
       throw new Error("Comando no reconocido.");
     }
 
-    // URL de la API (protegida en Base64)
+   
     const apiUrl = Buffer.from("aHR0cHM6Ly9kZWxpcml1cy1hcGlvZmMudmVyY2VsLmFwcC9uc2Z3L2dpcmxz", "base64").toString("utf-8");
 
     // Realizar la solicitud a la API
@@ -17,10 +17,8 @@ let handler = async (m, { conn, usedPrefix, command }) => {
       throw new Error(`La API devolvió un estado HTTP ${response.status}.`);
     }
 
-    // Extraer el contenido (la imagen en este caso)
     const imageBuffer = await response.buffer();
 
-    // Enviar la imagen al usuario
     await conn.sendMessage(m.chat, {
       image: imageBuffer,
       caption: Buffer.from("8J+PjSBBCcOkcO8YSB0aWVuZXMgdW5hIGltYWdlbiBhbGVhdG9yaWEgc29saWNpdGFkYSBjb24gZWwgY29tYW5kbyAq", "base64").toString("utf-8") + `${usedPrefix}${command}*.`,
@@ -34,7 +32,6 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   }
 };
 
-// Definir el comando (protegido)
 handler.command = [Buffer.from("cHVzc3k=", "base64").toString("utf-8")];
 
 export default handler;
