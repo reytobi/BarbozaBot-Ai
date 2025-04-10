344a49a6f627f1ae931c3445a6bb41e116b079b9
Wichi <sebastianbarbaro82@gmail.com>
2025-04-09 20:55:09 -0400

    Actualizar Barboza-playdoc.js

--- plugins/Barboza-playdoc.js
+++ plugins/Barboza-playdoc.js
@@ -1,32 +1,35 @@
-// By WillZek >> https://github.com/WillZek
 
 import fetch from 'node-fetch';
 
 let handler = async(m, { conn, usedPrefix, command, text }) => {
+    if (!text) return m.reply('🍭 Ingresa Un Texto Para Buscar En Youtube\n> *Ejemplo:* ${usedPrefix + command}crow edits');
 
-if (!text) return m.reply(`🍭 Ingresa Un Texto Para Buscar En Youtube\n> *Ejemplo:* ${usedPrefix + command}crow edits`);
+    try {
+        let api = await (await fetch(`https://delirius-apiofc.vercel.app/search/ytsearch?q=${text}`)).json();
 
-try {
-let api = await (await fetch(`https://delirius-apiofc.vercel.app/search/ytsearch?q=${text}`)).json();
+        if (!api.data || api.data.length === 0) {
+            return m.reply('No se encontraron resultados para tu búsqueda.');
+        }
 
-let results = api.data[0];
+        let results = api.data[0];
 
-let txt = `✨ *Título:* ${results.title}\n⌛ *Duración:* ${results.duration}\n📎 *Link:* ${results.url}\n📆 *Publicado:* ${results.publishedAt}`;
+        let txt = `✨ *Título:* ${results.title}\n⌛ *Duración:* ${results.duration}\n📎 *Link:* ${results.url}\n📆 *Publicado:* ${results.publishedAt}`;
+        let img = results.image;
 
-let img = results.image;
+        conn.sendMessage(m.chat, { image: { url: img }, caption: txt }, { quoted: m });
 
-conn.sendMessage(m.chat, { image: { url: img }, caption: txt }, { quoted: m });
+        let api2 = await (await fetch(`https://api.vreden.my.id/api/ytmp3?url=${results.url}`)).json();
 
-let api2 = await(await fetch(`https://api.vreden.my.id/api/ytmp3?url=${results.url}`)).json();
+        if (!api2?.result?.download?.url) {
+            return m.reply('No se encontraron resultados válidos para la descarga.');
+        }
 
-// if (!api2?.result?.download.url) return m.reply('No Se  Encontraron Resultados');
+        await conn.sendMessage(m.chat, { document: { url: api2.result.download.url }, mimetype: 'audio/mpeg', fileName: `${results.title}.mp3` }, { quoted: m });
 
-await conn.sendMessage(m.chat, { document: { url: api2.result.download.url }, mimetype: 'audio/mpeg', fileName: `${results.title}.mp3` }, { quoted: m });
-
-} catch (e) {
-m.reply(`Error: ${e.message}`);
-m.react('✖️');
-  }
+    } catch (e) {
+        m.reply(`Error: ${e.message}`);
+        m.react('✖️');
+    }
 }
 
 handler.command = ['playdoc', 'pdoc'];
