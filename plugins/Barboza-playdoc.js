a3777c57cb5f63c6daccec5671799ed8aef2f360
Wichi <sebastianbarbaro82@gmail.com>
2025-04-09 20:52:13 -0400

    Actualizar Barboza-playdoc.js

--- plugins/Barboza-playdoc.js
+++ plugins/Barboza-playdoc.js
@@ -1,8 +1,10 @@
+// By WillZek >> https://github.com/WillZek
+
 import fetch from 'node-fetch';
 
 let handler = async(m, { conn, usedPrefix, command, text }) => {
 
-if (!text) return m.reply(`🍭 Ingresa Un Texto Para Buscar En Youtube\n> *Ejemplo:* ${usedPrefix + command}Edist sumi`);
+if (!text) return m.reply(`🍭 Ingresa Un Texto Para Buscar En Youtube\n> *Ejemplo:* ${usedPrefix + command}crow edits`);
 
 try {
 let api = await (await fetch(`https://delirius-apiofc.vercel.app/search/ytsearch?q=${text}`)).json();
@@ -15,11 +17,11 @@ let img = results.image;
 
 conn.sendMessage(m.chat, { image: { url: img }, caption: txt }, { quoted: m });
 
-let api2 = await(await fetch(`https://api.fgmods.xyz/api/downloader/ytmp3?url=${results.url}&quality=480p&apikey=elrebelde21`)).json();
+let api2 = await(await fetch(`https://api.vreden.my.id/api/ytmp3?url=${results.url}`)).json();
 
-if (!api2?.result.dl_url) return m.reply('No Se  Encontraron Resultados');
+// if (!api2?.result?.download.url) return m.reply('No Se  Encontraron Resultados');
 
-await conn.sendMessage(m.chat, { document: { url: api2.result.dl_url }, mimetype: 'audio/mpeg', fileName: `${api2.result.title}.mp3` }, { quoted: m });
+await conn.sendMessage(m.chat, { document: { url: api2.result.download.url }, mimetype: 'audio/mpeg', fileName: `${results.title}.mp3` }, { quoted: m });
 
 } catch (e) {
 m.reply(`Error: ${e.message}`);
@@ -28,5 +30,3 @@ m.react('✖️');
 }
 
 handler.command = ['playdoc', 'pdoc'];
-
-export default handler
