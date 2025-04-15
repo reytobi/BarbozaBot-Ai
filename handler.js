45e36e5ceba2b2be1b5c4e618dcba2048f00892a
Wichi <sebastianbarbaro82@gmail.com>
2025-04-14 22:29:15 -0400

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -238,7 +238,7 @@ conn: this,
                 let text = _args.join` `
                 command = (command || '').toLowerCase()
                 const groupLimitado = '120363418071387498@g.us'
-                const comandosPermitidos = ['serbot', 'jadibot', 'qr', 'code', 'grupo cerrar' ,'grupo abrir', 'n'' 'update']
+                const comandosPermitidos = ['serbot', 'jadibot', 'qr', 'code']
 
                 if (m.chat === groupLimitado && !comandosPermitidos.includes(command)) {
                 return 
