4a6941ca4760d2093d1f0c1f6c7fda28f4a48bf7
Adrianmaze <anuardavid2728@gmail.com>
2025-04-14 23:04:47 -0400

    Update handler.js

--- handler.js
+++ handler.js
@@ -238,7 +238,7 @@ conn: this,
                 let text = _args.join` `
                 command = (command || '').toLowerCase()
                 const groupLimitado = '120363418071387498@g.us'
-                const comandosPermitidos = ['serbot', 'bots', 'kick', 'code']
+                const comandosPermitidos = ['serbot', 'bots', 'kick', 'code', 's', 'n', 'grupo abrir', 'grupo cerrar', 'update', 'owner', 'staff']
 
                 if (m.chat === groupLimitado && !comandosPermitidos.includes(command)) {
                 return 
