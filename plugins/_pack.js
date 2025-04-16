faddbc8ed51156da2baf98b6dbe5cb53e4028799
Wichi <sebastianbarbaro82@gmail.com>
2025-04-15 20:23:10 -0400

    Actualizar _pack.js

--- plugins/_pack.js
+++ plugins/_pack.js
@@ -12,7 +12,7 @@ m.react('🕑');
 
 let txt = 'Pack🔥🔥🔥\n> Pon De Nuevo .pack para mirar el siguiente ✨';
 
-let img = 'https://delirius-apiofc.vercel.app/nsfw/corean';
+let img = 'https://delirius-apiofc.vercel.app/nsfw/girls';
 
 m.react('✅');
 conn.sendMessage(m.chat, { image: { url: img }, caption: txt }, { quoted: fkontak });
