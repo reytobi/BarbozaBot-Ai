b780e512f491039b77dcccd7925894d7df662809
Wichi <sebastianbarbaro82@gmail.com>
2025-04-17 22:33:10 -0400

    Actualizar main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,43 +1,12 @@
 
-//código creado por Barbosa
-import PhoneNumber from 'awesome-phonenumber';
-
 async function handler(m, { conn }) { 
     let numcreador = '584146277368';
-    let ownerJid = numcreador + '@s.whatsapp.net';
+    let imagen = 'https://qu.ax/Mvhfa.jpg'; // URL de la imagen que deseas enviar
 
-    let name = await conn.getName(ownerJid) || 'owner'; 
-    let about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || 'Creador de bots de WhatsApp y Creador del Bot Barboza Ai';
-
-    let empresa = 'Barboza- Servicios Tecnológicos';
-  
-   let imagen = 'https://qu.ax/Mvhfa.jpg';
-
-    let vcard = `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name};;;
-FN:${name}
-ORG:${empresa};
-TITLE:CEO & Fundador
-TEL;waid=${numcreador}:${new PhoneNumber('+' + numcreador).getNumber('international')}
-EMAIL:sebastianbarbaro82@gmail.com
-URL:https://www.instagram.com/sebastian_barboza13
-NOTE:${about}
-ADR:;;Dirección de tu empresa;;;;
-X-ABADR:ES
-X-ABLabel:Dirección Web
-X-ABLabel:Correo Electrónico
-X-ABLabel:Teléfono de contacto
-X-WA-BIZ-NAME:${name}
-X-WA-BIZ-DESCRIPTION:${about}
-END:VCARD`.trim();
-
+    // Enviar solo la imagen y el número debajo
     await conn.sendMessage(m.chat, { 
-        contacts: { 
-            displayName: name, 
-            contacts: [{ vcard }]
-        } 
+        image: { url: imagen },
+        caption: `📞 ${numcreador}`, // Número mostrado debajo de la imagen
     }, { quoted: m });
 }
 
