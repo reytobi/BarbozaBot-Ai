7bcf032c417552a46d2d9dd663a2e8575483a89a
Wichi <sebastianbarbaro82@gmail.com>
2025-04-17 22:47:56 -0400

    Actualizar main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -8,37 +8,9 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
    let ownerJid = numcreador + '@s.whatsapp.net';
    let name = await conn.getName(ownerJid) || 'Owner';
    let empresa = 'Barboza - Servicios Tecnológicos';
-   let about = 'Creador de bots de WhatsApp y del Bot Barboza Ai';
    let imagen = 'https://qu.ax/Mvhfa.jpg'; // URL de la imagen del creador
 
-   // VCARD con información de contacto
-   let list = [{
-       displayName: name,
-       vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:${name}\nORG:${empresa}\nTITLE:CEO & Fundador\nTEL;waid=${numcreador}:${numcreador}\nEMAIL:sebastianbarbaro82@gmail.com\nURL:https://www.instagram.com/sebastian_barboza13\nNOTE:${about}\nEND:VCARD`,
-   }];
-
-   // Enviar el contacto con los detalles
-   await conn.sendMessage(m.chat, {
-       contacts: {
-           displayName: `${list.length} Contacto`,
-           contacts: list
-       },
-       contextInfo: {
-           externalAdReply: {
-               showAdAttribution: true,
-               title: 'Dueño del Bot - Barboza AI',
-               body: empresa,
-               thumbnailUrl: imagen,
-               sourceUrl: 'https://www.instagram.com/sebastian_barboza13',
-               mediaType: 1,
-               renderLargerThumbnail: true
-           }
-       }
-   }, {
-       quoted: m
-   });
-
-   // Mensaje de texto con la imagen y el número del dueño
+   // Enviar solo la imagen con el número debajo
    await conn.sendMessage(m.chat, { 
        image: { url: imagen },
        caption: `📞 ${numcreador}`,
