3e19a8270a58c944b676d510b7478dacc9d9a5ed
Wichi <sebastianbarbaro82@gmail.com>
2025-04-17 22:45:46 -0400

    Actualizar main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,24 +1,52 @@
 
-import PhoneNumber from 'awesome-phonenumber';
+import fetch from 'node-fetch';
 
-async function handler(m, { conn }) { 
-    let numcreador = '584146277368';
-    let ownerJid = numcreador + '@s.whatsapp.net';
+let handler = async (m, { conn, usedPrefix, text, args, command }) => {
+   await m.react('🎩');
 
-    let name = await conn.getName(ownerJid) || 'Owner'; 
-    let about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || 'Creador de bots de WhatsApp y del Bot Barboza Ai';
-    let empresa = 'Barboza- Servicios Tecnológicos';
-    let imagen = 'https://qu.ax/Mvhfa.jpg'; // Reemplaza con la URL de la imagen que deseas mostrar
+   let numcreador = '584146277368';
+   let ownerJid = numcreador + '@s.whatsapp.net';
+   let name = await conn.getName(ownerJid) || 'Owner';
+   let empresa = 'Barboza - Servicios Tecnológicos';
+   let about = 'Creador de bots de WhatsApp y del Bot Barboza Ai';
+   let imagen = 'https://qu.ax/Mvhfa.jpg'; // URL de la imagen del creador
 
-    // Enviar imagen junto con el número del dueño y sus detalles
-    await conn.sendMessage(m.chat, { 
-        image: { url: imagen },
-        caption: `👤 *Dueño del bot*\n📌 *Nombre:* ${name}\n📞 *Número:* wa.me/${numcreador}\n📝 *Descripción:* ${about}\n🏢 *Empresa:* ${empresa}\n📧 *Email:* sebastianbarbaro82@gmail.com\n🌐 *Instagram:* https://www.instagram.com/sebastian_barboza13`,
-    }, { quoted: m });
-}
+   // VCARD con información de contacto
+   let list = [{
+       displayName: name,
+       vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:${name}\nORG:${empresa}\nTITLE:CEO & Fundador\nTEL;waid=${numcreador}:${numcreador}\nEMAIL:sebastianbarbaro82@gmail.com\nURL:https://www.instagram.com/sebastian_barboza13\nNOTE:${about}\nEND:VCARD`,
+   }];
 
-handler.help = ['owner']; 
-handler.tags = ['main']; 
-handler.command = ['owner', 'creator', 'creador', 'dueño'];
+   // Enviar el contacto con los detalles
+   await conn.sendMessage(m.chat, {
+       contacts: {
+           displayName: `${list.length} Contacto`,
+           contacts: list
+       },
+       contextInfo: {
+           externalAdReply: {
+               showAdAttribution: true,
+               title: 'Dueño del Bot - Barboza AI',
+               body: empresa,
+               thumbnailUrl: imagen,
+               sourceUrl: 'https://www.instagram.com/sebastian_barboza13',
+               mediaType: 1,
+               renderLargerThumbnail: true
+           }
+       }
+   }, {
+       quoted: m
+   });
+
+   // Mensaje de texto con la imagen y el número del dueño
+   await conn.sendMessage(m.chat, { 
+       image: { url: imagen },
+       caption: `📞 ${numcreador}`,
+   }, { quoted: m });
+};
+
+handler.help = ['owner', 'creator'];
+handler.tags = ['main'];
+handler.command = /^(owner|creator|creador|dueño)$/i;
 
 export default handler;
