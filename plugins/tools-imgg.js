db496027190a4d90b460e6038dd5c40408feacf8
Wichi <sebastianbarbaro82@gmail.com>
2025-04-30 22:16:05 -0400

    Actualizar tools-imgg.js

--- plugins/tools-imgg.js
+++ plugins/tools-imgg.js
@@ -7,7 +7,7 @@ const handler = async (m, { conn, args }) => {
   }
 
   const text = args.join(' ');
-  const apiUrl = `https://api.nekorinn.my.id/ai-img/text2img?text=${encodeURIComponent(text)}`;
+  const apiUrl = `https://api.nekorinn.my.id/ai-img/imagen?text=${encodeURIComponent(text)}`;
 
   try {
     m.reply('⏳ Generando tu imagen, espera un momento...');
