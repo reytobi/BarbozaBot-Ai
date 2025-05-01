b7964c9d716a278289068e868494f64ebe3da6e9
Wichi <sebastianbarbaro82@gmail.com>
2025-05-01 17:28:24 -0400

    Actualizar reg-tuto.js

--- plugins/reg-tuto.js
+++ plugins/reg-tuto.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn }) => {
   const usuario = m.pushName || 'Usuario';
-  const videoUrl = 'https://qu.ax/nMLul.mp4';
+  const videoUrl = 'https://qu.ax/yTjnH.mp4';
 
-  const texto = `Hola @${m.sender.split('@')[0]} aquí está el tutorial para registrarte en Barboza-Bot.`;
+  const texto = `Hola @${m.sender.split('@')[0]} aquí está el tutorial para hacerte subbot en Barboza-Bot.`;
 
   const options = {
     quoted: m,
@@ -13,6 +13,6 @@ let handler = async (m, { conn }) => {
   await conn.sendMessage(m.chat, { video: { url: videoUrl }, ...options });
 };
 
-handler.command = ['tutorialreg']
+handler.command = ['tutosub']
 
 export default handler;
