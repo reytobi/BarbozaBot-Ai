6144e5592b1f50182e76bc4a351181e66e2ccfd8
Adrianmaze <anuardavid2728@gmail.com>
2025-05-05 13:15:32 -0400

    Update handler.js

--- handler.js
+++ handler.js
@@ -105,6 +105,13 @@ export async function handler(chatUpdate) {
             // Inicializar configuración del bot
             const botJid = this.user.jid
             let settings = global.db.data.settings[botJid]
+
+            const gruposLimitados = ['120363418071387498@g.us', '120363400282268465@g.us']
+const settings = global.db.data.settings[this.user.jid] || {}
+const esSubBot = settings?.modoSubbot === true
+const esBotPrincipal = !global.conns.find(conn => conn.user?.jid === this.user.jid)
+
+if (!esBotPrincipal && esSubBot && gruposLimitados.includes(m.chat)) return
             
             if (typeof settings !== 'object') global.db.data.settings[botJid] = {}
             if (settings) {
