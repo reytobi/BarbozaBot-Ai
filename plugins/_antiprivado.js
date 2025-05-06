db243f71d2133e2c9607dadeb2b7eb638820168f
Wichi <sebastianbarbaro82@gmail.com>
2025-05-06 13:22:42 -0400

    Actualizar _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,18 +1,21 @@
 
 export async function before(m, { conn, isOwner, isROwner }) {
-  if (m.isBaileys && m.fromMe) return true;  
-  if (m.isGroup) return false;  
-  if (!m.message) return true;  
+    if (m.isBaileys && m.fromMe) return true;
+    if (m.isGroup) return false;
+    if (!m.message) return true;
 
-  const botSettings = global.db.data.settings[this.user.jid] || {};
+    const botSettings = global.db.data.settings[this.user.jid] || {};
   
-  if (botSettings.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`🚫 *Hola @${m.sender.split`@`[0]}, el uso de mensajes privados no está permitido.*  
-❌ *Serás bloqueado automáticamente.*  
-✅ *Si necesitas soporte, únete a nuestro grupo oficial!*`, false, { mentions: [m.sender] });
+    if (botSettings.antiPrivate && !isOwner && !isROwner) {
+        await m.reply(`🚨 *¡Atención @${m.sender.split`@`[0]}!*  
+❌ *No está permitido enviar mensajes privados.*  
+🚫 *Serás bloqueado automáticamente.*  
+🔗 *Si necesitas ayuda, únete a nuestro grupo oficial:*  
+👉 [Grupo Oficial](https://chat.whatsapp.com/G1XFpYDLN8aF3fpVUSD2uF)`, false, { mentions: [m.sender] });
 
-    await conn.updateBlockStatus(m.chat, 'block');
-  }
+        await conn.updateBlockStatus(m.chat, 'block');
+        console.log(`Usuario ${m.sender} bloqueado por contacto privado.`);
+    }
 
-  return false;
+    return false;
 }
