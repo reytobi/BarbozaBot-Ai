a49548d86982ea58853d8e74d909ec2000b0e2c3
Wichi <sebastianbarbaro82@gmail.com>
2025-05-09 20:56:40 -0400

    Actualizar main-menu4.js

--- plugins/main-menu4.js
+++ plugins/main-menu4.js
@@ -1,30 +1,40 @@
 
 const handler = async (m, { conn}) => {
     let mensaje = `
-🎮 *Menú de Juegos* 🎮
+🎮 *Menú de Juegos - Diviértete con tu bot!* 🎮
 
- .trivia
- .ahorcado
- .game
- .ruleta
- .pelear
- .sopa
- .cazar
- .acertijo
- .dado
- .mate
- .emoji
- .supervivencia 
- .carrera
- .animal
- .detective 
- .zombie
- .gladiador 
- .asalto
- .robot
+✨ *Juegos de habilidad:*
+🧠.trivia - Prueba tu conocimiento
+🔤.ahorcado - Adivina la palabra antes de perder
+🎭.acertijo - Desafíos mentales para los más astutos
+🎯.mate - Operaciones matemáticas rápidas
 
-📌 *Usa el comando de cada juego para jugar!*
+🔥 *Juegos de azar:*
+🎰.ruleta - Gira y prueba tu suerte
+🎲.dado - Lanza el dado y gana puntos
+🥊.pelear - Un duelo entre jugadores
+
+🌿 *Juegos de aventura:*
+🏹.cazar - Busca presas y consigue recompensas
+🌍.supervivencia - Toma decisiones para sobrevivir
+🔍.detective - Investiga casos misteriosos
+
+🚀 *Juegos de velocidad:*
+🏎️.carrera - Corre con tu vehículo favorito
+🐉.animal - Apuesta por un animal en la carrera
+
+🧟 *Juegos temáticos:*
+🧟‍♂️.zombie - Sobrevive al apocalipsis zombie
+⚔️.gladiador - Lucha en la arena y conviértete en campeón
+💰.asalto - Planea un robo sin ser atrapado
+🤖.robot - Pelea con tu robot y vence en combate
+🔮.magia - Usa hechizos y gana el duelo mágico
+
+📌 *Usa el comando de cada juego para jugar y ganar puntos!*
+🔹 **Compite con amigos y escala en el ranking de los mejores jugadores!** 🏆🔥
+
 `;
+
     const imageUrl = "https://qu.ax/Mvhfa.jpg";
 
     await conn.sendMessage(m.chat, {
