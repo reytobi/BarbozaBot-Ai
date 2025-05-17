a3a5669eb5131fa9129d371039bd71a7083650fe
Wichi <sebastianbarbaro82@gmail.com>
2025-05-17 09:23:23 -0400

    Actualizar tools-newname.js

--- plugins/tools-newname.js
+++ plugins/tools-newname.js
@@ -1,18 +1,14 @@
 
 const handler = async (m, { conn, text}) => {
     try {
-        // Verificar que el usuario haya ingresado un nuevo nombre
         if (!text) return m.reply('❌ *Error:* Debes escribir el nuevo nombre después de `.newname`.');
 
-        // Cambiar el nombre del bot principal
         await conn.updateProfileName(text);
 
-        // Cambiar el nombre de los subbots si existen
         if (conn.authState.creds.me.id) {
             await conn.updateProfileName(text);
 }
 
-        // Confirmar el cambio con emojis
         m.reply(`✅ *¡Nombre cambiado exitosamente!* 😃✨\n📌 *Nuevo nombre:* ${text}`);
 
 } catch (error) {
