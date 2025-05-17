36258ab76915557be679cdc5c0214a59e8ad68cc
Wichi <sebastianbarbaro82@gmail.com>
2025-05-17 13:24:41 -0400

    Actualizar tools-setprofile.js

--- plugins/tools-setprofile.js
+++ plugins/tools-setprofile.js
@@ -4,31 +4,21 @@ import fs from 'fs';
 
 const handler = async (m, { conn}) => {
     try {
-        // Verificar si el mensaje citado es una imagen
         if (!m.quoted ||!m.quoted.mimetype ||!m.quoted.mimetype.startsWith('image/')) {
             return m.reply('❌ *Error:* Responde a una imagen con el comando `.setprofile` para cambiar la foto de perfil.');
 }
-
-        // Descargar la imagen adjunta y almacenar el resultado en una variable
         const mediaStream = await downloadContentFromMessage(m.quoted, 'image');
         let buffer = Buffer.from([]);
 
-        // Leer los datos de la imagen
         for await (const chunk of mediaStream) {
-            buffer = Buffer.concat([buffer, chunk]);
-}
-
-        // Guardar la imagen temporalmente
+            buffer = Buffer.concat([buffer, chunk])
+{
         const path = './profile.jpg';
         fs.writeFileSync(path, buffer);
-
-        // Cambiar la foto de perfil del bot y los subbots si existen
         await conn.updateProfilePicture(conn.user.id, { url: path});
         if (conn.authState.creds.me?.id) {
             await conn.updateProfilePicture(conn.authState.creds.me.id, { url: path});
 }
-
-        // Confirmación con emojis
         m.reply('✅ *¡Foto de perfil actualizada con éxito!* 😃📸');
 
 } catch (error) {
