7649a2b65a5f788102982bafcf6a493c9c93d500
Wichi <sebastianbarbaro82@gmail.com>
2025-05-18 22:13:52 -0400

    Actualizar main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -8,7 +8,7 @@ async function handler(m, { conn }) {
     let name = await conn.getName(ownerJid) || 'Owner'; 
     let about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || 'Creador de bots de WhatsApp y del Bot Barboza Ai';
     let empresa = 'Barboza- Servicios Tecnológicos';
-    let imagen = 'https://qu.ax/Mvhfa.jpg'; // Reemplaza con la URL de la imagen que deseas mostrar
+    let imagen = 'https://files.catbox.moe/l81ahk.jpg'; // Reemplaza con la URL de la imagen que deseas mostrar
 
     // Enviar imagen junto con el número del dueño y sus detalles
     await conn.sendMessage(m.chat, { 
