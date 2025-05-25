8a8d5af2a3a2499f782ffaa2f6a74a8df59db67e
FzTeis <146758681+FzTeis@users.noreply.github.com>
2025-05-24 19:18:07 -0600

    Actualización automática

---
+++ plugins/group-hidetag.js
@@ -0,0 +1,74 @@
+import { generateWAMessageFromContent } from '@whiskeysockets/baileys';
+import * as fs from 'fs';
+
+const handler = async (m, { conn, text, participants, isOwner, isAdmin }) => {
+  try {
+    const users = participants.map((u) => conn.decodeJid(u.id));
+    const watermark = '\n\n> _BOT- BARBOZA 🌪️_';
+
+    const q = m.quoted ? m.quoted : m || m.text || m.sender;
+    const c = m.quoted ? await m.getQuotedObj() : m.msg || m.text || m.sender;
+    const msg = conn.cMod(
+      m.chat,
+      generateWAMessageFromContent(
+        m.chat,
+        { [m.quoted ? q.mtype : 'extendedTextMessage']: m.quoted ? c.message[q.mtype] : { text: '' || c } },
+        { quoted: m, userJid: conn.user.id }
+      ),
+      (text || q.text) + watermark,
+      conn.user.jid,
+      { mentions: users }
+    );
+
+    await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id });
+  } catch {
+    const users = participants.map((u) => conn.decodeJid(u.id));
+    const quoted = m.quoted ? m.quoted : m;
+    const mime = (quoted.msg || quoted).mimetype || '';
+    const isMedia = /image|video|sticker|audio/.test(mime);
+    const watermark = '\n\n> BOT - BARBOZA';
+
+    if (isMedia) {
+      const mediax = await quoted.download?.();
+      const options = { mentions: users, quoted: m };
+
+      if (quoted.mtype === 'imageMessage') {
+        conn.sendMessage(m.chat, { image: mediax, caption: (text || '') + watermark, ...options });
+      } else if (quoted.mtype === 'videoMessage') {
+        conn.sendMessage(m.chat, { video: mediax, caption: (text || '') + watermark, mimetype: 'video/mp4', ...options });
+      } else if (quoted.mtype === 'audioMessage') {
+        conn.sendMessage(m.chat, { audio: mediax, caption: watermark, mimetype: 'audio/mpeg', fileName: `Hidetag.mp3`, ...options });
+      } else if (quoted.mtype === 'stickerMessage') {
+        conn.sendMessage(m.chat, { sticker: mediax, ...options });
+      }
+    } else {
+      const more = String.fromCharCode(8206);
+      const masss = more.repeat(850) + watermark;
+
+      await conn.relayMessage(
+        m.chat,
+        {
+          extendedTextMessage: {
+            text: `${masss}`,
+            contextInfo: {
+              mentionedJid: users,
+              externalAdReply: {
+                thumbnail: 'https://telegra.ph/file/03d1e7fc24e1a72c60714.jpg',
+                sourceUrl: global.canal
+              }
+            }
+          }
+        },
+        {}
+      );
+    }
+  }
+};
+
+handler.help = ['hidetag'];
+handler.tags = ['group'];
+handler.command = /^(hidetag|notify|notificar|noti|n|hidetah|hidet)$/i;
+handler.group = true;
+handler.Botadmin = true;
+
+export default handler;
