32354f23465fd614a046a381117c1e296e2e48d6
FzTeis <146758681+FzTeis@users.noreply.github.com>
2025-05-24 19:19:01 -0600

    Actualización automática

---
+++ plugins/owner-autoadmin.js
@@ -0,0 +1,20 @@
+
+const handler = async (m, {conn, isAdmin, groupMetadata }) => {
+  if (isAdmin) return m.reply('🚩 *¡YA ERES ADM JEFE!*');
+  try {
+    await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote');
+  await m.react(done)
+   m.reply('🚩 *¡YA TE DI ADM MI JEFE!*');
+    let nn = conn.getName(m.sender);
+     conn.reply('544123989549@s.whatsapp.net', `🚩 *${nn}* se dio Auto Admin en:\n> ${groupMetadata.subject}.`, m, rcanal, );
+  } catch {
+    m.reply('Demasiado Bueno 👻');
+  }
+};
+handler.tags = ['owner'];
+handler.help = ['autoadmin'];
+handler.command = ['autoadmin' ,'tenerpoder'];
+handler.mods = true;
+handler.group = true;
+handler.botAdmin = true;
+export default handler;
