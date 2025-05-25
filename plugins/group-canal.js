c1cd206293606934b98aba84bfe090b1431fed10
FzTeis <146758681+FzTeis@users.noreply.github.com>
2025-05-24 19:04:24 -0600

    Eliminar archivo plugins/group-canal.js

--- plugins/group-canal.js
+++
@@ -1,7 +0,0 @@
-
-var handler = async (m, { command }) => {
-    if (command === 'canal') {
-        const canalLink = 'https://whatsapp.com/channel/0029Vaua0ZD3gvWjQaIpSy18'; // Reemplaza esto con tu enlace real
-        conn.reply(m.chat, `¡Aquí está el enlace de mi canal! 👉 ${canalLink}`, m);
-    }
-};
